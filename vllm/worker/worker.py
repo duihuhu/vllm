127f03f5b0f18b479835c579e838545720744ed4
duihuhu <duihuhu@163.com>
2023-08-02 16:16:17 +0800

    debug breakdown prefill and decode

--- vllm/worker/worker.py
+++ vllm/worker/worker.py
@@ -279,7 +279,8 @@ class Worker:
         # Prepare input tensors.
         input_tokens, input_positions, input_metadata = self._prepare_inputs(
             seq_group_metadata_list)
-
+        
+        print(self.gpu_cache)
         # Execute the model.
         output = self.model(
             input_ids=input_tokens,
