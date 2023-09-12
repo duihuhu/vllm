24f1f03adabe79d37ac0ee5a5b63afdd40edb433
duihuhu <duihuhu@163.com>
2023-09-12 13:59:39 +0800

    debug seq

--- vllm/entrypoints/llm.py
+++ vllm/entrypoints/llm.py
@@ -169,7 +169,7 @@ class LLM:
         
         while self.llm_engine.has_unfinished_requests():
             step_outputs = self.llm_engine.step_decoder()
-            print(step_outputs)
+            # print(step_outputs)
             for output in step_outputs:
                 if output.finished:
                     outputs.append(output)
