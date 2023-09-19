df330fa395bf14c8874ffc78762748d3558aa68e
duihuhu <duihuhu@163.com>
2023-09-19 10:11:30 +0800

    debug key value content swap blocks

--- vllm/worker/cache_engine.py
+++ vllm/worker/cache_engine.py
@@ -162,8 +162,8 @@ class CacheEngine:
                 #     print("ks, ds : ", dst_value_cache[ds].shape)
                 # print(src_to_dst)
                 # print(src_key_cache)
-                dst_key_cache_address = hex(id(dst_key_cache))
-                print("dst_key_cache_address: ", dst_key_cache_address)
+                dst_value_cache_address = hex(id(dst_value_cache))
+                print("dst_key_cache_address: ", dst_value_cache_address)
                 
     def swap_out_prefilled(self, src_to_dst: Dict[int, int]) -> None:
         self._swap_prefilled(self.gpu_cache, self.cpu_cache, src_to_dst)
