b9fad0f422818cd17fcd168c44e605fcfa82062d
duihuhu <duihuhu@163.com>
2023-09-22 09:00:39 +0800

    debug vllm only change status

--- benchmarks/experiment_batch_size_increase.sh
+++ benchmarks/experiment_batch_size_increase.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 num_prompts=0
-for ((batch_size=2; batch_size<=64; batch_size=batch_size*2))
+for ((ts=1; ts<=4; ts=ts*4))
 do
-  for ((ts=1; ts<=4; ts=ts*4))
+  for ((batch_size=2; batch_size<=4; batch_size=batch_size*2))
   do
     if [ $ts -eq 1 ]
     then
-      num_prompts=64
+      num_prompts=2
     else
-      num_prompts=128
+      num_prompts=4
     fi
     echo $batch_size $ts $num_prompts
 
-    # python3 benchmark_throughput.py --backend vllm --dataset /workspace/ShareGPT_V3_unfiltered_cleaned_split.json \
-    # --model /workspace/opt-13b/model/snapshots/e515202d1e7750da62d245fbccb2723b9c1790f5/ --num-prompts 64 --tensor-parallel-size=1
+    python3 benchmark_throughput.py --backend vllm --dataset /workspace/ShareGPT_V3_unfiltered_cleaned_split.json \
+    --model /workspace/opt-13b/model/snapshots/e515202d1e7750da62d245fbccb2723b9c1790f5/ --num-prompts $num_prompts --tensor-parallel-size=$ts
 
   done 
 done
