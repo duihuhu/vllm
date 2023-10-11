e260c04bea872d14c71e1009daf799873a3cc886
duihuhu <duihuhu@163.com>
2023-10-11 11:10:29 +0800

    debug key value content swap blocks

--- vllm/worker/worker.py
+++ vllm/worker/worker.py
@@ -281,6 +281,7 @@ class Worker:
         blocks_to_swap_out: Dict[SequenceGroup, Dict[int, int]],
         blocks_to_object_swap_out: Dict[SequenceGroup, Dict[int, List[plasma_object.ObjectID]]],
     )  -> None:
+        print("blocks_to_object_swap_out: ", blocks_to_object_swap_out)
         if blocks_to_swap_out:
             for key, value in blocks_to_swap_out.items():
                 self.cache_engine.swap_out_prefilled(value)
