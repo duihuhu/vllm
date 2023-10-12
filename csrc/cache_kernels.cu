b60365bfe40f654c4b2afddd059d0254b0256dac
duihuhu <duihuhu@163.com>
2023-10-12 09:51:09 +0800

    debug key value content swap blocks

--- csrc/cache_kernels.cu
+++ csrc/cache_kernels.cu
@@ -27,7 +27,7 @@ void swap_blocks_to_object(
 
   const int64_t block_size_in_bytes = src.element_size() * src[0].numel();
   float *t_dest = float* malloc(block_size_in_bytes);
-  memset(t_dest, 0, block_size_in_bytes)
+  memset(t_dest, 0, block_size_in_bytes);
   printf("start Tensor Data:\n");
 
   // for (int i = 0; i < block_size_in_bytes; i++) {
