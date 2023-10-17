6d897491358180fc4eb484cd4c3421d93f06ad7a
duihuhu <duihuhu@163.com>
2023-10-17 13:12:57 +0800

    debug swap in from plasma object to gpu block when convert prefilled object to running

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -608,7 +608,7 @@ class Scheduler:
                 seq_id = seq.seq_id
                 seq_data[seq_id] = seq.data
                 block_tables[seq_id] = self.block_manager.get_block_table(seq)
-
+                print("schedule seq id block tables ", seq_id, block_tables[seq_id])
             seq_group_metadata = SequenceGroupMetadata(
                 request_id=seq_group.request_id,
                 is_prompt=is_prompt,
@@ -640,8 +640,8 @@ class Scheduler:
                 seq_id = seq.seq_id
                 seq_data[seq_id] = seq.data
                 block_tables[seq_id] = self.block_manager.get_object_block_table(seq)
-                print("block tables ", block_tables[seq_id])
-                
+                print("obj_schedule seq id block tables ", seq_id, block_tables[seq_id])
+
             seq_group_metadata = SequenceGroupMetadata(
                 request_id=seq_group.request_id,
                 is_prompt=is_prompt,
