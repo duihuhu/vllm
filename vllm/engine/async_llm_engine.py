263633afd0969ec9b424d9bbec8c42ef59d081ef
duihuhu <duihuhu@163.com>
2023-10-18 17:10:07 +0800

    debug add client for send requests to multi vllm instances

--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -137,12 +137,19 @@ class AsyncLLMEngine:
         start = time.time()
         outputs: List[RequestOutput] = []
         while self.engine.has_unfinished_requests():
-            print("step")
             step_outputs = self.engine.step()
             for output in step_outputs:
                 if output.finished:
                     outputs.append(output)
         end = time.time()
+
+        elapsed_time = end-start
+        total_num_tokens = sum(
+            len(output.prompt_token_ids) + len(output.outputs[0].token_ids)
+            for output in outputs
+        )
+        print(f"Throughput: {len(outputs) / elapsed_time:.2f} requests/s, "
+            f"{total_num_tokens / elapsed_time:.2f} tokens/s")
 
 
        
