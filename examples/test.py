11b209b0ae6d291e6fed6cb90fe54680ebf02c21
duihuhu <duihuhu@163.com>
2023-10-19 08:46:24 +0800

    debug first to run prefill alone on gpus, and client to send requests to prefill server and receive prefilled status back

--- examples/test.py
+++ examples/test.py
@@ -1,20 +1,22 @@
 import threading
 n = 0
-def add_ops(n):
+def add_ops():
+  global n
   while True:
     n = n + 1
     if n == 10:
       break
     
-def print_ops(n):
+def print_ops():
+  global n
   while True:
     print(n)
     
 if __name__ == "__main__":
   task_tds = []
 
-  task_tds.append(threading.Thread(target=add_ops, args=(n,)))
-  task_tds.append(threading.Thread(target=print_ops, args=(n,)))
+  task_tds.append(threading.Thread(target=add_ops))
+  task_tds.append(threading.Thread(target=print_ops))
   for td in task_tds:
     td.start()
     
