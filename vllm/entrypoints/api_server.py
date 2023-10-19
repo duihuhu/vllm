4b745caca8a641411c9d5b02d4744607548c4987
duihuhu <duihuhu@163.com>
2023-10-19 11:17:48 +0800

    debug add test for ops

--- vllm/entrypoints/api_server.py
+++ vllm/entrypoints/api_server.py
@@ -15,6 +15,57 @@ TIMEOUT_KEEP_ALIVE = 5  # seconds.
 TIMEOUT_TO_PREVENT_DEADLOCK = 1  # seconds.
 app = FastAPI()
 
+@app.post("/mul_generate")
+async def mul_generate(request: Request) -> Response:
+    """Generate completion for the request, containing a list of prompts.
+
+    The request should be a JSON object with the following fields:
+    - prompts: the prompt to use for the generation.
+    - stream: whether to stream the results or not.
+    - other fields: the sampling parameters (See `SamplingParams` for details).
+    """
+    request_dict = await request.json()
+    prompt = request_dict.pop("prompt")
+    stream = request_dict.pop("stream", False)
+    sampling_params = SamplingParams(**request_dict)
+    # # request_id = random_uuid()
+    
+    results_generator = engine.mul_generate(prompt, sampling_params)
+
+    # # Streaming case
+    # async def stream_results() -> AsyncGenerator[bytes, None]:
+    #     async for request_output in results_generator:
+    #         prompt = request_output.prompt
+    #         text_outputs = [
+    #             prompt + output.text for output in request_output.outputs
+    #         ]
+    #         ret = {"text": text_outputs}
+    #         yield (json.dumps(ret) + "\0").encode("utf-8")
+
+    # async def abort_request() -> None:
+    #     await engine.abort(request_id)
+
+    # if stream:
+    #     background_tasks = BackgroundTasks()
+    #     # Abort the request if the client disconnects.
+    #     background_tasks.add_task(abort_request)
+    #     return StreamingResponse(stream_results(), background=background_tasks)
+
+    # # Non-streaming case
+    # final_output = None
+    # async for request_output in results_generator:
+    #     if await request.is_disconnected():
+    #         # Abort the request if the client disconnects.
+    #         await engine.abort(request_id)
+    #         return Response(status_code=499)
+    #     final_output = request_output
+
+    # assert final_output is not None
+    # prompt = final_output.prompt
+    # text_outputs = [prompt + output.text for output in final_output.outputs]
+    # ret = {"text": text_outputs}
+    ret = {"text": 'test'}
+    return JSONResponse(ret)
 
 @app.post("/generate")
 async def generate(request: Request) -> Response:
