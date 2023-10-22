8623c8de99e09a5c815eb61098d8cabfe68424a2
duihuhu <duihuhu@163.com>
2023-10-22 10:38:54 +0800

    debug add object id allocate client and server

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -348,11 +348,6 @@ class LLMEngine:
                 print("key seq_data_prompt_token_id, output_token_ids cumulative_logprob: ", 
                       k, seq_data.prompt_token_ids, seq_data.output_token_ids, seq_data.cumulative_logprob)
  
-        
-        print("scheduler_outputs.blocks_to_swap_in", scheduler_outputs.blocks_to_swap_in)
-        print("scheduler_outputs.blocks_to_swap_out", scheduler_outputs.blocks_to_swap_out)
-        print("scheduler_outputs.blocks_to_copy", scheduler_outputs.blocks_to_copy)
-
         output = self._run_workers(
             "execute_model_decode",
             seq_group_metadata_list=seq_group_metadata_list,
