5a576f864fdf938ed1c5911915b86008ba28df10
duihuhu <duihuhu@163.com>
2023-10-23 16:18:44 +0800

    debug vllm split by change parameter

--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -187,7 +187,7 @@ class AsyncLLMEngine:
                 step_outputs = self.engine.step_decoder()
                 for output in step_outputs:
                     if output.finished:
-                        print("output: ", output)
+                        # print("output: ", output)
                         outputs.append(output)
 
             end = time.time()
