3b0e37118b357621177c791299b1effebcc8d161
duihuhu <duihuhu@163.com>
2023-10-23 14:47:28 +0800

    debug vllm origin by change parameter

--- examples/mul_vllm_serving_client.py
+++ examples/mul_vllm_serving_client.py
@@ -44,10 +44,11 @@ def post_http_request(prompt: List[str],
         headers = {"User-Agent": "Test Client"}
         pload = {
             "prompt": prompt[tid*num_prompt:(tid+1)*num_prompt],
-            "n": n,
-            "use_beam_search": True,
+            "n": 1,
+            "use_beam_search": False,
             "temperature": 0.0,
             "max_tokens": 16,
+            'ignore_eos': True,
             "stream": stream,
         }
         response = requests.post(api_url, headers=headers, json=pload, stream=True)
