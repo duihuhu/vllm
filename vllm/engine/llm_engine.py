44e85a1085cee49a899e37e28d53b84b3397cd39
FredHuang99 <woshihhydy@163.com>
2023-10-24 14:33:10 +0800

    debug

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -352,25 +352,18 @@ class LLMEngine:
         
         (seq_group_metadata_list, scheduler_outputs,
          ignored_seq_groups) = self.scheduler.obj_decode_schedule()
-        
-        for seq_group_metadata in seq_group_metadata_list:
-            print("request_id sampling_params is_prmopt: ", \
-                  seq_group_metadata.request_id, seq_group_metadata.sampling_params, seq_group_metadata.is_prompt)
-            for k, seq_data in seq_group_metadata.seq_data.items():
-                print("key seq_data_prompt_token_id, output_token_ids cumulative_logprob: ", 
-                      k, seq_data.prompt_token_ids, seq_data.output_token_ids, seq_data.cumulative_logprob)
 
         if ((not seq_group_metadata_list) and scheduler_outputs.is_empty()
                 and (not ignored_seq_groups)):
             # Nothing to do.
             return []
         
-        # for seq_group_metadata in seq_group_metadata_list:
-        #     print("request_id sampling_params is_prmopt: ", \
-        #           seq_group_metadata.request_id, seq_group_metadata.sampling_params, seq_group_metadata.is_prompt)
-        #     for k, seq_data in seq_group_metadata.seq_data.items():
-        #         print("seq id, seq_data_prompt_token_id, output_token_ids cumulative_logprob: ", 
-        #               k, seq_data.prompt_token_ids, seq_data.output_token_ids, seq_data.cumulative_logprob)
+        for seq_group_metadata in seq_group_metadata_list:
+             print("request_id sampling_params is_prmopt: ", \
+                   seq_group_metadata.request_id, seq_group_metadata.sampling_params, seq_group_metadata.is_prompt)
+             for k, seq_data in seq_group_metadata.seq_data.items():
+                 print("seq id, seq_data_prompt_token_id, output_token_ids cumulative_logprob: ", 
+                       k, seq_data.prompt_token_ids, seq_data.output_token_ids, seq_data.cumulative_logprob)
 
 
         output = self._run_workers(
