6f6b3aa57a938fd31b6f39e89bc80b15f7751d1c
FredHuang99 <woshihhydy@163.com>
2023-10-30 11:10:36 +0800

    add prints for debug

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -159,6 +159,9 @@ class Scheduler:
 
     def _obj_schedule(
             self) -> Tuple[SchedulerOutputs, List[str], List[SequenceGroup]]:
+        print(f"running length {len(self.running)}")
+        print(f"swapped length {len(self.swapped)}")
+        print(f"waiting length {len(self.waiting)}")
         # Blocks that need to be swaped or copied before model execution.
         blocks_to_swap_in: Dict[int, List[ObjectInfo]] = {}
         blocks_to_swap_out: Dict[int, int] = {}
@@ -166,7 +169,7 @@ class Scheduler:
         ignored_seq_groups: List[SequenceGroup] = []
 
         blocks_to_object_swap_in: Dict[SequenceGroup, Dict[int, List[ObjectInfo]]] = {}
-
+   
         # Fix the current time.
         now = time.time()
 
