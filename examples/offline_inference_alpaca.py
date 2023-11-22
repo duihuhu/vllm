10d8ef1aa4b3658cf03f50cf3aaad263f45ac82c
duihuhu <duihuhu@163.com>
2023-11-22 14:24:29 +0800

    offline inference for alpaca dataset

--- examples/offline_inference_alpaca.py
+++ examples/offline_inference_alpaca.py
@@ -13,17 +13,15 @@ def get_alpaca(dataset_path):
 # Create a sampling params object.
 sampling_params = SamplingParams(temperature=0.95, top_p=1, max_tokens=2048)
 datasets = get_alpaca("/workspace/alpaca_data.json")
-for data in datasets:
-    print(data[0])
+
 # Create an LLM.
-# llm = LLM(model="/workspace/opt-13b/model/snapshots/e515202d1e7750da62d245fbccb2723b9c1790f5")
-# # Generate texts from the prompts. The output is a list of RequestOutput objects
-# # that contain the prompt, generated text, and other information.
-# for data in datasets:
-#     print(data["instruction"])
-#     outputs = llm.generate(data["instruction"], sampling_params)
-#     # Print the outputs.
-#     for output in outputs:
-#         prompt = output.prompt
-#         generated_text = output.outputs[0].text
-#         print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+llm = LLM(model="/workspace/opt-13b/model/snapshots/e515202d1e7750da62d245fbccb2723b9c1790f5")
+# Generate texts from the prompts. The output is a list of RequestOutput objects
+# that contain the prompt, generated text, and other information.
+for data in datasets:
+    outputs = llm.generate(data[0], sampling_params)
+    # Print the outputs.
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
