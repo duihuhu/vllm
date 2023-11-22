effcc47eae9a97d47137c1917920930d43052440
duihuhu <duihuhu@163.com>
2023-11-22 17:04:57 +0800

    offline inference for alpaca dataset

--- examples/offline_inference_alpaca.py
+++ examples/offline_inference_alpaca.py
@@ -21,20 +21,21 @@ llm = LLM(model="/workspace/opt-13b/model/snapshots/e515202d1e7750da62d245fbccb2
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
 opt_answer_datasets = []
-for data in datasets:
-    js = {}
-    outputs = llm.generate(data[0], sampling_params)
-    # Print the outputs.
-    for output in outputs:
-        prompt = output.prompt
-        generated_text = output.outputs[0].text
-        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
-        js['instruction'] = data[0]
-        js['output_text'] = generated_text
-        js['input'] = ""
-        js['output'] = len(output.outputs[0].token_ids)
-        js['stop_reason'] = output.outputs[0].finish_reason
-        opt_answer_datasets.append(js)
-        
 with open("alpaca_opt13b_answer.json", 'w+') as json_file:
-    json.dump(opt_answer_datasets, json_file, indent=2) 
+    for data in datasets:
+        js = {}
+        outputs = llm.generate(data[0], sampling_params)
+        # Print the outputs.
+        for output in outputs:
+            prompt = output.prompt
+            generated_text = output.outputs[0].text
+            print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+            js['instruction'] = data[0]
+            js['output_text'] = generated_text
+            js['input'] = ""
+            js['output'] = len(output.outputs[0].token_ids)
+            js['stop_reason'] = output.outputs[0].finish_reason
+            opt_answer_datasets.append(js)
+            
+
+        json.dump(js, json_file, indent=2) 
