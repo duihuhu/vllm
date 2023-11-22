7d3553e8b1674e5749d5d6aca5d901ee7a0f5b0b
duihuhu <duihuhu@163.com>
2023-11-22 17:08:32 +0800

    offline inference for alpaca dataset

--- examples/offline_inference_alpaca.py
+++ examples/offline_inference_alpaca.py
@@ -14,28 +14,27 @@ def get_alpaca(dataset_path):
 
 # Create a sampling params object.
 sampling_params = SamplingParams(temperature=0.95, top_p=1, max_tokens=4096)
-datasets = get_alpaca("/workspace/alpaca_data.json")
+datasets = get_alpaca("/workspace/alpaca_data_test.json")
 
 # Create an LLM.
 llm = LLM(model="/workspace/opt-13b/model/snapshots/e515202d1e7750da62d245fbccb2723b9c1790f5")
 # Generate texts from the prompts. The output is a list of RequestOutput objects
 # that contain the prompt, generated text, and other information.
 opt_answer_datasets = []
+for data in datasets:
+    js = {}
+    outputs = llm.generate(data[0], sampling_params)
+    # Print the outputs.
+    for output in outputs:
+        prompt = output.prompt
+        generated_text = output.outputs[0].text
+        print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
+        js['instruction'] = data[0]
+        js['output_text'] = generated_text
+        js['input'] = ""
+        js['output'] = len(output.outputs[0].token_ids)
+        js['stop_reason'] = output.outputs[0].finish_reason
+        opt_answer_datasets.append(js)
+        
 with open("alpaca_opt13b_answer.json", 'w+') as json_file:
-    for data in datasets:
-        js = {}
-        outputs = llm.generate(data[0], sampling_params)
-        # Print the outputs.
-        for output in outputs:
-            prompt = output.prompt
-            generated_text = output.outputs[0].text
-            print(f"Prompt: {prompt!r}, Generated text: {generated_text!r}")
-            js['instruction'] = data[0]
-            js['output_text'] = generated_text
-            js['input'] = ""
-            js['output'] = len(output.outputs[0].token_ids)
-            js['stop_reason'] = output.outputs[0].finish_reason
-            opt_answer_datasets.append(js)
-            
-
-        json.dump(js, json_file, indent=2) 
+    json.dump(opt_answer_datasets, json_file, indent=2) 
