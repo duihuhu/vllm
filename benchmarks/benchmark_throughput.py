7136647d52784a1fcabe477e76dee7187ec98a4d
duihuhu <duihuhu@163.com>
2023-12-04 11:18:25 +0800

    debug test for prompt heavy

--- benchmarks/benchmark_throughput.py
+++ benchmarks/benchmark_throughput.py
@@ -43,8 +43,8 @@ def sample_requests(
         # output_len = len(completion_token_ids[8])
         # tokenized_dataset.append((prompts[8], prompt_token_ids[8], output_len))
         # else:
-        output_len = len(completion_token_ids[71])
-        tokenized_dataset.append((prompts[71], prompt_token_ids[71], output_len))
+        output_len = len(completion_token_ids[227])
+        tokenized_dataset.append((prompts[227], prompt_token_ids[227], output_len))
     # print(prompts[71])
     # Filter out too long sequences.
     filtered_dataset: List[Tuple[str, int, int]] = []
