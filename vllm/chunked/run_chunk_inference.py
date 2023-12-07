d88374c4f9223a61ab78987c97d887a9b255a1a8
FredHuang99 <woshihhydy@163.com>
2023-12-07 13:01:25 +0800

    start verify the chunk prefill unit: debug3

--- vllm/chunked/run_chunk_inference.py
+++ vllm/chunked/run_chunk_inference.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     parser.add_argument("--tokenizer", type=str, default=None)
     parser.add_argument("--num-prompts", type=int, default=10,
                         help="Number of prompts to process.")
-    parser.add_argument("--chunk-szie", type=int, default=560)
+    parser.add_argument("--chunk-size", type=int, default=560)
     parser.add_argument("--chunk-num", type=int, default=100)
     args = parser.parse_args()
 
