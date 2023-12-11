faa42291df5669dbdb4c14b7520b2c1823af70d7
duihuhu <duihuhu@163.com>
2023-12-11 14:58:20 +0800

    debug the first token is not sam

--- vllm/model_executor/models/opt.py
+++ vllm/model_executor/models/opt.py
@@ -251,27 +251,27 @@ class OPTDecoder(nn.Module):
         inputs_embeds = self.embed_tokens(input_ids)
         pos_embeds = self.embed_positions(positions)
         
+        if self.project_in is not None:
+            inputs_embeds, _ = self.project_in(inputs_embeds)
+        hidden_states = inputs_embeds + pos_embeds
+        
         print("inputs_embeds shape: ", inputs_embeds.shape)
         dim0, dim1, dim2 = inputs_embeds.shape
         import numpy as np
         if dim0 > 1:
             if self.index == 1:
                 # inputs_embeds_shaped = inputs_embeds.reshape(inputs_embeds[].shape[0], -1)
-                print("sample_results pos_embeds : ", pos_embeds[1])
-                x_t = pos_embeds[1].cpu().numpy()
-                np.savetxt("pos_embeds1.txt", x_t, delimiter='\n')
+                print("sample_results hidden_states : ", hidden_states[1])
+                x_t = hidden_states[1].cpu().numpy()
+                np.savetxt("hidden_states1.txt", x_t, delimiter='\n')
         else:
             if self.index == 1:
                 # inputs_embeds_shaped = inputs_embeds.reshape(inputs_embeds.shape[0], -1)
-                print("sample_results pos_embeds : ", pos_embeds[0])
-                x_t = pos_embeds[0].cpu().numpy()
-                np.savetxt("pos_embeds0.txt", x_t, delimiter='\n')
+                print("sample_results hidden_states : ", hidden_states[0])
+                x_t = hidden_states[0].cpu().numpy()
+                np.savetxt("hidden_states0.txt", x_t, delimiter='\n')
         self.index = self.index + 1
         
-        if self.project_in is not None:
-            inputs_embeds, _ = self.project_in(inputs_embeds)
-        hidden_states = inputs_embeds + pos_embeds
-
         for i in range(len(self.layers)):
             cache_event = None if cache_events is None else cache_events[i]
             layer = self.layers[i]
