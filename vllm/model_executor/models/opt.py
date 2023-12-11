8c0cb55019ed6dba98467a4903961c39df2acd33
duihuhu <duihuhu@163.com>
2023-12-11 14:45:07 +0800

    debug the first token is not sam

--- vllm/model_executor/models/opt.py
+++ vllm/model_executor/models/opt.py
@@ -251,8 +251,8 @@ class OPTDecoder(nn.Module):
         inputs_embeds = self.embed_tokens(input_ids)
         pos_embeds = self.embed_positions(positions)
         
-        print("input_ids shape: ", input_ids.shape)
-        dim0, dim1 = input_ids.shape
+        print("inputs_embeds shape: ", inputs_embeds.shape)
+        dim0, dim1, dim2 = inputs_embeds.shape
         # import numpy as np
         # if dim0 > 1:
         #     if self.index == 1:
