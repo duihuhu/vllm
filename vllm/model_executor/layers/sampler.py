db5cc971702702f773fd21c008f1b87a82d25a70
duihuhu <duihuhu@163.com>
2023-12-11 09:17:53 +0800

    debug the first token is not sam

--- vllm/model_executor/layers/sampler.py
+++ vllm/model_executor/layers/sampler.py
@@ -92,9 +92,9 @@ class Sampler(nn.Module):
 
         # Sample the next tokens.
         sample_results = _sample(probs, logprobs, sampling_metadata)
-        print("sample_results logits", logits)
-        print("sample_results logits", probs)
-        print("sample_results logits", logprobs)
+        print("sample_results logits ", logits)
+        print("sample_results probs ", probs)
+        print("sample_results logprobs ", logprobs)
         # Get the logprobs query results.
         prompt_logprobs, sample_logprobs = _get_logprobs(
             logprobs, sampling_metadata, sample_results)
