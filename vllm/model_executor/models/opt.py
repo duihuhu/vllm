01590a8d253a89034354cde7bbf53ba36f7fe5ee
duihuhu <duihuhu@163.com>
2023-12-11 14:29:30 +0800

    debug the first token is not sam

--- vllm/model_executor/models/opt.py
+++ vllm/model_executor/models/opt.py
@@ -255,9 +255,9 @@ class OPTDecoder(nn.Module):
         import numpy as np
         if dim0 > 1:
             if self.index == 1:
-                pos_embeds_shaped = pos_embeds.reshape(pos_embeds.shape[0], -1)
+                pos_embeds_shaped = pos_embeds.reshape(pos_embeds[-1].shape[0], -1)
                 print("sample_results inputs_embeds : ", pos_embeds_shaped)
-                x_t = pos_embeds_shaped[-1].cpu().numpy()
+                x_t = pos_embeds_shaped.cpu().numpy()
                 np.savetxt("pos_embeds1.txt", x_t, delimiter=',')
         else:
             if self.index == 1:
