5f14c716102521d7367fe1675b1c4df9ca850f98
duihuhu <duihuhu@163.com>
2023-12-13 08:43:00 +0800

    debug qkv value

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -286,13 +286,13 @@ class ModelRunner:
         import numpy as np
         dim0, dim1, dim2 = hidden_states.shape
         if dim0 > 1:
-            if self.index == 2:
+            if self.index == 3:
                 # inputs_embeds_shaped = inputs_embeds.reshape(inputs_embeds[].shape[0], -1)
                 print("sample_results hidden_states : ", hidden_states[1])
                 x_t = hidden_states[1].cpu().numpy()
                 np.savetxt("hidden_states13.txt", x_t, delimiter='\n')
         else:
-            if self.index == 2:
+            if self.index == 3:
                 # inputs_embeds_shaped = inputs_embeds.reshape(inputs_embeds.shape[0], -1)
                 print("sample_results hidden_states : ", hidden_states[0])
                 x_t = hidden_states[0].cpu().numpy()
