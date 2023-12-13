fad85c536c48be19a0759932ac13b3f6ed018cfa
duihuhu <duihuhu@163.com>
2023-12-13 12:10:15 +0800

    debug qkv value

--- vllm/model_executor/layers/sampler.py
+++ vllm/model_executor/layers/sampler.py
@@ -479,7 +479,7 @@ def _random_sample(
     random_samples_cat = []
     import numpy as np
     
-    if dim0 > 1 and dim0!=256:
+    if dim0 > 1 and dim0!=256 and index > 1:
         for i in range(dim0):
             print("probs i shape: ", probs[i].unsqueeze(0).shape)
             random_samples = torch.multinomial(probs[i].unsqueeze(0),
