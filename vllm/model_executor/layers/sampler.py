e61707aee2e7edf472ff99347f0bb8eab4d35ca9
duihuhu <duihuhu@163.com>
2023-12-13 21:43:13 +0800

    debug qkv value

--- vllm/model_executor/layers/sampler.py
+++ vllm/model_executor/layers/sampler.py
@@ -536,14 +536,12 @@ def _random_sample(
     # else:
     #     if index == 2:
     #         print("random_samples shape " ,random_samples.shape, max_best_of)
-    print("initial_seed ", torch.initial_seed())
-    rng_state1 = torch.get_rng_state()
+    print("initial_seed ", torch.cuda.initial_seed())
+    rng_state1 = torch.cuda.get_rng_state()
     random_samples = torch.multinomial(probs,
                         num_samples=max_best_of,
                         replacement=True).cpu()
-    print("random_samples orgin 0 : ", type(random_samples),  " ", random_samples.shape, " ", random_samples, "\n")
-    random_samples_a = probs.multinomial(1,replacement=True).cpu()
-    rng_state2 = torch.get_rng_state()
+    rng_state2 = torch.cuda.get_rng_state()
     if torch.equal(rng_state2, rng_state1):
         print("rng_state2 rng_state1 equal ")
     else:
