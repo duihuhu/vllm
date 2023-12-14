d60bc89d02082efd4062da93d723071c21ef173f
FredHuang99 <woshihhydy@163.com>
2023-12-14 23:23:49 +0800

    test2 start

--- vllm/worker/worker.py
+++ vllm/worker/worker.py
@@ -284,7 +284,7 @@ class Worker:
             seq_group_metadata_list)
         
         # Execute the model.
-        st = time.time()
+        #st = time.time()
         output = self.model(
             input_ids=input_tokens,
             positions=input_positions,
@@ -292,8 +292,8 @@ class Worker:
             input_metadata=input_metadata,
             cache_events=cache_events,
         )
-        ed = time.time()
-        print(f"st at {st}, ed at {ed}, costs {ed - st}s")
+        #ed = time.time()
+        #print(f"st at {st}, ed at {ed}, costs {ed - st}s")
         # for seq_group_metadata in seq_group_metadata_list:
         #     print("seq_group_metadata:",seq_group_metadata, seq_group_metadata.is_prompt)
         return output
