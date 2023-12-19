75758ec419e6658669b4f97818602a73ff39716f
duihuhu <duihuhu@163.com>
2023-12-19 10:36:43 +0800

    debug heavy decode and light decode mix

--- benchmarks/benchmark_throughput.py
+++ benchmarks/benchmark_throughput.py
@@ -40,7 +40,7 @@ def sample_requests(
     completion_token_ids = tokenizer(completions).input_ids
     tokenized_dataset = []
     for i in range(len(dataset)):
-        if i != 0 :
+        if i %3 == 0 :
             output_len = len(completion_token_ids[8])
             tokenized_dataset.append((prompts[8], prompt_token_ids[8], output_len))
         else:
