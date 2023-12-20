0ad9c815906c56053d47c795dcd9455be747888f
duihuhu <duihuhu@163.com>
2023-12-20 10:16:01 +0800

    debug add mock prefill decode serving by signal

--- examples/prefill_decode_serving_client_bak.py
+++ examples/prefill_decode_serving_client_bak.py
@@ -1,6 +1,7 @@
 """Example Python client for vllm.entrypoints.api_server"""
-# python3 mul_vllm_serving_client.py --dataset /workspace/ShareGPT_V3_unfiltered_cleaned_split.json --model /workspace/opt-13b/model/snapshots/e515202d1e7750da62d245fbccb2723b9c1790f5/  --num-prompts 200 --num-servers 2
-#python3 -m vllm.entrypoints.api_server --model /workspace/opt-13b/model/snapshots/e515202d1e7750da62d245fbccb2723b9c1790f5/  --host 127.0.0.1 --port 8001 --tensor-parallel-size 2
+#start prefilled vllm: python3 -m vllm.entrypoints.api_server --model /workspace/opt-13b/model/snapshots/e515202d1e7750da62d245fbccb2723b9c1790f5/  --host 127.0.0.1 --port 8000 --tensor-parallel-size 2
+#start decode vllm:  python3 -m vllm.entrypoints.api_server --model /workspace/opt-13b/model/snapshots/e515202d1e7750da62d245fbccb2723b9c1790f5/  --host 127.0.0.1 --port 8001 --tensor-parallel-size 2
+#start workload: python3 prefill_decode_serving_client.py --dataset /workspace/ShareGPT_V3_unfiltered_cleaned_split.json --model /workspace/opt-13b/model/snapshots/e515202d1e7750da62d245fbccb2723b9c1790f5/  --num-prompts 4 --host 127.0.0.1 --port 9000
 
 import argparse
 import json
@@ -14,10 +15,20 @@ import random
 import threading
 from vllm.utils import random_uuid
 
+import fastapi
+from fastapi import Request
+from fastapi.responses import JSONResponse, Response, StreamingResponse
+
+import uvicorn
+import time
+
 request_prompts_token_ids = {}
 request_prompts = {}
 
-status = 0
+app = fastapi.FastAPI()
+TIMEOUT_KEEP_ALIVE = 5  # seconds
+
+
 def clear_line(n: int = 1) -> None:
     LINE_UP = '\033[1A'
     LINE_CLEAR = '\x1b[2K'
@@ -25,75 +36,78 @@ def clear_line(n: int = 1) -> None:
         print(LINE_UP, end=LINE_CLEAR, flush=True)
 
 
-def post_http_request(prompt: List[str],
-                      output_lens: List[int],
-                      api_url: str,
-                      n: int = 1,
-                      stream: bool = False,
-                      tid: Optional[int] = 0,
-                      num_servers: Optional[int] = 1) -> requests.Response:
-    if num_servers == 1:
-        headers = {"User-Agent": "Test Client"}
-        pload = {
-            "prompt": prompt,
-            "output_lens": output_lens,
-            "n": 1,
-            "use_beam_search": False,
-            "temperature": 0.0,
-            # "max_tokens": 16,
-            'ignore_eos': True,
-            "stream": stream,
-        }
-        response = requests.post(api_url, headers=headers, json=pload, stream=True)
-    
-    else:
-        num_prompt = int(len(prompt)/num_servers)
-        headers = {"User-Agent": "Test Client"}
-        pload = {
-            "prompt": prompt[tid*num_prompt:(tid+1)*num_prompt],
-            "output_lens": output_lens[tid*num_prompt:(tid+1)*num_prompt],
-            "n": 1,
-            "use_beam_search": False,
-            "temperature": 0.0,
-            # "max_tokens": 16,
-            'ignore_eos': True,
-            "stream": stream,
-        }
-        response = requests.post(api_url, headers=headers, json=pload, stream=True)
-    return response
-
-
-def post_init_decode_prefill_http_request(prompt: List[str],
-                      output_lens: List[int],
+def post_inited_request(prompts: List[str],
+                        output_lens: List[int],
                       request_ids: List[str],
                       api_url: str,
                       n: int = 1,
                       stream: bool = False,
-                      tid: Optional[int] = 0,
-                      num_servers: Optional[int] = 1) -> requests.Response: 
+                      status: str = 'start') -> requests.Response:
     headers = {"User-Agent": "Test Client"}
     pload = {
-        "prompt": prompt,
+        "prompts": prompts,
         "output_lens": output_lens,
         "request_ids": request_ids,
-        "n": n,
+        "n": 1,
         "use_beam_search": False,
         "temperature": 0.0,
         # "max_tokens": 16,
         'ignore_eos': True,
         "stream": stream,
+        "status": status
     }
+    
     response = requests.post(api_url, headers=headers, json=pload, stream=True)
     return response
 
-def post_requests_to_prefilling(prompt: List[str],
-                      output_lens: List[int],
-                      api_url: str,
-                      n: int = 1,
-                      stream: bool = False,
-                      tid: Optional[int] = 0,
-                      num_servers: Optional[int] = 1):
-    return 
+def receive_prefilled_request(host, port):
+    uvicorn.run(app,
+              host=host,
+              port=port,
+              log_level="info",
+              timeout_keep_alive=TIMEOUT_KEEP_ALIVE)
+
+@app.post("/prefilled")
+async def prefilled(request: Request) -> Response:
+    request_dict = await request.json()
+    request_ids = request_dict.pop("request_ids")
+    seq_ids = request_dict.pop("seq_ids")
+    prefilled_token_ids =  request_dict.pop("prefilled_token_ids")
+    prefilled_text = request_dict.pop("prefilled_texts")
+    cumulative_logprobs = request_dict.pop("cumulative_logprobs")
+    output_lens = request_dict.pop("output_lens")
+
+    prompts = []
+    prompt_token_ids = []
+    for request_id in request_ids:
+        prompt_token_ids.append(request_prompts_token_ids.get(request_id))
+        prompts.append(request_prompts.get(request_id))
+        
+    headers = {"User-Agent": "Test Client"}
+    host = '127.0.0.1'
+    port = '8001'
+    api_url = f"http://{host}:{port}/continuous_batching"
+    
+    pload = {
+        "prompts": prompts,
+        "output_lens": output_lens,
+        "request_ids": request_ids,
+        "seq_ids": seq_ids,
+        "prompt_token_ids": prompt_token_ids,
+        "prefilled_token_ids": prefilled_token_ids,
+        "prefilled_texts" : prefilled_text,
+        "cumulative_logprobs": cumulative_logprobs,
+        "n": 1,
+        "use_beam_search": False,
+        "temperature": 0.0,
+        # "max_tokens": 16,
+        'ignore_eos': True,
+        "stream": False,
+        "status": 'prefilled'
+    }
+    response = requests.post(api_url, headers=headers, json=pload, stream=True)
+    return
+  
 def get_streaming_response(response: requests.Response) -> Iterable[List[str]]:
     for chunk in response.iter_lines(chunk_size=8192,
                                      decode_unicode=False,
@@ -113,7 +127,7 @@ def sample_requests(
     dataset_path: str,
     num_requests: int,
     tokenizer: PreTrainedTokenizerBase,
-) -> List[str]:
+) -> List[Tuple[str, List[int], str]] :
     random.seed(0)
     # Load the dataset.
     with open(dataset_path) as f:
@@ -130,6 +144,10 @@ def sample_requests(
     ]
 
     # Tokenize the prompts and completions.
+    # prompts = []
+    # for prompt, _ in dataset:
+    #     prompts.append("San Francisco is a")
+    
     prompts = [prompt for prompt, _ in dataset]
     prompt_token_ids = tokenizer(prompts).input_ids
     completions = [completion for _, completion in dataset]
@@ -137,11 +155,12 @@ def sample_requests(
     tokenized_dataset = []
     for i in range(len(dataset)):
         output_len = len(completion_token_ids[i])
-        # output_len = 16
         tokenized_dataset.append((prompts[i], prompt_token_ids[i], output_len))
 
     # Filter out too long sequences.
     # filtered_dataset: List[Tuple[str, int, int]] = []
+    # filtered_prompts: List[str] = [] 
+    # filtered_tokenids: List[str] = []
     filtered_dataset: List[Tuple[str, List[int], str, int]] = []
     for prompt, prompt_token_ids, output_len in tokenized_dataset:
         request_id = random_uuid()
@@ -162,12 +181,12 @@ def sample_requests(
     return sampled_prompts
 
 
-
 if __name__ == "__main__":
+    
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default="localhost")
     parser.add_argument("--port", type=int, default=8000)
-    parser.add_argument("--n", type=int, default=4)
+    parser.add_argument("--n", type=int, default=1)
     parser.add_argument("--prompt", type=str, default="San Francisco is a")
     parser.add_argument("--stream", action="store_true")
     parser.add_argument("--dataset", type=str, required=True,
@@ -176,8 +195,6 @@ if __name__ == "__main__":
                         help="Number of prompts to process.")
     parser.add_argument("--tokenizer", type=str, default=None)
     parser.add_argument("--model", type=str, default="facebook/opt-125m")
-    parser.add_argument("--num-servers", type=int, default=1)
-
       
     args = parser.parse_args()
     
@@ -195,25 +212,30 @@ if __name__ == "__main__":
       prompts.append(prompt[0])
       request_ids.append(prompt[-2])
       output_lens.append(prompt[-1])
-    
+    # prompts = ["What is the easiest idea to earn money", "What is the easiest idea to earn money"]
+    # prompt = args.prompt
     n = args.n
     stream = args.stream
-            
-    while True:
-        if status == 0:
-            host_decode = args.host
-            port_decode = args.port + 1
-            api_url = f"http://{host_decode}:{port_decode}/init_decode_prefill"
-            response = post_init_decode_prefill_http_request(prompts, output_lens, request_ids, api_url , n, stream)
-            status = status + 1
-        elif status == 2:
-            host_prefill = args.host
-            port_prefill = args.port
-            api_url = f"http://{host_prefill}:{port_prefill}/prefilled"
-            post_requests_to_prefilling(prompts, output_lens, api_url, n, stream)
+    api_url = f"http://{args.host}:{args.port-1000}/mul_generate"
+    
+    # api_url = f"http://{args.host}:{args.port-1000}/continuous_batching"
+    # response = post_inited_request(prompts, api_url, n, stream)
+    
 
-            break
-        
+    task_td = []
+    task_td.append(threading.Thread(target=receive_prefilled_request, args=(args.host, args.port)))
+
+    task_td.append(threading.Thread(target=post_inited_request, args=(prompts, output_lens, request_ids, api_url, n, stream)))
+      
+  
+    for td in task_td:
+      td.start()
+    for td in task_td:
+      td.join()  
+      
+    # task_td.append(pk_td = threading.Thread(post_prefilled_request, args=()))
+    
+    
     # if stream:
     #     num_printed_lines = 0
     #     for h in get_streaming_response(response):
