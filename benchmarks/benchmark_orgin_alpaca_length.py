7069787ab86dd5b0eda1894a025f0e94baddd389
duihuhu <duihuhu@163.com>
2023-12-26 20:50:34 +0800

    debug add api server prefill decode in one file

---
+++ benchmarks/benchmark_orgin_alpaca_length.py
@@ -0,0 +1,78 @@
+"""Benchmark offline inference throughput."""
+import argparse
+import json
+import random
+import time
+from typing import List, Tuple
+
+import torch
+from transformers import AutoModelForCausalLM, PreTrainedTokenizerBase
+from tqdm import tqdm
+
+from vllm import LLM, SamplingParams
+from vllm.transformers_utils.tokenizer import get_tokenizer
+
+
+def sample_requests(
+    dataset_path: str,
+    num_requests: int,
+    tokenizer: PreTrainedTokenizerBase,
+) -> List[Tuple[str, int, int]]:
+    # Load the dataset.
+    with open(dataset_path) as f:
+        dataset = json.load(f)
+    # Filter out the conversations with less than 2 turns.
+    aplaca_data = []
+    for data in dataset:
+        if data["input"] == "":
+            aplaca_data.append(data["instruction"], data["output"])
+
+    # Tokenize the prompts and completions.
+    prompts = [prompt for prompt, _ in aplaca_data]
+    prompt_token_ids = tokenizer(prompts).input_ids
+    completions = [completion for _, completion in aplaca_data]
+    completion_token_ids = tokenizer(completions).input_ids
+    tokenized_dataset = []
+    for i in range(len(aplaca_data)):
+        js = {}
+        js["instruction"] = aplaca_data["instruction"]
+        js["output"] = len(completion_token_ids[i])
+        tokenized_dataset.append(js)
+def main(args: argparse.Namespace):
+    print(args)
+    random.seed(args.seed)
+
+    # Sample the requests.
+    tokenizer = get_tokenizer(args.tokenizer)
+    requests = sample_requests(args.dataset, args.num_prompts, tokenizer)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Benchmark the throughput.")
+    parser.add_argument("--backend", type=str, choices=["vllm", "hf"],
+                        default="vllm")
+    parser.add_argument("--dataset", type=str, required=True,
+                        help="Path to the dataset.")
+    parser.add_argument("--model", type=str, default="facebook/opt-125m")
+    parser.add_argument("--tokenizer", type=str, default=None)
+    parser.add_argument("--tensor-parallel-size", "-tp", type=int, default=1)
+    parser.add_argument("--n", type=int, default=1,
+                        help="Number of generated sequences per prompt.")
+    parser.add_argument("--use-beam-search", action="store_true")
+    parser.add_argument("--num-prompts", type=int, default=1000,
+                        help="Number of prompts to process.")
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--hf-max-batch-size", type=int, default=None,
+                        help="Maximum batch size for HF backend.")
+    args = parser.parse_args()
+
+    if args.backend == "vllm":
+        if args.hf_max_batch_size is not None:
+            raise ValueError("HF max batch size is only for HF backend.")
+    elif args.backend == "hf":
+        if args.hf_max_batch_size is None:
+            raise ValueError("HF max batch size is required for HF backend.")
+    if args.tokenizer is None:
+        args.tokenizer = args.model
+
+    main(args)
---
+++ examples/api_server_prefill_decode.py
@@ -0,0 +1,271 @@
+import argparse
+import json
+from typing import AsyncGenerator
+
+from fastapi import BackgroundTasks, FastAPI, Request
+from fastapi.responses import JSONResponse, Response, StreamingResponse
+import uvicorn
+
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.engine.async_llm_engine import AsyncLLMEngine
+from vllm.sampling_params import SamplingParams
+from vllm.utils import random_uuid
+import asyncio
+import threading
+import time
+import multiprocessing
+# manager = multiprocessing.Manager()
+
+TIMEOUT_KEEP_ALIVE = 5  # seconds.
+TIMEOUT_TO_PREVENT_DEADLOCK = 1  # seconds.
+app = FastAPI()
+
+mdecode_status = "init_mdecode_prefill"
+mprefill_status_curr = "mprefill_execute"
+
+# decode_event = asyncio.Event()
+
+prefill_event = asyncio.Event()
+
+decode_event = threading.Event()
+
+@app.post("/notify_mdecode")
+async def notify_mdecode(request: Request) -> Response:
+    global decode_event
+    global mdecode_status
+    print("mdecode recv signal from mprefill ", time.time())
+    request_dict = await request.json()
+    request_ids = request_dict.pop("request_ids")
+    engine.convert_reqs_status(request_ids)
+    mdecode_status = "decode"
+    decode_event.set()
+    ret = {"text": 'test'}
+    return JSONResponse(ret)
+
+def init_mdecode_prefill(request_dict):
+    global mdecode_status
+    while True:
+        # print("init_mdecode_prefill ", mdecode_status)
+        if mdecode_status == "init_mdecode_prefill":
+            decode_event.wait()
+            # request_ids = request_dict.pop("request_ids")
+            # prompts = request_dict.pop("prompts")
+            # output_lens = request_dict.pop("output_lens")
+            # stream = request_dict.pop("stream", False)
+            # sampling_params_list = []
+            # for i in range(len(prompts)):
+            #     sampling_params = SamplingParams(**request_dict)
+            #     sampling_params_list.append(sampling_params)
+            results_generator = engine.generate_mdecode_prefill()
+        elif mdecode_status == "decode":
+            print("status is chanage, mdecode start exec decode", mdecode_status)
+            engine.generate_decode()
+        decode_event.clear()
+        decode_event.wait()
+        
+@app.on_event("startup")
+def startup_decode_event():
+    threading.Thread(target=init_mdecode_prefill, daemon=True).start()
+        
+async def mprefill_exec_prefill(request_dict):
+    global mprefill_status_curr
+    while True:
+        if mprefill_status_curr == "mprefill_execute":
+            print("mprefill exec prefill request ")
+            request_ids = request_dict.pop("request_ids")
+            prompts = request_dict.pop("prompts")
+            output_lens = request_dict.pop("output_lens")
+            stream = request_dict.pop("stream", False)
+            mprefill_status = request_dict.pop("mprefill_status")
+            sampling_params_list = []
+            for i in range(len(prompts)):
+                sampling_params = SamplingParams(**request_dict)
+                sampling_params_list.append(sampling_params)
+            results_generator = engine.generate_prefill(prompts=prompts, output_lens=output_lens, request_ids=request_ids, sampling_params=sampling_params_list, status=mprefill_status)
+        elif mprefill_status_curr == "mprefill_add":
+            results_generator = engine.generate_prefill(prompts=prompts, output_lens=output_lens, request_ids=request_ids, sampling_params=sampling_params_list, status=mprefill_status)
+        prefill_event.clear()
+        await prefill_event.wait()
+
+async def mprefill_add_prefill(request_dict):
+    print("mprefill add prefill request ")
+    global mprefill_status_curr
+    request_ids = request_dict.pop("request_ids")
+    prompts = request_dict.pop("prompts")
+    output_lens = request_dict.pop("output_lens")
+    stream = request_dict.pop("stream", False)
+    mprefill_status = request_dict.pop("mprefill_status")
+    sampling_params_list = []
+    for i in range(len(prompts)):
+        sampling_params = SamplingParams(**request_dict)
+        sampling_params_list.append(sampling_params)
+    results_generator = engine.generate_prefill(prompts=prompts, output_lens=output_lens, request_ids=request_ids, sampling_params=sampling_params_list, status=mprefill_status)
+    mprefill_status_curr = "mprefill_add"
+    prefill_event.set()
+
+@app.post("/mprefill_add")
+async def mprefill_add(request: Request) -> Response:
+    request_dict = await request.json()
+    await mprefill_add_prefill(request_dict)
+    ret = {"text": 'test'}
+    return JSONResponse(ret)
+    
+@app.post("/mprefill_execute")
+async def mprefill_execute(request: Request) -> Response:
+    request_dict = await request.json()
+    background_task_future = asyncio.ensure_future(mprefill_exec_prefill(request_dict))
+    ret = {"text": 'test'}
+    return JSONResponse(ret)
+
+#background threads
+@app.post("/init_mdecode")
+async def init_mdecode(request: Request, background_tasks: BackgroundTasks) -> Response:
+    """Generate completion for the request, containing a list of prompts.
+
+    The request should be a JSON object with the following fields:
+    - prompts: the prompt to use for the generation.
+    - stream: whether to stream the results or not.
+    - other fields: the sampling parameters (See `SamplingParams` for details).
+    """
+    request_dict = await request.json()
+    
+    request_ids = request_dict.pop("request_ids")
+    prompts = request_dict.pop("prompts")
+    output_lens = request_dict.pop("output_lens")
+    stream = request_dict.pop("stream", False)
+    sampling_params_list = []
+    for i in range(len(prompts)):
+        sampling_params = SamplingParams(**request_dict)
+        sampling_params_list.append(sampling_params)
+    results_generator = engine.generate_prefill(prompts=prompts, output_lens=output_lens, request_ids=request_ids, sampling_params=sampling_params_list, status="")
+    decode_event.set()
+    
+    # background_task_future = asyncio.ensure_future(init_mdecode_prefill(request_dict))
+    
+    # background_tasks.add_task(init_mdecode_prefill(request_dict))
+    # thread = threading.Thread(target=init_mdecode_prefill, args=(request_dict))
+    # thread.start()
+    # prompts = request_dict.pop("prompt")
+    # output_lens = request_dict.pop("output_lens")
+    
+    
+    # stream = request_dict.pop("stream", False)
+    # sampling_params_list = []
+    # for i in range(len(prompts)):
+    #     sampling_params = SamplingParams(**request_dict)
+    #     sampling_params_list.append(sampling_params)
+    
+    # # # request_id = random_uuid()
+    
+    # results_generator = engine.mul_generate(prompts, output_lens, sampling_params_list)
+
+    # # Streaming case
+    # async def stream_results() -> AsyncGenerator[bytes, None]:
+    #     async for request_output in results_generator:
+    #         prompt = request_output.prompt
+    #         text_outputs = [
+    #             prompt + output.text for output in request_output.outputs
+    #         ]
+    #         ret = {"text": text_outputs}
+    #         yield (json.dumps(ret) + "\0").encode("utf-8")
+
+    # async def abort_request() -> None:
+    #     await engine.abort(request_id)
+
+    # if stream:
+    #     background_tasks = BackgroundTasks()
+    #     # Abort the request if the client disconnects.
+    #     background_tasks.add_task(abort_request)
+    #     return StreamingResponse(stream_results(), background=background_tasks)
+
+    # # Non-streaming case
+    # final_output = None
+    # async for request_output in results_generator:
+    #     if await request.is_disconnected():
+    #         # Abort the request if the client disconnects.
+    #         await engine.abort(request_id)
+    #         return Response(status_code=499)
+    #     final_output = request_output
+
+    # assert final_output is not None
+    # prompt = final_output.prompt
+    # text_outputs = [prompt + output.text for output in final_output.outputs]
+    # ret = {"text": text_outputs}
+    print("init_mdecode return ")
+    ret = {"text": 'test'}
+    return JSONResponse(ret)
+
+@app.post("/generate")
+async def generate(request: Request) -> Response:
+    """Generate completion for the request.
+
+    The request should be a JSON object with the following fields:
+    - prompt: the prompt to use for the generation.
+    - stream: whether to stream the results or not.
+    - other fields: the sampling parameters (See `SamplingParams` for details).
+    """
+    request_dict = await request.json()
+    prompt = request_dict.pop("prompt")
+    stream = request_dict.pop("stream", False)
+    sampling_params = SamplingParams(**request_dict)
+    request_id = random_uuid()
+    results_generator = engine.generate(prompt, sampling_params, request_id)
+
+    # Streaming case
+    async def stream_results() -> AsyncGenerator[bytes, None]:
+        async for request_output in results_generator:
+            prompt = request_output.prompt
+            text_outputs = [
+                prompt + output.text for output in request_output.outputs
+            ]
+            ret = {"text": text_outputs}
+            yield (json.dumps(ret) + "\0").encode("utf-8")
+
+    async def abort_request() -> None:
+        await engine.abort(request_id)
+
+    if stream:
+        background_tasks = BackgroundTasks()
+        # Abort the request if the client disconnects.
+        background_tasks.add_task(abort_request)
+        return StreamingResponse(stream_results(), background=background_tasks)
+
+    # Non-streaming case
+    final_output = None
+    async for request_output in results_generator:
+        if await request.is_disconnected():
+            # Abort the request if the client disconnects.
+            await engine.abort(request_id)
+            return Response(status_code=499)
+        final_output = request_output
+
+    assert final_output is not None
+    prompt = final_output.prompt
+    text_outputs = [prompt + output.text for output in final_output.outputs]
+    ret = {"text": text_outputs}
+    return JSONResponse(ret)
+
+def start_server(port):
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="127.0.0.1")
+    parser.add_argument("--port", type=int, default=port)
+    parser = AsyncEngineArgs.add_cli_args(parser)
+    args = parser.parse_args()
+
+    engine_args = AsyncEngineArgs.from_cli_args(args)
+    engine = AsyncLLMEngine.from_engine_args(engine_args)
+    uvicorn.run(app,
+            host=args.host,
+            port=args.port,
+            log_level="debug",
+            timeout_keep_alive=TIMEOUT_KEEP_ALIVE)
+
+if __name__ == "__main__":
+    process1 = multiprocessing.Process(target=start_server, args=(7001,))
+    process2 = multiprocessing.Process(target=start_server, args=(7002,))
+    # 启动进程
+    process1.start()
+    process2.start()
+    
+    process1.join()
+    process2.join()
