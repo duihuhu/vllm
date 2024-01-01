50c3532f8f3f9c4724cce53ff9a39c7a5b67b105
duihuhu <duihuhu@163.com>
2024-01-01 16:46:41 +0800

    debug add global scheduler and monitor

---
+++ examples/api_server_c_decode_thread_with_monitor.py
@@ -0,0 +1,119 @@
+import argparse
+import json
+from typing import AsyncGenerator
+
+from fastapi import BackgroundTasks, FastAPI, Request
+from fastapi.responses import JSONResponse, Response, StreamingResponse
+import uvicorn
+
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.engine.async_llm_engine import AsyncLLMEngine
+from vllm.sampling_params import SamplingParams
+from vllm.utils import random_uuid
+import asyncio
+import threading
+import time
+import mmap
+import os
+# import multiprocessing
+# manager = multiprocessing.Manager()
+
+TIMEOUT_KEEP_ALIVE = 5  # seconds.
+TIMEOUT_TO_PREVENT_DEADLOCK = 1  # seconds.
+app = FastAPI()
+
+mdecode_status = "init_mdecode_prefill"
+ 
+decode_event = threading.Event()
+
+dp_md = 'mdispatcher_to_mdecode.txt'
+
+if not os.path.isfile(dp_md):
+    # create initial file
+    with open(dp_md, "w+b") as fd:
+        fd.write(b'\x00\x00\x00\x00\x00\x00\x00\x00')
+
+# @app.post("/notify_mdecode")
+def notify_mdecode():
+    global decode_event
+    global mdecode_status
+    hex_char = b'\x0F'
+    # 判断内存
+    prefill_nums = b'\x00'
+    request_num =  b'\x00'
+    
+    with open(dp_md, "r+b") as fd:
+        mm = mmap.mmap(fd.fileno(), 8, access=mmap.ACCESS_WRITE, offset=0)
+        # 读取内存映射区域的数据
+        while True:
+            if prefill_nums != mm[0:1]:
+                prefill_nums = mm[0:1]
+                request_num = int.from_bytes(mm[1:2], byteorder='big')
+                # print("mdecode recv signal from mprefill ", time.time())
+                engine.convert_reqs_status_by_num(request_num)
+                mdecode_status = "decode"
+                decode_event.set()
+
+def init_mdecode_prefill():
+    global mdecode_status
+    while True:
+        # print("init_mdecode_prefill ", mdecode_status)
+        if mdecode_status == "init_mdecode_prefill":
+            decode_event.wait()
+            results_generator = engine.generate_mdecode_prefill()
+        elif mdecode_status == "decode":
+            print("status is chanage, mdecode start exec decode", mdecode_status)
+            engine.generate_decode()
+        decode_event.clear()
+        decode_event.wait()
+        
+@app.on_event("startup")
+def startup_decode_event():
+    threading.Thread(target=init_mdecode_prefill, daemon=True).start()
+    threading.Thread(target=notify_mdecode, daemon=True).start()
+    threading.Thread(target=monitor_mdecode_info, args=(args.host, args.port) ,daemon=True).start()
+
+def monitor_mdecode_info(host, service_port):
+    global engine
+    machine_type = "decode"
+    while True:
+        num_labels = engine.monitor_mdecode_info()
+
+        
+#background threads
+@app.post("/init_mdecode")
+async def init_mdecode(request: Request) -> Response:
+    """init mdecode machine before execute. 
+    add request to queue
+    """
+    request_dict = await request.json()
+    
+    request_ids = request_dict.pop("request_ids")
+    prompts = request_dict.pop("prompts")
+    output_lens = request_dict.pop("output_lens")
+    stream = request_dict.pop("stream", False)
+    sampling_params_list = []
+    for i in range(len(prompts)):
+        sampling_params = SamplingParams(**request_dict)
+        sampling_params_list.append(sampling_params)
+    engine.add_request(prompts=prompts, output_lens=output_lens, request_ids=request_ids, sampling_params=sampling_params_list)
+    decode_event.set()
+    print("init_mdecode return ")
+    ret = {"text": 'test'}
+    return JSONResponse(ret)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="localhost")
+    parser.add_argument("--port", type=int, default=8000)
+    parser = AsyncEngineArgs.add_cli_args(parser)
+    args = parser.parse_args()
+
+    engine_args = AsyncEngineArgs.from_cli_args(args)
+    engine = AsyncLLMEngine.from_engine_args(engine_args)
+
+    uvicorn.run(app,
+                host=args.host,
+                port=args.port,
+                log_level="debug",
+                timeout_keep_alive=TIMEOUT_KEEP_ALIVE)
