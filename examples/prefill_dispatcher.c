13f91ac3beebfd2c0fbc0fc37a79e81bf4497083
duihuhu <duihuhu@163.com>
2024-01-02 18:40:42 +0800

    debug add move send request id

--- examples/prefill_dispatcher.c
+++ examples/prefill_dispatcher.c
@@ -50,7 +50,7 @@ int main(void)
       // dispatcher_shared[1] = prefilled_request_num;
 
       memcpy(dispatcher_shared+(alread_num*35), mprefill_shared+(alread_num*35), 35);
-      printf("alread_num %d", alread_num);
+      printf("alread_num %d\n", alread_num);
       alread_num = alread_num + 1;
     }
   } 
