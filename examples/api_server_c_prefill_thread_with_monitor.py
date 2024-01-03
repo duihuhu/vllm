3cb181978836ab6ba7ca1b442ff8ac96fbd63b0f
duihuhu <duihuhu@163.com>
2024-01-03 20:42:23 +0800

    debug sorted and waiting queue in chunk runner

--- examples/api_server_c_prefill_thread_with_monitor.py
+++ examples/api_server_c_prefill_thread_with_monitor.py
@@ -117,8 +117,10 @@ async def mprefill_add_prefill(request_dict):
                                   chunkrunner.request_waiting[4]), key=lambda x: x[4])
         
         chunkrunner.request_waiting[0],  chunkrunner.request_waiting[1] ,  chunkrunner.request_waiting[2], chunkrunner.request_waiting[3] , chunkrunner.request_waiting[4] =zip(*sorted_request_waiting)
+        execute_time_end = time.time()
+        print("sort time  ", execute_time_end - execute_time)
         print("chunkrunner ", chunkrunner.request_waiting[4])
-        
+
         chunkrunner.add_requests_to_job_sequences(prompts_s = chunkrunner.request_waiting[3], prompt_token_ids_s = chunkrunner.request_waiting[1], 
                                                 sampling_params_s = chunkrunner.request_waiting[2], request_ids=chunkrunner.request_waiting[0], request_label=request_label)
         chunkrunner.request_waiting[0] = []
