9117aee04bb38e20e1a2e7e4dff4b8a61e6a4729
duihuhu <duihuhu@163.com>
2024-01-04 16:43:12 +0800

    debug produce data in decode machine

--- examples/api_server_c_decode_thread_with_monitor_test.py
+++ examples/api_server_c_decode_thread_with_monitor_test.py
@@ -5,7 +5,9 @@ from typing import AsyncGenerator
 from fastapi import BackgroundTasks, FastAPI, Request
 from fastapi.responses import JSONResponse, Response, StreamingResponse
 import uvicorn
+
 from transformers import AutoModelForCausalLM, PreTrainedTokenizerBase
+
 from vllm.engine.arg_utils import AsyncEngineArgs
 from vllm.engine.async_llm_engine import AsyncLLMEngine
 from vllm.sampling_params import SamplingParams
@@ -16,9 +18,10 @@ import time
 import mmap
 import os
 import requests
+from typing import List, Tuple
 import random
 from vllm.transformers_utils.tokenizer import get_tokenizer
-from typing import List, Tuple
+from vllm import LLM
 # import multiprocessing
 # manager = multiprocessing.Manager()
 
@@ -29,8 +32,6 @@ app = FastAPI()
 mdecode_status = "init_mdecode_prefill"
  
 decode_event = threading.Event()
-
-execute_event = threading.Event()
 
 dp_md = 'mdispatcher_to_mdecode.txt'
 
@@ -44,7 +45,7 @@ fd = open(dp_md, "r+b")
 mm = mmap.mmap(fd.fileno(), 35 * 1024, access=mmap.ACCESS_WRITE, offset=0)
 
 # @app.post("/notify_mdecode")
-def notify_mdecode(engine, num_prompts):
+def notify_mdecode():
     global decode_event
     global mdecode_status
     hex_char = b'\x0F'
@@ -53,7 +54,7 @@ def notify_mdecode(engine, num_prompts):
     request_num =  b'\x00'
     already_num = 0 
     # 读取内存映射区域的数据
-    
+
     while True:
         if prefill_nums != mm[(already_num*35+34):(already_num*35+35)]:
             # prefill_nums = mm[(already_num*35):(already_num*35+1)]
@@ -61,7 +62,7 @@ def notify_mdecode(engine, num_prompts):
             request_num = int.from_bytes(mm[(already_num*35):(already_num*35+1)], byteorder='big')
             request_id = mm[(already_num*35+1):(already_num*35+33)].decode("utf-8")
             label = int.from_bytes(mm[(already_num*35+33):(already_num*35+34)], byteorder='big')
-            # arrive_time = time.time()
+            arrive_time = time.time()
             # print("decode get data " , request_id, start, end, end-start)
             # print("mdecode recv signal from mprefill ", time.time())
             # print("request info ", request_id, request_num, label, time.time())
@@ -69,29 +70,31 @@ def notify_mdecode(engine, num_prompts):
             # engine.convert_reqs_status_by_num(request_num)
             # engine.convert_reqs_status(request_id)
             engine.convert_req_label_status(request_id, label)
-            # add_time = time.time()
-            # print("decode get data " , request_id, arrive_time, add_time, add_time-arrive_time)
+            add_time = time.time()
+            print("decode get data " , request_id, arrive_time, add_time, add_time-arrive_time)
 
             mdecode_status = "decode"
             already_num = already_num + 1
-            execute_event.set()
-            if already_num == num_prompts:
-                break
+            decode_event.set()
 
-def init_mdecode_prefill(engine):
+def init_mdecode_prefill():
     global mdecode_status
     while True:
         # print("init_mdecode_prefill ", mdecode_status)
         if mdecode_status == "init_mdecode_prefill":
             decode_event.wait()
             results_generator = engine.generate_mdecode_prefill()
-            break
-        # elif mdecode_status == "decode":
-        #     print("status is chanage, mdecode start exec decode", mdecode_status)
-        #     engine.generate_decode()
+        elif mdecode_status == "decode":
+            print("status is chanage, mdecode start exec decode", mdecode_status)
+            engine.generate_decode()
         decode_event.clear()
         decode_event.wait()
         
+# @app.on_event("startup")
+def startup_decode_event():
+    threading.Thread(target=init_mdecode_prefill, daemon=True).start()
+    threading.Thread(target=notify_mdecode, daemon=True).start()
+    threading.Thread(target=monitor_mdecode_info, args=(args.host, args.port) ,daemon=True).start()
 
 def post_monitor_request(monitor_url: str,
                       host: str,
@@ -125,6 +128,28 @@ def monitor_mdecode_info(host, service_port):
         post_mdecode_info(host, service_port, machine_type, num_labels)
         time.sleep(1000)
 
+
+#background threads
+@app.post("/init_mdecode")
+async def init_mdecode(request: Request) -> Response:
+    """init mdecode machine before execute. 
+    add request to queue
+    """
+    request_dict = await request.json()
+    
+    request_ids = request_dict.pop("request_ids")
+    prompts = request_dict.pop("prompts")
+    output_lens = request_dict.pop("output_lens")
+    stream = request_dict.pop("stream", False)
+    sampling_params_list = []
+    for i in range(len(prompts)):
+        sampling_params = SamplingParams(**request_dict)
+        sampling_params_list.append(sampling_params)
+    engine.add_request(prompts=prompts, output_lens=output_lens, request_ids=request_ids, sampling_params=sampling_params_list)
+    decode_event.set()
+    print("init_mdecode return ")
+    ret = {"text": 'test'}
+    return JSONResponse(ret)
 
 def sample_requests(
     dataset_path: str,
@@ -180,22 +205,18 @@ def sample_requests(
         print("choose req info ", req[1], req[2])
     return sampled_requests
 
-def post_request_id(request_ids
-                      ) -> requests.Response:
-    request_url = "http://127.0.0.1:8000/mdecode_requestIds"
-    headers = {"User-Agent": "request_id "}
-    pload = {
-        "request_ids": request_ids,
-    }
-    response = requests.post(request_url, headers=headers, json=pload)
-    
-def init_mdecode(engine, num_prompts):
-    """init mdecode machine before execute. 
-    add request to queue
-    """
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="localhost")
+    parser.add_argument("--port", type=int, default=8000)
+    parser = AsyncEngineArgs.add_cli_args(parser)
+    args = parser.parse_args()
+
+    engine_args = AsyncEngineArgs.from_cli_args(args)
+    engine = AsyncLLMEngine.from_engine_args(engine_args)
     tokenizer = get_tokenizer("/workspace/opt-13b/model/snapshots/e515202d1e7750da62d245fbccb2723b9c1790f5/")
-    sampled_requests = sample_requests("/workspace/ShareGPT_V3_unfiltered_cleaned_split.json", num_prompts, tokenizer)
-    request_ids = []
+    sampled_requests = sample_requests("/workspace/ShareGPT_V3_unfiltered_cleaned_split.json", 128, tokenizer)
+
     for prompt, _, output_len in sampled_requests:
         request_id = random_uuid()
         sampling_params = SamplingParams(
@@ -208,44 +229,42 @@ def init_mdecode(engine, num_prompts):
             max_tokens=output_len,
         )
         engine.engine.add_request(request_id, prompt, sampling_params)
-        request_ids.append(request_id)
-    post_request_id(request_ids)
-    decode_event.set()
-    print("init_mdecode return ")
-    ret = {"text": 'test'}
-    return JSONResponse(ret)
+    engine.generate_decode()
 
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--host", type=str, default="localhost")
-    parser.add_argument("--port", type=int, default=8000)
-    parser.add_argument("--num-prompts", type=int, default=128)
-    parser = AsyncEngineArgs.add_cli_args(parser)
-    args = parser.parse_args()
+    # uvicorn.run(app,
+    #             host=args.host,
+    #             port=args.port,
+    #             log_level="debug",
+    #             timeout_keep_alive=TIMEOUT_KEEP_ALIVE)
 
-    engine_args = AsyncEngineArgs.from_cli_args(args)
-    engine = AsyncLLMEngine.from_engine_args(engine_args)
-    
-    td_list= []
-    t1 = threading.Thread(target=init_mdecode_prefill, args=(engine,))
-    td_list.append(t1)
-    t2 = threading.Thread(target=notify_mdecode, args=(engine, args.num_prompts))
-    td_list.append(t2)
-    t3 = threading.Thread(target=monitor_mdecode_info, args=(args.host, args.port))
-    td_list.append(t3)
 
-    for td in td_list:
-        td.start()
+    # llm = LLM(
+    #     model="/workspace/opt-13b/model/snapshots/e515202d1e7750da62d245fbccb2723b9c1790f5/",
+    #     tokenizer="/workspace/opt-13b/model/snapshots/e515202d1e7750da62d245fbccb2723b9c1790f5/",
+    #     tensor_parallel_size=2,
+    #     seed=0,
+    #     max_num_seqs=256,
+    # )
 
-    init_mdecode(engine, args.num_prompts)
-    
-    while True:
-        if mdecode_status == "decode":
-            print("status is chanage, mdecode start exec decode", mdecode_status)
-            engine.generate_decode()
-        execute_event.clear()
-        execute_event.wait()
-        
-    for td in td_list:
-        td.join()
-    
+    # # Add the requests to the engine.
+    # for prompt, _, output_len in sampled_requests:
+    #     sampling_params = SamplingParams(
+    #         n=1,
+    #         # temperature=0.0 if use_beam_search else 1.0,
+    #         temperature=0.0,
+    #         top_p=1.0,
+    #         use_beam_search=False,
+    #         ignore_eos=True,
+    #         max_tokens=output_len,
+    #     )
+    #     # FIXME(woosuk): Do not use internal method.
+    #     llm._add_request(
+    #         prompt=prompt,
+    #         prompt_token_ids=None,
+    #         sampling_params=sampling_params,
+    #     )
+
+    # start = time.time()
+    # # FIXME(woosuk): Do use internal method.
+    # outputs = llm._run_engine(use_tqdm=False, split_two_phase=0)
+    # end = time.time()
