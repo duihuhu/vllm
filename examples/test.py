b539279055212ff5d2d9cedb0b41d9344e271d2c
duihuhu <duihuhu@163.com>
2024-01-09 15:40:04 +0800

    debug test for mixed model affect

--- examples/test.py
+++ examples/test.py
@@ -170,6 +170,7 @@ if __name__ == "__main__":
         input_positions_tensors.append(input_positions_tensor)
         input_chunkinputmetadata.append(chunkinputmetadata)
 
+    time.sleep(10)
     thread_small = threading.Thread(target = execute_small_model, args = (input_prompts,))
     thread_big = threading.Thread(target = execute_big_model, 
                                     args = (input_tokens_ids_tensors, 
