cf09d4ead4df5a546cd2c9eef794739fc8487336
duihuhu <duihuhu@163.com>
2024-01-12 23:13:19 +0800

    debug add convert role test

---
+++ examples/api_global_post_covert_role_request.py
@@ -0,0 +1,10 @@
+import requests
+import time
+def post_init_decode_prefill() -> requests.Response:
+    api_url = "http://127.0.0.1:8000/covert_mprefill_mdecode_exec" 
+    headers = {"User-Agent": "Test Client"}
+    start_time = time.time()
+    response = requests.post(api_url, headers=headers, stream=True)
+    end_time = time.time()
+    print("post_init_decode_prefill ", start_time, end_time)
+    return response
---
+++ examples/api_server_c_prefill_thread_with_monitor_with_convert_role.py
@@ -0,0 +1,154 @@
+import argparse
+import json
+from typing import AsyncGenerator
+
+from fastapi import BackgroundTasks, FastAPI, Request
+from fastapi.responses import JSONResponse, Response, StreamingResponse
+import uvicorn
+
+from vllm.engine.arg_utils import AsyncEngineArgs
+from vllm.engine.async_llm_engine import AsyncLLMEngine
+from vllm.sampling_params import SamplingParams
+from vllm.utils import random_uuid
+import asyncio
+import threading
+import time
+import mmap
+import os
+import multiprocessing
+import requests
+
+TIMEOUT_KEEP_ALIVE = 5  # seconds.
+TIMEOUT_TO_PREVENT_DEADLOCK = 1  # seconds.
+app = FastAPI()
+
+mp_dp = 'mprefill_to_mdispatcher.txt'
+
+if not os.path.isfile(mp_dp):
+    # create initial file
+    with open(mp_dp, "w+b") as fd:
+        fd.write(b'\x00\x00\x00\x00\x00\x00\x00\x00')
+
+mp_md_dp = 'mprefill_mdispatcher_to_mdecode_mdispatcher.txt'
+
+if not os.path.isfile(mp_md_dp):
+    # create initial file
+    with open(mp_md_dp, "w+b") as fd:
+        fd.write(b'\x00\x00\x00\x00\x00\x00\x00\x00')
+
+prefill_event = threading.Event()
+
+decode_event = threading.Event()
+
+def mprefill_exec_prefill():
+    with open(mp_dp, "r+b") as fd:
+        mm = mmap.mmap(fd.fileno(), 8, access=mmap.ACCESS_WRITE, offset=0)
+        prefill_nums = 0 
+        while True:
+            prefill_event.wait() 
+            prefill_nums = engine.mprefill_generate_prefill(mm, prefill_nums)
+            prefill_event.clear()
+            prefill_event.wait()     
+        mm.close()
+        fd.close()
+        
+async def mprefill_add_prefill(request_dict):
+    print("mprefill add prefill request ", time.time())
+    request_ids = request_dict.pop("request_ids")
+    prompts = request_dict.pop("prompts")
+    output_lens = request_dict.pop("output_lens")
+    stream = request_dict.pop("stream", False)
+    mprefill_status = request_dict.pop("mprefill_status")
+    sampling_params_list = []
+    for i in range(len(prompts)):
+        sampling_params = SamplingParams(**request_dict)
+        sampling_params_list.append(sampling_params)
+    # engine.add_request(prompts=prompts, output_lens=output_lens, request_ids=request_ids, sampling_params=sampling_params_list)
+    engine.add_mprefill_request(prompts=prompts, output_lens=output_lens, request_ids=request_ids, sampling_params=sampling_params_list)
+    prefill_event.set()
+
+@app.post("/mprefill_add")
+async def mprefill_add(request: Request) -> Response:
+    request_dict = await request.json()
+    await mprefill_add_prefill(request_dict)
+    ret = {"text": 'test'}
+    return JSONResponse(ret)
+
+def exec_mprefill_mdecode():
+    while True:
+        # print("init_mdecode_prefill ", mdecode_status)
+        decode_event.clear()
+        decode_event.wait()
+        end_time = time.time()
+        print("covert role end time ", end_time)
+        engine.generate_decode()
+
+@app.post("/covert_mprefill_mdecode_exec")
+async def covert_mprefill_mdecode_exec(request: Request) -> Response:
+    decode_event.set()
+    ret = {"text": 'covert complish'}
+    return JSONResponse(ret)
+
+
+@app.on_event("startup")
+def startup_decode_event():
+    threading.Thread(target=mprefill_exec_prefill, daemon=True).start()
+    threading.Thread(target=monitor_mprefill_info, args=(args.host, args.port) ,daemon=True).start()
+    
+    threading.Thread(target=exec_mprefill_mdecode, daemon=True).start()
+
+def post_monitor_request(monitor_url: str,
+                      host: str,
+                      service_port: int,
+                      machine_type: str, 
+                      unfinished_req: int ,
+                      unfinished_tokens: int
+                      ) -> requests.Response:
+    headers = {"User-Agent": "mprefill "}
+    timestamp = time.time()
+    pload = {
+        "host": host,
+        "service_port": service_port,
+        "machine_type": machine_type,
+        "unfinished_req": unfinished_req,
+        "unfinished_tokens": unfinished_tokens,
+        "timestamp":timestamp,
+    }
+    response = requests.post(monitor_url, headers=headers, json=pload)
+    
+    return response
+
+def post_mprefill_info(host, service_port, machine_type, unfinished_req, unfinished_tokens):
+    monitor_url = "http://127.0.0.1:9000/mprefill_monitor_report"
+    response = post_monitor_request(monitor_url, host, service_port, machine_type, unfinished_req, unfinished_tokens)
+    
+def monitor_mprefill_info(host, service_port):
+    global engine
+    machine_type = "prefill"
+    while True:
+        unfinished_req, unfinished_tokens = engine.monitor_mprefill_info()
+        print("unfinished_req, unfinished_tokens", unfinished_req, unfinished_tokens)
+        post_mprefill_info(host, service_port, machine_type, unfinished_req, unfinished_tokens)
+        time.sleep(0.2)
+    return
+    
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--host", type=str, default="localhost")
+    parser.add_argument("--port", type=int, default=8000)
+    parser = AsyncEngineArgs.add_cli_args(parser)
+    args = parser.parse_args()
+
+    engine_args = AsyncEngineArgs.from_cli_args(args)
+    engine = AsyncLLMEngine.from_engine_args(engine_args)
+    
+    # 创建一个新的进程
+    # process = multiprocessing.Process(target=monitor_prefill_info, args=(args.host,args.port,))
+    # # 启动进程
+    # process.start()
+
+    uvicorn.run(app,
+                host=args.host,
+                port=args.port,
+                log_level="debug",
+                timeout_keep_alive=TIMEOUT_KEEP_ALIVE)
