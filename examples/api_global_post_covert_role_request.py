a0fa780bb9b11f212be0dd8370539212382c6086
duihuhu <duihuhu@163.com>
2024-01-12 23:17:28 +0800

    debug add convert role test

--- examples/api_global_post_covert_role_request.py
+++ examples/api_global_post_covert_role_request.py
@@ -1,7 +1,7 @@
 import requests
 import time
 def post_init_decode_prefill() -> requests.Response:
-    api_url = "http://127.0.0.1:8000/covert_mprefill_mdecode_exec" 
+    api_url = "http://127.0.0.1:7001/covert_mprefill_mdecode_exec" 
     headers = {"User-Agent": "Test Client"}
     start_time = time.time()
     response = requests.post(api_url, headers=headers, stream=True)
