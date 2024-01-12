f2b7568ce3347a42f014cc7c547ea343b50e436a
duihuhu <duihuhu@163.com>
2024-01-12 21:01:27 +0800

    debug resource to sum

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -315,8 +315,8 @@ class Scheduler:
                             min_resource_need.append(t)
                             add = True
                     if add:
-                        # if min(min_resource_need) * len(min_resource_need) <= total_free_tokens:
-                        if sum(min_resource_need) <= total_free_tokens:
+                        if min(min_resource_need) * len(min_resource_need) <= total_free_tokens:
+                        # if sum(min_resource_need) <= total_free_tokens:
                             self.running.append(seq_group)
                             # print("add resource need ", min(min_resource_need) * len(min_resource_need), total_free_tokens)
                             # print(f"min is {min(min_resource_need)}, length is {len(min_resource_need)}", "total blocks is {total_free_tokens}")
