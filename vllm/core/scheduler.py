97cf7cc231ddabc8ccc9f3afd9112eb1f3d0ecc4
duihuhu <duihuhu@163.com>
2024-01-12 18:13:47 +0800

    debug resource

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -433,7 +433,10 @@ class Scheduler:
                         preempted.append(victim_seq_group)
                         self.expelled += 1
                         t_expelled += 1
-                        print(f"In ite {self.ite} this req has been expelled from running queue total {self.expelled}")
+                        
+                        num_free_gpu_blocks = self.block_manager.gpu_allocator.get_num_free_blocks()
+                        num_seqs = seq_group.num_seqs(status=SequenceStatus.RUNNING) 
+                        print(f"In ite {self.ite} this req has been expelled from running queue total {self.expelled}", num_free_gpu_blocks, num_seqs)
                     else:
                         # No other sequence groups can be preempted.
                         # Preempt the current sequence group.
@@ -441,7 +444,7 @@ class Scheduler:
                         preempted.append(seq_group) 
                         self.expelled += 1
                         t_expelled += 1
-                        print(f"In ite {self.ite} this req has been expelled from running queue total {self.expelled}")
+                        # print(f"In ite {self.ite} this req has been expelled from running queue total {self.expelled}")
                         break
                 else:
                     # Append new slots to the sequence group.
