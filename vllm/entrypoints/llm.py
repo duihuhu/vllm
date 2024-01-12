b31dab9a70b82b9b3e1478729a40c8a4d803087f
duihuhu <duihuhu@163.com>
2024-01-12 09:55:58 +0800

    debug for htest thrput bench

--- vllm/entrypoints/llm.py
+++ vllm/entrypoints/llm.py
@@ -179,13 +179,12 @@ class LLM:
             st2 = time.time()
             print(f"Start Decode at {st2}")
             while self.llm_engine.has_unfinished_requests():
-                step_outputs = self.llm_engine.step(banker = False, steps = steps)
-                #step_outputs = self.llm_engine.step(banker = True, steps = steps)
+                # step_outputs = self.llm_engine.step(banker = False, steps = steps)
+                step_outputs = self.llm_engine.step(banker = True, steps = steps)
                 steps += 1
                 for output in step_outputs:
                     if output.finished:
                         end = time.time()
-                        print("request end time ", end, output.outputs[0].finish_reason)
                         with open("/workspace/vllm/benchmarks/end_time.txt", 'a') as file:
                             file.write(f"req {output.request_id} end decode at {end} costs {end - st2} seconds\n")
                         
