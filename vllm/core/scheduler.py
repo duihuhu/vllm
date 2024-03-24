c1cd733f4954ec09a90969135a700f7f4fc01912
unknown <3328677832@qq.com>
2024-03-24 16:37:01 +0800

    debug1

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -229,9 +229,9 @@ class Scheduler:
 
         # Fix the current time.
         now = time.time()
-        while self.running_stay:
-             seq_group = self.running_stay.pop(0)
-             self.running.append(seq_group)
+        #while self.running_stay:
+        #     seq_group = self.running_stay.pop(0)
+        #     self.running.append(seq_group)
         
         # NOTE(woosuk): We prioritize the sequence groups in the RUNNING state
         # in order to minimize the preemption overheads.
@@ -325,10 +325,10 @@ class Scheduler:
                 self._append_slot(seq_group, blocks_to_copy)
                 running.append(seq_group)
                 # index = index + 1
-                if len(running) >= self.scheduler_config.max_num_seqs:
-                     while self.running:
-                         seq_group = self.running.pop(0)
-                         self.running_stay.append(seq_group)
+                #if len(running) >= self.scheduler_config.max_num_seqs:
+                #     while self.running:
+                #         seq_group = self.running.pop(0)
+                #         self.running_stay.append(seq_group)
         self.running = running
 
         # Swap in the sequence groups in the SWAPPED state if possible.
