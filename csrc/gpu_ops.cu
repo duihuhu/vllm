4ed6994e47b3075acd5165d491644ab523c09c58
duihuhu <duihuhu@163.com>
2024-03-26 19:44:02 +0800

    debug gs client and pd sperate

--- csrc/gpu_ops.cu
+++ csrc/gpu_ops.cu
@@ -138,10 +138,11 @@ int32_t CreateGlobalNcclComm(int32_t rank, int32_t NumDevice=8) {
     }
 
     // 关闭共享内存
-    if (close(shm_fd) == -1) {
-        perror("close");
-        exit(1);
-    }
+    // if (close(shm_fd) == -1) {
+    //     perror("close");
+    //     exit(1);
+    // }
+    std::cout << "Start init Global NCCL Comm Success" << std::endl;
 
     NCCLCHECK(ncclCommInitRank(&g_globalNcclComm, NumDevice, uniqueId ,rank));
 
