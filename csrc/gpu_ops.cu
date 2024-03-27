94ce932670b208ecb755b629409ceeee16ef20c1
duihuhu <duihuhu@163.com>
2024-03-27 16:17:33 +0800

    debug gpu ops

--- csrc/gpu_ops.cu
+++ csrc/gpu_ops.cu
@@ -89,10 +89,10 @@ int32_t CreateGlobalNcclComm(int32_t rank, int32_t NumDevice=8) {
 
         // 生成唯一ID
         ncclGetUniqueId(&uniqueId);
-        char *out = (char*)(&uniqueId);
-        for(int i=0; i<shmSize; ++i)
-            std::cout<<(out)[i];
-        std::cout<<std::endl;
+        // char *out = (char*)(&uniqueId);
+        // for(int i=0; i<shmSize; ++i)
+        //     std::cout<<(out)[i];
+        // std::cout<<std::endl;
         // 将唯一ID写入共享内存
         memcpy(shmaddr, &uniqueId, shmSize);
 
@@ -123,11 +123,11 @@ int32_t CreateGlobalNcclComm(int32_t rank, int32_t NumDevice=8) {
         // 从共享内存中读取唯一ID
         memcpy(&uniqueId, shmaddr, shmSize);
 
-        char *out = (char*)(&uniqueId);
-        std::cout<<std::endl;
-        for(int i=0; i<shmSize; ++i)
-            std::cout<<out[i];
-        std::cout<<std::endl;
+        // char *out = (char*)(&uniqueId);
+        // std::cout<<std::endl;
+        // for(int i=0; i<shmSize; ++i)
+        //     std::cout<<out[i];
+        // std::cout<<std::endl;
 
         // 解除映射
         if (munmap(shmaddr, shmSize) == -1) {
@@ -146,10 +146,10 @@ int32_t CreateGlobalNcclComm(int32_t rank, int32_t NumDevice=8) {
     NCCLCHECK(ncclCommInitRank(&g_globalNcclComm, NumDevice, uniqueId ,rank));
 
     // 删除共享内存对象
-    if (shm_unlink(shmName) == -1) {
-        perror("shm_unlink");
-        exit(1);
-    }
+    // if (shm_unlink(shmName) == -1) {
+    //     perror("shm_unlink");
+    //     exit(1);
+    // }
 
     std::cout << "Create Global NCCL Comm Success" << std::endl;
 
