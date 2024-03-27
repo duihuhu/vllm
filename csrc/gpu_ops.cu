d74a5cac7e9a5c2146d88cb851f97d290d81cef6
duihuhu <duihuhu@163.com>
2024-03-27 08:39:38 +0800

    debug gpu ops

--- csrc/gpu_ops.cu
+++ csrc/gpu_ops.cu
@@ -101,6 +101,9 @@ int32_t CreateGlobalNcclComm(int32_t rank, int32_t NumDevice=8) {
         // 生成唯一ID
         ncclGetUniqueId(&uniqueId);
 
+        for(int i=0; i<shmSize; ++i)
+            std::cout<<shmaddr[i];
+        std::cout<<std::endl;
         // 将唯一ID写入共享内存
         memcpy(shmaddr, &uniqueId, shmSize);
 
@@ -130,6 +133,11 @@ int32_t CreateGlobalNcclComm(int32_t rank, int32_t NumDevice=8) {
 
         // 从共享内存中读取唯一ID
         memcpy(&uniqueId, shmaddr, shmSize);
+        
+        std::cout<<std::endl;
+        for(int i=0; i<shmSize; ++i)
+            std::cout<<(&uniqueId)[i];
+        std::cout<<std::endl;
 
         // 解除映射
         if (munmap(shmaddr, shmSize) == -1) {
