b45e98ec7612d04a9f00170b2317813be79971e7
duihuhu <duihuhu@163.com>
2024-03-27 21:12:36 +0800

    debug gpu ops

--- vllm/entrypoints/api_client.py
+++ vllm/entrypoints/api_client.py
@@ -82,7 +82,7 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--host", type=str, default="localhost")
     parser.add_argument("--port", type=int, default=8000)
-    parser.add_argument("--n", type=int, default=4)
+    parser.add_argument("--n", type=int, default=1)
     parser.add_argument("--prompt", type=str, default="San Francisco is a")
     parser.add_argument("--stream", action="store_true")
     args = parser.parse_args()
