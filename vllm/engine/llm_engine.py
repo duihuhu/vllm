4b3790fb0884b67e501a3f698fba4d8f52d333ba
duihuhu <duihuhu@163.com>
2024-04-02 20:44:27 +0800

    debug test for prefix cache

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -449,7 +449,9 @@ class LLMEngine:
             prompt_token_ids[:prefix_pos], lora_request.lora_int_id
             if lora_request else 0) if prefix_pos is not None else None
 
-        print("prefix ",  prefix)
+        if prefix:
+            for bk in prefix.block_table:
+                print(bk.block_number, bk.block_size)
         # Create the sequence group.
         seq_group = SequenceGroup(request_id, [seq], sampling_params,
                                   arrival_time, lora_request, prefix)
