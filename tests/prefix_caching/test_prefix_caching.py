b1e0df1606e535ba7f5a0810168de0f5eb11e447
duihuhu <duihuhu@163.com>
2024-04-02 20:21:29 +0800

    debug test for prefix cache

--- tests/prefix_caching/test_prefix_caching.py
+++ tests/prefix_caching/test_prefix_caching.py
@@ -28,7 +28,7 @@ def test_prefix_caching(
     llm = LLM(model=model)
     # -1 since the last token can change when concatenating prompts.
     prefix_pos = len(llm.llm_engine.tokenizer.encode(prefix)) - 1
-    prompts = [prefix + prompt for prompt in example_prompts]
+    prompts = [prefix + prompt for prompt in example_prompts] * 100
     print("prompts ", prompts)
     sampling_params = SamplingParams(temperature=0.0, max_tokens=max_tokens)
     t1=time.time()
