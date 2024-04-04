1cd944365be08264f910909fbeb5c6152415166b
duihuhu <duihuhu@163.com>
2024-04-04 17:28:15 +0800

    debug add decode transfer data to prefill

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -194,11 +194,12 @@ class Scheduler:
         self.send_transfering[seq_group.request_id] = seq_group
     
     #todo check free_seq
+    #del_send_transfering: 分配block失败的时候删除
     def del_send_transfering(self, request_id: str) -> None:
         # Delete sequence groups to the send  transfering map 
         if request_id in self.send_transfering:
             seq = self.send_transfering[request_id].get_seqs()[0]
-            self.free_seq(seq)
+            # self.free_seq(seq)
             del self.send_transfering[request_id]
     
     def get_send_transfering(self, request_id: str) -> None:
@@ -690,11 +691,12 @@ class Scheduler:
             passed_delay = True
         return passed_delay
 
+    #kv缓存传输完了
     def _check_tranfer_finished_req(self) -> None:
         for request_id in self.send_finished_req_ids[:]:
             seq_group = self.send_transfering[request_id]
             seq = seq_group.get_seqs()[0]
-            self.free_seq(seq)
+            # self.free_seq(seq)
             del self.send_transfering[request_id]
             self.send_finished_req_ids.remove(request_id)
 
