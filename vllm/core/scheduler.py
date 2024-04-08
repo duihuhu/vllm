abcf72fa329a532df0ff8c86adc5de278f4fa054
duihuhu <duihuhu@163.com>
2024-04-08 10:16:43 +0800

    debug add decode transfer data to prefill

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -700,6 +700,17 @@ class Scheduler:
             # self.free_seq(seq)
             del self.send_transfering[request_id]
             self.send_finished_req_ids.remove(request_id)
+            
+            #should free or swap to cpu blocks
+            block_table = self.block_manager.block_tables[seq.seq_id]
+            max_full_block = seq.get_len() // self.block_manager.block_size - 1
+            new_block_table = []
+            for i in range(max_full_block):
+                if block_table[i].computed:
+                    new_block_table.append(block_table[i])
+                else:
+                    self.block_manager.gpu_allocator.free(block_table[i])
+            self.block_manager.block_tables[seq.seq_id] = block_table
 
         for request_id in self.recv_finished_req_ids[:]:
             seq_group = self.recv_transfering[request_id]
@@ -707,3 +718,4 @@ class Scheduler:
                 self.running.append(seq_group)
             del self.recv_transfering[request_id]
             self.recv_finished_req_ids.remove(request_id)
+            self.block_manager.mark_blocks_as_computed(seq_group=seq_group)
