e04b4c0aaa1ee070692b7196c08689daa13baad2
duihuhu <duihuhu@163.com>
2024-04-08 11:31:03 +0800

    debug enable prefix

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -247,16 +247,17 @@ class ModelRunner:
                 block_offset = i % self.block_size
                 slot = block_number * self.block_size + block_offset
                 slot_mapping.append(slot)
-
+            
+        print("_prepare_prompt start ")
+        print("seq_data ", seq_data)
+        print("computed_len ", computed_len)
+        print("input_tokens,  prefill_end, start_idx , computed_block_nums, token_chunk_size ", input_tokens, prefill_end, start_idx, computed_block_nums, token_chunk_size)
+        
         max_subquery_len = max(subquery_lens)
         max_prompt_len = max(prompt_lens)
         num_prompt_tokens = len(input_tokens)
         assert max_subquery_len > 0
         
-        print("_prepare_prompt start ")
-        print("seq_data ", seq_data)
-        print("computed_len ", computed_len)
-        print("input_tokens,  prefill_end, start_idx , computed_block_nums, token_chunk_size ", input_tokens, prefill_end, start_idx, computed_block_nums, token_chunk_size)
         print("input_positions ", input_positions)
         print("slot_mapping ", slot_mapping)
         print("lora_index_mapping ", lora_index_mapping)
