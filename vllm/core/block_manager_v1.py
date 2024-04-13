c2bf0a19440c4ab90568582fca016f7e5ca9960e
duihuhu <duihuhu@163.com>
2024-04-13 10:39:24 +0800

    [cache cpu debug] add cached kv blocks and kv_block_tables for transfer kv blocks

--- vllm/core/block_manager_v1.py
+++ vllm/core/block_manager_v1.py
@@ -718,6 +718,7 @@ class BlockSpaceManagerV1(BlockSpaceManager):
     def compute_full_blocks_in_seq(self, seq: Sequence):
         if seq.seq_id not in self.block_tables:
             return
+        print("seq.get_len() ", seq.get_len())
         if seq.get_len() % self.block_size == 0:
             max_full_block = seq.get_len() // self.block_size - 1
                         
