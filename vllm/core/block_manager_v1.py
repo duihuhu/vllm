20da36f69e8a9265ec0e2890a32b3e2632aeb2e3
duihuhu <duihuhu@163.com>
2024-04-15 22:04:54 +0800

    [no cache allocator]

--- vllm/core/block_manager_v1.py
+++ vllm/core/block_manager_v1.py
@@ -507,11 +507,14 @@ class BlockSpaceManagerV1(BlockSpaceManager):
                 block = self.gpu_allocator.allocate()
                 # Set the reference counts of the token blocks.
                 block.ref_count = seq_group.num_seqs()
-            if (not in_hbm and in_mem):
-                block_table.append(gpu_block)
+            if self.enable_caching:
+                if (not in_hbm and in_mem):
+                    block_table.append(gpu_block)
+                else:
+                    block_table.append(block)
             else:
                 block_table.append(block)
-        
+                
         block_number_mapping = {
             cpu_block.block_number: gpu_block.block_number
             for cpu_block, gpu_block in mapping.items()
