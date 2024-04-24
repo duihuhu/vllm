52d3e8608c086c2a1adc174c61b3ecce306fcb2a
duihuhu <duihuhu@163.com>
2024-04-24 21:40:08 +0800

    debug add radix tree for token cache reuse

--- vllm/core/block_manager_v1.py
+++ vllm/core/block_manager_v1.py
@@ -300,13 +300,13 @@ class BlockSpaceManagerV1(BlockSpaceManager):
         import time
         start = time.time()
         if seq.last_node.parent == None:
-            prefix_len, last_node = self.gpu_allocator.insert_radix_cache(seq.data.get_tensor_token_ids(),
+            prefix_len, last_node = self.gpu_allocator.insert_radix_cache(tensor_token_ids,
                                                                           block_table[:num_prompt_blocks])
         else:
             # print("seq.last_node ", seq.last_node)
             # print("seq.last_node data ",  seq.data.get_tensor_token_ids()[seq.prefix_len:])
             # print("seq.last_node block_table ",  block_table[seq.prefix_len:num_prompt_blocks])
-            prefix_len, last_node = self.gpu_allocator.insert_radix_cache_on_node(seq.last_node, seq.data.get_tensor_token_ids()[seq.prefix_len:], block_table[seq.prefix_len:num_prompt_blocks])
+            prefix_len, last_node = self.gpu_allocator.insert_radix_cache_on_node(seq.last_node, tensor_token_ids[seq.prefix_len:], block_table[seq.prefix_len:num_prompt_blocks])
         seq.prefix_len = seq.prefix_len + prefix_len
         seq.last_node = last_node
         
