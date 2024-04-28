7de34d8f5572ab6d0e911408af0b80176ec81261
duihuhu <duihuhu@163.com>
2024-04-28 17:31:00 +0800

    debug uncomment model time static

--- vllm/worker/worker.py
+++ vllm/worker/worker.py
@@ -222,7 +222,7 @@ class Worker:
         output = self.model_runner.execute_model(seq_group_metadata_list,
                                                  self.gpu_cache)
         end = time.time()
-        print("model execute end time ", (end - start) * 1000 )
+        # print("model execute end time ", (end - start) * 1000 )
         return output
 
     def add_lora(self, lora_request: LoRARequest) -> bool:
