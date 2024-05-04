6ce59b7c0a95e1ee4b8e98ddfb91a6cef19e25ae
duihuhu <duihuhu@163.com>
2024-05-04 16:30:22 +0800

    debug for only match prefix

--- vllm/core/block_manager_v1.py
+++ vllm/core/block_manager_v1.py
@@ -328,8 +328,7 @@ class BlockSpaceManagerV1(BlockSpaceManager):
             # print("seq.last_node data ",  seq.data.get_radix_token_ids()[seq.prefix_len:])
             # print("seq.last_node block_table ",  block_table[seq.prefix_len:num_prompt_blocks])
             if s_prefix_len < num_prompt_blocks:
-                print("seq.last_node ", seq.last_node.value)
-                prefix_len, last_node = self.gpu_allocator.insert_radix_cache_on_node(seq.last_node, radix_token_ids[s_prefix_len:], block_table[s_prefix_len:num_prompt_blocks])
+                prefix_len, last_node = self.gpu_allocator.insert_radix_cache_on_node(seq.last_node, radix_token_ids, block_table)
                 seq.prefix_len = seq.prefix_len + prefix_len
                 seq.last_node = last_node
                 # Assign the block table for each sequence.
