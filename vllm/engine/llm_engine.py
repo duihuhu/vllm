457c9539053c90491713967d66f5017bdf445c50
duihuhu <duihuhu@163.com>
2024-05-07 00:24:06 +0800

    debug update radix tree when finished

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -601,6 +601,8 @@ class LLMEngine:
             prefix_info, last_matched_len = self.scheduler.block_manager.gpu_allocator.insert_radix_cache_on_node(seq.last_node.parent, radix_token_ids[(seq.prefix_len-seq.last_matched_len):-1], block_table[(seq.prefix_len-seq.last_matched_len):])
             print("after update radix tree ", radix_token_ids)
             self.scheduler.block_manager.gpu_allocator.radix_cache.pretty_print()
+            print("after update radix tree ", seq.prefix_len, prefix_info[0])
+
             seq.prefix_len = seq.prefix_len + prefix_info[0]
             seq.last_node = prefix_info[1] 
             seq.last_matched_len = last_matched_len
