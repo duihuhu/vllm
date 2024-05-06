053bf1791a8c914f853d63354085aef90ce8d082
duihuhu <duihuhu@163.com>
2024-05-06 19:25:08 +0800

    debug update radix tree when finished

--- vllm/core/block_manager_v1.py
+++ vllm/core/block_manager_v1.py
@@ -335,18 +335,20 @@ class BlockSpaceManagerV1(BlockSpaceManager):
         if seq.last_node == self.gpu_allocator.radix_cache.root_node \
             or seq.last_node.parent == self.gpu_allocator.radix_cache.root_node:
             if seq.last_node == self.gpu_allocator.radix_cache.root_node:
-                print("radix_token_ids ", pre_prefix_len, seq.last_matched_len, 
-                    seq.last_node.children.keys())
+                # print("radix_token_ids ", pre_prefix_len, seq.last_matched_len, 
+                #     seq.last_node.children.keys())
                 prefix_len, last_node = self.gpu_allocator.insert_radix_cache_on_node(seq.last_node, radix_token_ids[(pre_prefix_len-seq.last_matched_len):], block_table[(pre_prefix_len-seq.last_matched_len):])
             else:
-                print("radix_token_ids ", pre_prefix_len, seq.last_matched_len, 
-                    seq.last_node.children.keys(), seq.last_node.parent.children.keys())
+                # print("radix_token_ids ", pre_prefix_len, seq.last_matched_len, 
+                #     seq.last_node.children.keys(), seq.last_node.parent.children.keys())
                 prefix_len, last_node = self.gpu_allocator.insert_radix_cache_on_node(seq.last_node.parent, radix_token_ids[(pre_prefix_len-seq.last_matched_len):], block_table[(pre_prefix_len-seq.last_matched_len):])
             seq.prefix_len = prefix_len
             seq.last_node = last_node
             # print("allocate ", radix_token_ids, seq.prefix_len, prefix_len)
         else:
-            if pre_prefix_len < num_prompt_blocks:
+            if pre_prefix_len <= num_prompt_blocks:
+                print("radix_token_ids ", pre_prefix_len, seq.last_matched_len, 
+                    seq.last_node.children.keys(), seq.last_node.parent.children.keys())
                 prefix_len, last_node = self.gpu_allocator.insert_radix_cache_on_node(seq.last_node.parent, radix_token_ids[(pre_prefix_len-seq.last_matched_len):], block_table[(pre_prefix_len-seq.last_matched_len):])
                 seq.prefix_len = seq.prefix_len + prefix_len
                 seq.last_node = last_node
