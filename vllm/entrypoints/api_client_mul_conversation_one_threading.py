a75d24f7ac9ac297f40569861fcf1f4504269351
duihuhu <duihuhu@163.com>
2024-05-06 17:49:32 +0800

    debug update radix tree when finished

--- vllm/entrypoints/api_client_mul_conversation_one_threading.py
+++ vllm/entrypoints/api_client_mul_conversation_one_threading.py
@@ -194,7 +194,7 @@ def post_request_and_get_response(args, prompts_set):
             str(start_time[0]) + ", " + str(start_time[1]) + ", " + 
             str(end_time[0])   + ", " + str(end_time[1]))
 
-        print("history_value ", history_value)
+        # print("history_value ", history_value)
                     # waiting_time = output_len * waiting_time_per_token / 1000
                     # time.sleep(waiting_time)
     # return True    
@@ -235,8 +235,8 @@ if __name__ == "__main__":
         pre_time = pre_time + interval
         reqs_interval.append(pre_time)
 
-    # warmup(args, datasets[args.session:(args.session+1)])
+    warmup(args, datasets[args.session:(args.session+1)])
     
     # print("reqs_interval ", reqs_interval)
-    main(args, datasets[9:args.session], reqs_interval)
+    main(args, datasets[:args.session], reqs_interval)
     # asyncio.run(main(args, datasets[:args.session], reqs_interval))
