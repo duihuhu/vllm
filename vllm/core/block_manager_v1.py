46409630a2dc52cfd4c0067561ca0b5188a73376
duihuhu <duihuhu@163.com>
2024-05-06 23:05:41 +0800

    debug update radix tree when finished

--- vllm/core/block_manager_v1.py
+++ vllm/core/block_manager_v1.py
@@ -351,6 +351,7 @@ class BlockSpaceManagerV1(BlockSpaceManager):
         else:
             if pre_prefix_len <= num_prompt_blocks:
                 print("radix_token_ids ", pre_prefix_len, seq.last_matched_len, seq.last_node.parent.children.keys())
+                print(self.gpu_allocator.radix_cache.pretty_print())
                 prefix_info, last_matched_len = self.gpu_allocator.insert_radix_cache_on_node(seq.last_node.parent, \
                     radix_token_ids[(pre_prefix_len-seq.last_matched_len):], block_table[(pre_prefix_len-seq.last_matched_len):])
                 seq.prefix_len = seq.prefix_len + prefix_info[0]
