e0ab9a450b29d5c1b084a19766ff24bf11eb67a0
duihuhu <duihuhu@163.com>
2024-05-08 21:27:02 +0800

    debug add aysnc gs

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -291,7 +291,9 @@ class Scheduler:
     def fetch_prefilled_seq_groups(self) -> List[SequenceGroup]:
         prefilled_seq_groups = []
         while self.running:
-            prefilled_seq_groups.append(self.running.popleft())
+            seq_group = self.running[0]
+            print("seq group ", seq_group.request_id)
+            prefilled_seq_groups.append(seq_group)
         return prefilled_seq_groups
     
     def get_num_unfinished_seq_groups(self) -> int:
