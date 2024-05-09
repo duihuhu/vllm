4c23d9d3fe4ded5dd341533a598baed8e16e3258
duihuhu <duihuhu@163.com>
2024-05-09 22:21:56 +0800

    debug for agg and dissagg

--- vllm/core/block_manager_v1.py
+++ vllm/core/block_manager_v1.py
@@ -386,7 +386,7 @@ class BlockSpaceManagerV1(BlockSpaceManager):
         block_table = self.block_tables[seq.seq_id]
         # If we need to allocate a new physical block
         if len(block_table) < len(logical_blocks):
-            print("allocate new physical ")
+            # print("allocate new physical ")
             # Currently this code only supports adding one physical block
             assert len(block_table) == len(logical_blocks) - 1
 
