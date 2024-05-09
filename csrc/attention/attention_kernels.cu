087bfd96e2f6b0770a3a884de25209048f566113
duihuhu <duihuhu@163.com>
2024-05-09 20:49:12 +0800

    debug scheduler to compare with agg

--- csrc/attention/attention_kernels.cu
+++ csrc/attention/attention_kernels.cu
@@ -706,12 +706,6 @@ void paged_attention_v1_launcher(
     case 32:                                                          \
       CALL_V1_LAUNCHER(T, CACHE_T, 32, IS_FP8_E5M2_KV_CACHE);         \
       break;                                                          \
-    case 64:                                                          \
-      CALL_V1_LAUNCHER(T, CACHE_T, 64, IS_FP8_E5M2_KV_CACHE);         \
-      break;                                                          \
-    case 128:                                                          \
-      CALL_V1_LAUNCHER(T, CACHE_T, 128, IS_FP8_E5M2_KV_CACHE);         \
-      break;                                                          \
     case 256:                                                          \
       CALL_V1_LAUNCHER(T, CACHE_T, 256, IS_FP8_E5M2_KV_CACHE);         \
       break;                                                          \
@@ -907,12 +901,6 @@ void paged_attention_v2_launcher(
       break;                                                                \
     case 32:                                                                \
       CALL_V2_LAUNCHER(T, CACHE_T, 32, IS_FP8_E5M2_KV_CACHE);               \
-      break;                                                                \
-    case 64:                                                                \
-      CALL_V2_LAUNCHER(T, CACHE_T, 64, IS_FP8_E5M2_KV_CACHE);               \
-      break;                                                                \
-    case 128:                                                                \
-      CALL_V2_LAUNCHER(T, CACHE_T, 128, IS_FP8_E5M2_KV_CACHE);               \
       break;                                                                \
     case 256:                                                                \
       CALL_V2_LAUNCHER(T, CACHE_T, 256, IS_FP8_E5M2_KV_CACHE);               \
