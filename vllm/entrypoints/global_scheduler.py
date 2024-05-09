512595fd3d613e1c8630cacc3c4725c9a74e1e18
duihuhu <duihuhu@163.com>
2024-05-09 13:21:46 +0800

    debug add block size

--- vllm/entrypoints/global_scheduler.py
+++ vllm/entrypoints/global_scheduler.py
@@ -142,9 +142,9 @@ async def add_request(request: Request) -> Response:
                         #         "request_id": res['request_id'], 
                         #         "token_ids": res['prompt_token_ids'] + res['prefilled_token_id'],
                         #     }
-                        pkv_response = await forward_request_to_prefill(res, cfg.forward_eprefill_res_kv_url % (cfg.eprefill_host, cfg.eprefill_port))
-                        for pkv_res in get_streaming_response(pkv_response):
-                            await forward_request_to_decode(pkv_res, cfg.forward_edecode_res_kv_url  % (cfg.edecode_host, cfg.edecode_port))
+                        # pkv_response = await forward_request_to_prefill(res, cfg.forward_eprefill_res_kv_url % (cfg.eprefill_host, cfg.eprefill_port))
+                        # for pkv_res in get_streaming_response(pkv_response):
+                        #     await forward_request_to_decode(pkv_res, cfg.forward_edecode_res_kv_url  % (cfg.edecode_host, cfg.edecode_port))
                     
                     yield (json.dumps(res) + "\0").encode("utf-8")
                 n = n + 1
