84c13870f067204dd468babc2cb960a217242c69
duihuhu <duihuhu@163.com>
2024-05-09 09:40:45 +0800

    debug scheduler to compare with agg

--- vllm/worker/worker.py
+++ vllm/worker/worker.py
@@ -269,11 +269,8 @@ class Worker:
         # If there is no input, we don't need to execute the model.
         if num_seq_groups == 0:
             return {}
-        start_time = time.time()
         output = self.model_runner.execute_model(seq_group_metadata_list,
                                                  self.gpu_cache)
-        end_time = time.time()
-        print("model execute time ", end_time-start_time)
         # swap_finished_req_ids = self.cache_engine.check_finished_events()
         # return (output, swap_finished_req_ids)
         return output
