9429b716f92b6787b39607ff65b8281e512b8587
duihuhu <duihuhu@163.com>
2024-05-10 16:39:32 +0800

    debug test for disagg session

--- vllm/core/scheduler.py
+++ vllm/core/scheduler.py
@@ -699,7 +699,7 @@ class Scheduler:
         for request_id in self.send_finished_req_ids[:]:
             seq_group = self.send_transfering[request_id]
             seq = seq_group.get_seqs()[0]
-            # self.free_seq(seq)
+            self.free_seq(seq)
             del self.send_transfering[request_id]
             self.send_finished_req_ids.remove(request_id)
             
