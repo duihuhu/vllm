2a7463b41d3d6bc2a4a915c347c6ca435f603a6a
duihuhu <duihuhu@163.com>
2024-05-10 21:09:04 +0800

    debug merge radix tree

--- vllm/sequence.py
+++ vllm/sequence.py
@@ -122,10 +122,13 @@ class SequenceData:
         # The number of tokens that are computed (that run against the model).
         self._num_computed_tokens = 0
 
+        self.radix_token_id = tuple(prompt_token_ids)
+        
     def append_token_id(self, token_id: int, logprob: float) -> None:
         self.output_token_ids.append(token_id)
         self.cumulative_logprob += logprob
-
+        
+        self.radix_token_id = self.radix_token_id + (token_id,)
     def get_len(self) -> int:
         return len(self.output_token_ids) + len(self.prompt_token_ids)
 
@@ -138,6 +141,9 @@ class SequenceData:
     def get_token_ids(self) -> List[int]:
         return self.prompt_token_ids + self.output_token_ids
 
+    def get_radix_token_ids(self):
+        return self.radix_token_id
+    
     def get_num_computed_tokens(self) -> int:
         """Return the number of prefill tokens that are already computed."""
         return self._num_computed_tokens
