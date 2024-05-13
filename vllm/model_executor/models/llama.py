44305ebc6ab0e153571e9f5e34f7dd129dc1bf3e
duihuhu <duihuhu@163.com>
2024-05-13 23:06:23 +0800

    debug add aysnc gs

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -260,13 +260,13 @@ class LlamaModel(nn.Module):
         attn_metadata: AttentionMetadata,
         inputs_embeds: Optional[torch.Tensor] = None,
     ) -> torch.Tensor:
-        torch.cuda.synchronize()
-        start_time = time.time()
+
         if inputs_embeds is not None:
             hidden_states = inputs_embeds
         else:
             hidden_states = self.get_input_embeddings(input_ids)
-
+        torch.cuda.synchronize()
+        start_time = time.time()
         residual = None
         for i in range(len(self.layers)):
             layer = self.layers[i]
@@ -277,10 +277,11 @@ class LlamaModel(nn.Module):
                 attn_metadata,
                 residual,
             )
-        hidden_states, _ = self.norm(hidden_states, residual)
         torch.cuda.synchronize()
         end_time = time.time()
-        print("LlamaModel forward " , end_time-start_time)
+        print("LlamaModel forward layer " , end_time-start_time)
+        hidden_states, _ = self.norm(hidden_states, residual)
+
         return hidden_states
 
 
