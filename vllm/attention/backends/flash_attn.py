ee11ec9c6c8872eb00e059bbf548a56dfeacc4d3
duihuhu <duihuhu@163.com>
2024-05-13 18:52:27 +0800

    debug add aysnc gs

--- vllm/attention/backends/flash_attn.py
+++ vllm/attention/backends/flash_attn.py
@@ -15,7 +15,7 @@ from vllm.attention.backends.abstract import (AttentionBackend, AttentionImpl,
 from vllm.attention.ops.paged_attn import (PagedAttention,
                                            PagedAttentionMetadata)
 
-
+import time
 class FlashAttentionBackend(AttentionBackend):
 
     @staticmethod
@@ -222,6 +222,9 @@ class FlashAttentionImpl(AttentionImpl):
                 )
         else:
             # Decoding run.
+            torch.cuda.synchronize()
+
+            start_time = time.time()
             output = PagedAttention.forward_decode(
                 query,
                 key_cache,
@@ -234,6 +237,9 @@ class FlashAttentionImpl(AttentionImpl):
                 self.scale,
                 self.alibi_slopes,
             )
+            torch.cuda.synchronize()
+            end_time = time.time()
+            print("forward_decode " , end_time-start_time)
 
         # Reshape the output tensor.
         return output.view(num_tokens, hidden_size)
