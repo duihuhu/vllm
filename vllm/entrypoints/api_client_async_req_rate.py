1710ed1a6e5839d3a90a0624a1e74c1f25bcbb00
duihuhu <duihuhu@163.com>
2024-05-15 21:15:34 +0800

    debug breakdown disagg and transfer

--- vllm/entrypoints/api_client_async_req_rate.py
+++ vllm/entrypoints/api_client_async_req_rate.py
@@ -109,9 +109,9 @@ if __name__ == "__main__":
     # Sample the requests.
     tokenizer = AutoTokenizer.from_pretrained(tokenizer_path)
 
-    reqs = sample_requests("/home/jovyan/hucc/datasets/ShareGPT_V3_unfiltered_cleaned_split.json", tokenizer, args.num_requests)
+    # reqs = sample_requests("/home/jovyan/hucc/datasets/ShareGPT_V3_unfiltered_cleaned_split.json", tokenizer, args.num_requests)
     
-
+    reqs = [(None, [1,1,1,1], 4, 6), (None, [2,2,2,2], 4, 6)]
     asyncio.run(main(args, reqs))
 
     
