0c01ca06a156897117d0b85dd718dfc2eabdea88
duihuhu <duihuhu@163.com>
2024-05-15 14:35:45 +0800

    debug breakdown disagg and transfer

--- vllm/worker/cache_engine.py
+++ vllm/worker/cache_engine.py
@@ -166,7 +166,7 @@ class CacheEngine:
     def recv_blocks(self, channel: str, request_id: str, src_blocks: List[int], opposite_rank: int) -> None:      
         if channel not in self.recv_streams:
             self.recv_streams[channel] = torch.cuda.Stream(device=torch.cuda.current_device())
-        print("recv_blocks ", len(src_blocks))
+        # print("recv_blocks ", len(src_blocks))
         gpu_cache = [(kv_cache[0], kv_cache[1]) for kv_cache in self.gpu_cache]
         with torch.cuda.stream(self.recv_streams[channel]):
             gpu_ops.RecvBlocksRemote(gpu_cache, src_blocks, self.cache_size_per_block, opposite_rank)
