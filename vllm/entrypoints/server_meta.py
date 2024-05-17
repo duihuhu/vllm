99ec3168245b1b778f5bce3026d7f98a266eea81
duihuhu <duihuhu@163.com>
2024-05-17 23:15:03 +0800

    debug test layer pass data

--- vllm/entrypoints/server_meta.py
+++ vllm/entrypoints/server_meta.py
@@ -86,65 +86,6 @@ class InferResults:
             "end_time": self.end_time,    
         }
 
-
-    def __init__(
-        self,
-        request_id,
-        opp_ranks,
-        prompt_token_ids,
-        sampling_params,
-    ) -> None:
-        self.request_id = request_id
-        self.opp_ranks = opp_ranks
-        self.prompt_token_ids = prompt_token_ids
-        self.sampling_params = sampling_params
-    
-    def __json__(self) -> Dict:
-        prompt_logprobs = []
-        if self.prompt_logprobs != None:
-            for d in self.prompt_logprobs:
-                if d == None:
-                    prompt_logprobs.append(d)
-                    continue
-                serialized_d = {}
-                for key, value in d.items():
-                    serialized_value = value.__json__()
-                    serialized_d[key] = serialized_value
-                prompt_logprobs.append(serialized_d)
-    
-        output_logprobs = []
-        if self.output_logprobs != None:
-            for d in self.output_logprobs:
-                if d == None:
-                    output_logprobs.append(d)
-                    continue
-                serialized_d = {}
-                for key, value in d.items():
-                    serialized_value = value.__json__()
-                    serialized_d[key] = serialized_value
-                output_logprobs.append(serialized_d)
-    
-        return {
-            "request_id": self.request_id,
-            "opp_ranks": self.opp_ranks,
-            "prompt_token_ids": self.prompt_token_ids,
-            'prompt_logprobs': prompt_logprobs,
-            "prefilled_token_id": self.prefilled_token_id,
-            "output_logprobs": output_logprobs,
-            "cumulative_logprob": self.cumulative_logprob,
-            # "speculate_token_ids": self.speculate_token_ids,
-            "sampling_params": self.sampling_params.__json__(),
-            "index": self.index,
-            "texts": self.texts,
-            "finished": self.finished,
-            "ttft": self.ttft,
-            "jct": self.jct,
-            "tbt": self.tbt,
-            "n": self.n,
-            "start_time": self.start_time,
-            "end_time": self.end_time,    
-        }
-
 class QueryBlocks:
     def __init__(self, request_id , prompt_token_ids, sampling_params, global_ranks) -> None:
         self.request_id = request_id
