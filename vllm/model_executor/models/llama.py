671eb2144b239e48369c27fa5f4a91064192b35e
duihuhu <duihuhu@163.com>
2024-05-20 14:23:23 +0800

    debug test layer pass data

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -282,7 +282,7 @@ class LlamaModel(nn.Module):
             print("blocks_to_send_remote ", blocks_to_send_remote)
             if blocks_to_send_remote:
                 for request_id, block_info in blocks_to_send_remote.items():
-                    gpu_ops.SendBlocksOnLayer((kv_caches[i][0], kv_caches[i][1]), block_info[-1], 16, block_info[-2])
+                    gpu_ops.SendBlocksOnLayer((kv_caches[i][0], kv_caches[i][1]), block_info[-1], 163840, block_info[-2])
         hidden_states, _ = self.norm(hidden_states, residual)
         return hidden_states
 
