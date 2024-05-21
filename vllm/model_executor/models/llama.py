fda7a720e2b1275c8df117076abb87ad7dfa9af2
duihuhu <duihuhu@163.com>
2024-05-21 10:17:49 +0800

    debug gpu prefill time

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -281,7 +281,6 @@ class LlamaModel(nn.Module):
                 residual,
             )
             if blocks_to_send_remote:
-                t1 = time.time()
                 use_blocks_to_send_remote = blocks_to_send_remote[0]
                 cache_engine =  blocks_to_send_remote[1]
                 for request_id, block_info in use_blocks_to_send_remote.items():
@@ -291,10 +290,11 @@ class LlamaModel(nn.Module):
                                 channel = str(block_info[1][0])
                         else:
                             channel =  channel + "_" + str(block_info[1][i])
+                    t1 = time.time()
                     with torch.cuda.stream(cache_engine.send_streams[channel]):
                         gpu_ops.SendBlocksOnLayer((kv_caches[i][0], kv_caches[i][1]), block_info[-1], cache_engine.cache_size_per_block, block_info[-2][0]) #todo destRank
-                t2 = time.time()
-                print("forward ", t2-t1)
+                    t2 = time.time()
+                    print("forward ", t2-t1)
         hidden_states, _ = self.norm(hidden_states, residual)
         return hidden_states
 
