767dbae8af7d0eb917d3d67fe6a3c4711cacc08b
duihuhu <duihuhu@163.com>
2024-05-24 17:07:27 +0800

    debug keep serial req, del request id send

--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -504,15 +504,15 @@ class _AsyncLLMEngine(LLMEngine):
         
         # print("send_finished_tasks ", send_finished_tasks)
         # print("recv_finished_tasks ", recv_finished_tasks)
-        for worker_finished_tasks in send_finished_tasks:
+        for worker_finished_tasks in finished_tasks:
             send_finished_tasks = worker_finished_tasks[0]
-            real_send_finished_req_ids, real_recv_finished_req_ids = self.send_kv_trans_scheduler.add_finished_tasks(worker_finished_tasks)
+            real_send_finished_req_ids, real_recv_finished_req_ids = self.send_kv_trans_scheduler.add_finished_tasks(send_finished_tasks)
             if real_send_finished_req_ids:
                 self.scheduler.add_send_finished(real_send_finished_req_ids)
 
-        for worker_finished_tasks in recv_finished_tasks:
+        for worker_finished_tasks in finished_tasks:
             recv_finished_tasks = worker_finished_tasks[1]
-            real_send_finished_req_ids, real_recv_finished_req_ids = self.recv_kv_trans_scheduler.add_finished_tasks(worker_finished_tasks)
+            real_send_finished_req_ids, real_recv_finished_req_ids = self.recv_kv_trans_scheduler.add_finished_tasks(recv_finished_tasks)
             if real_recv_finished_req_ids:
                 self.scheduler.add_recv_finished(real_recv_finished_req_ids)
 
