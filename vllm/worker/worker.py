709f8a83b44256e94617a1256c8ee440432f74fa
duihuhu <duihuhu@163.com>
2024-05-30 17:51:55 +0800

    debug init vllm_v0.4.0_disagg_mlayer_radix_cthread

--- vllm/worker/worker.py
+++ vllm/worker/worker.py
@@ -197,8 +197,11 @@ class Worker:
         trans_config = TransConfig(self.model_config.get_head_size(),self.model_config.get_num_kv_heads(self.parallel_config), self.cache_config.cache_dtype, self.cache_engine.dtype)
         gpu_cache = [(kv_cache[0], kv_cache[1]) for kv_cache in self.gpu_cache]
         print("type gpu_cache " , type(gpu_cache))
-        self.trans_worker = trans_ops.TransWorker(trans_config, gpu_cache,  self.rank, self.local_rank, self.nccl_local_rank)
-        
+        self.trans_workera = trans_ops.TransWorker(trans_config, self.rank, self.local_rank, self.nccl_local_rank)
+        print("type trans_workera ")
+        self.trans_workerb = trans_ops.TransWorker(gpu_cache)
+        print("type trans_workerbs ")
+
     def warm_up_model(self) -> None:
         if not self.model_config.enforce_eager:
             self.model_runner.capture_model(self.gpu_cache)
