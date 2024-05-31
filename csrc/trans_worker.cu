ce004fc7b257650f4089a9308226a1c2d0a8c1d1
duihuhu <duihuhu@163.com>
2024-05-31 08:51:01 +0800

    debug init vllm_v0.4.0_disagg_mlayer_radix_cthread

--- csrc/trans_worker.cu
+++ csrc/trans_worker.cu
@@ -24,17 +24,17 @@ void TransWorker::worker() {
     
     while (true) {
         if(!task_queue.empty()) {
-            std::cout<<"task_queue is not empty ";
+            // std::cout<<"task_queue is not empty ";
             auto task = task_queue.pop_front();
             TaskType task_type = task.type;
             auto task_meta = task.meta;
             switch (task_type) {
                 case TaskType::TRANSFER_SEND_BLOCKS:
-                    std::cout<<"task_queue is not empty send " <<std::endl;
+                    // std::cout<<"task_queue is not empty send " <<std::endl;
                     trans_engine.send_blocks(task_meta.channel, task_meta.request_id, task.blocks, task.opposite_ranks[rank]);
                     break;
                 case TaskType::TRANSFER_RECV_BLOCKS:
-                    std::cout<<"task_queue is not empty recv " <<std::endl;
+                    // std::cout<<"task_queue is not empty recv " <<std::endl;
                     trans_engine.recv_blocks(task_meta.channel, task_meta.request_id, task.blocks, task.opposite_ranks[rank]);
                     break;
                 default:
@@ -45,7 +45,7 @@ void TransWorker::worker() {
         auto send_blocks_finished = trans_engine.check_send_finished_events();
         auto recv_blocks_finished = trans_engine.check_recv_finished_events();
         if (!send_blocks_finished.empty() || !recv_blocks_finished.empty()){
-            std::cout<<"task_queue is empty send " << send_blocks_finished.empty() << " recv " << recv_blocks_finished.empty()<<std::endl;
+            // std::cout<<"task_queue is empty send " << send_blocks_finished.empty() << " recv " << recv_blocks_finished.empty()<<std::endl;
             transfer_result_queue.push_back(std::make_pair(send_blocks_finished, recv_blocks_finished));
         }
     }
@@ -61,7 +61,7 @@ std::vector<std::pair<std::vector<TransferTaskMeta>, std::vector<TransferTaskMet
     std::vector<std::pair<std::vector<TransferTaskMeta>, std::vector<TransferTaskMeta>>> finished_tasks;
     while (!transfer_result_queue.empty())
     {
-        std::cout<<"transfer_result_queue is not empty ";
+        // std::cout<<"transfer_result_queue is not empty ";
         auto finished_task = transfer_result_queue.pop_front();
         finished_tasks.push_back(finished_task);
     }
--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -500,7 +500,7 @@ class _AsyncLLMEngine(LLMEngine):
                 print("worker_finished_tasks ", finished_tasks, worker_finished_tasks)
                 send_finished_tasks = worker_finished_tasks[0][0]
                 recv_finished_tasks = worker_finished_tasks[0][1]
-                print("send_finished_tasks, recv_finished_tasks ", send_finished_tasks, recv_finished_tasks)
+                # print("send_finished_tasks, recv_finished_tasks ", send_finished_tasks, recv_finished_tasks)
                 real_send_finished_req_ids = self.send_kv_trans_scheduler.add_finished_tasks(send_finished_tasks)
                 real_recv_finished_req_ids = self.recv_kv_trans_scheduler.add_finished_tasks(recv_finished_tasks)
                 if real_send_finished_req_ids:
