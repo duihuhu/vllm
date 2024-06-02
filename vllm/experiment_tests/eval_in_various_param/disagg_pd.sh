e9337d93d1627da3af2d2cb8b7258aebbf51576c
duihuhu <duihuhu@163.com>
2024-06-02 14:59:48 +0800

    debug add plt script

--- vllm/experiment_tests/eval_in_various_param/disagg_pd.sh
+++ vllm/experiment_tests/eval_in_various_param/disagg_pd.sh
@@ -27,11 +27,10 @@ input_lens=(64 128 256 512 1024 2048)
 output_lens=(16 32)
 request_rates=(3.2 6.4 12.8 25.6 51.2 102.4)
 num_requests=256
-mkdir ${dirname}
 for input_len in "${input_lens[@]}"; do
   for output_len in "${output_lens[@]}"; do
     for request_rate in "${request_rates[@]}"; do
-        output_file="${dirtype}/${name}_${input_len}_${output_len}_${request_rate}_${num_requests}.txt"
+        output_file="${dirtype}/${type}_${input_len}_${output_len}_${request_rate}_${num_requests}.txt"
         command="python3 ./vllm/entrypoints/api_client_async_req_rate_len.py --input-len $input_len --output-len $output_len --request-rate $request_rate --num-requests $num_requests"
         eval "$command" > "$output_file"
         echo "Sleeping for 10 seconds..."
