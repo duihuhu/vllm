a5f17194bfa36d811f9d3c0e083dfaeff17a688c
duihuhu <duihuhu@163.com>
2024-06-02 14:15:52 +0800

    debug add plt script

---
+++ vllm/experiment_tests/eval_in_various_param/pd.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+name="colocate"
+input_lens=(64 128 256 512 1024 2048)
+output_lens=(16 32 64)
+request_rates=(1.6 3.2 6.4 12.8 25.6 51.2)
+num_requests=256
+
+for input_len in "${input_lens[@]}"; do
+  for output_len in "${output_lens[@]}"; do
+    for request_rate in "${request_rates[@]}"; do
+        output_file="${name}_${input_len}_${output_len}_${request_rate}_${num_requests}.txt"
+        command="python3 ./vllm/entrypoints/api_client_async_req_rate_len.py --input-len $input_len --output-len $output_len --request-rate $request_rate --num-requests $num_requests"
+        echo "$command" | tee -a "$output_file"
+        eval "$command" | tee -a "$output_file"
+        echo "Sleeping for 10 seconds..."
+        sleep 10
+    done
+  done
+done
