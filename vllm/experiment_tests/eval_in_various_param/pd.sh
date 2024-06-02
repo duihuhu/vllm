8ebdf554b07d11abe84707b1f2eab0ffce5038ea
duihuhu <duihuhu@163.com>
2024-06-02 16:04:01 +0800

    debug add plt script

--- vllm/experiment_tests/eval_in_various_param/pd.sh
+++ vllm/experiment_tests/eval_in_various_param/pd.sh
@@ -1,17 +1,38 @@
 #!/bin/bash
-name="colocate"
+dirname="e2e_exp"
+type="colocate"
+dirtype="${dirname}/${type}"
+if [ -d "$dirtype" ]; then
+    rm -rf ${dirtype}
+    if [ $? -eq 0 ]; then
+        mkdir ${dirtype}
+        if [ $? -eq 0 ]; then
+            echo "Directory ${dirtype} created successfully."
+        else
+            echo "Error creating directory ${dirtype}."
+        fi
+    else
+        echo "Error deleting directory ${dirtype}."
+    fi
+else
+    mkdir  -p ${dirtype}
+    if [ $? -eq 0 ]; then
+        echo "Directory ${dirtype} created successfully."
+    else
+        echo "Error creating directory ${dirtype}."
+    fi
+fi
+
 input_lens=(64 128 256 512 1024 2048)
-output_lens=(16 32 64)
+output_lens=(16 32)
 request_rates=(1.6 3.2 6.4 12.8 25.6 51.2)
 num_requests=256
-
 for input_len in "${input_lens[@]}"; do
   for output_len in "${output_lens[@]}"; do
     for request_rate in "${request_rates[@]}"; do
-        output_file="${name}_${input_len}_${output_len}_${request_rate}_${num_requests}.txt"
+        output_file="${dirtype}/${type}_${input_len}_${output_len}_${request_rate}_${num_requests}.txt"
         command="python3 ./vllm/entrypoints/api_client_async_req_rate_len.py --input-len $input_len --output-len $output_len --request-rate $request_rate --num-requests $num_requests"
-        echo "$command" | tee -a "$output_file"
-        eval "$command" | tee -a "$output_file"
+        eval "$command" > "$output_file"
         echo "Sleeping for 10 seconds..."
         sleep 10
     done
