90f1607541539ad16d2c12612abcab886ca6082c
duihuhu <duihuhu@163.com>
2024-06-02 14:28:03 +0800

    debug add plt script

--- vllm/experiment_tests/eval_in_various_param/disagg_pd.sh
+++ vllm/experiment_tests/eval_in_various_param/disagg_pd.sh
@@ -10,8 +10,7 @@ for input_len in "${input_lens[@]}"; do
     for request_rate in "${request_rates[@]}"; do
         output_file="${name}_${input_len}_${output_len}_${request_rate}_${num_requests}.txt"
         command="python3 ./vllm/entrypoints/api_client_async_req_rate_len.py --input-len $input_len --output-len $output_len --request-rate $request_rate --num-requests $num_requests"
-        echo "$command" | tee -a "$output_file"
-        eval "$command" | tee -a "$output_file"
+        eval "$command" > "$output_file"
         echo "Sleeping for 10 seconds..."
         sleep 10
     done
