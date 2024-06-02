159c7931aa04089db0ab0d8c27aec21fbe29a5d0
duihuhu <duihuhu@163.com>
2024-06-02 23:14:38 +0800

    debug breakdown disagg under reqs

--- vllm/experiment_tests/other/trans_kv_reqs/disagg_bk_reqs.sh
+++ vllm/experiment_tests/other/trans_kv_reqs/disagg_bk_reqs.sh
@@ -31,7 +31,7 @@ for input_len in "${input_lens[@]}"; do
   for output_len in "${output_lens[@]}"; do
     for request_rate in "${request_rates[@]}"; do
         output_file="${dirtype}/${type}_${input_len}_${output_len}_${request_rate}_${num_requests}.txt"
-        command="python3 ./vllm/entrypoints/api_client_async_req_rate_len.py --input-len $input_len --output-len $output_len --request-rate $request_rate --num-requests $num_requests"
+        command="python3 ./vllm/global_scheduler/client/api_client_async_req_rate_len.py --input-len $input_len --output-len $output_len --request-rate $request_rate --num-requests $num_requests"
         eval "$command"
         sleep 2
         mv /home/jovyan/vllm/prefill_add_kv_request.txt ${dirtype}/${type}_${input_len}_${output_len}_${request_rate}_${num_requests}_prefill_add_kv_request.txt
