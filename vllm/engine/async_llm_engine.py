686e8c413e7b5b11a6a9db262235b06525e0140f
duihuhu <duihuhu@163.com>
2024-06-03 23:50:36 +0800

    debug add mul request query layer kv cache

--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -468,7 +468,7 @@ class _AsyncLLMEngine(LLMEngine):
                     # await self.send_prefilled_meta(processed_output_with_layer)
         if self.deploy_config.enable_layer:
             return processed_output_without_layer, processed_output_with_layer
-        return processed_output_without_layer
+        return processed_output_without_layer, []
 
     async def encode_request_async(
         self,
