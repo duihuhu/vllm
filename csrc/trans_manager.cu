9c54e4e3f5c4b1f642aa352f45fc2f15244f136e
duihuhu <duihuhu@163.com>
2024-06-07 11:12:33 +0800

    debug npnd build mul comm connection

--- csrc/trans_manager.cu
+++ csrc/trans_manager.cu
@@ -21,6 +21,7 @@ std::vector<char> TransManager::get_nccl_id(std::string dst_channel){
         std::cout << std::hex << (int)c << " ";
     }
     if(trans_workers.find(dst_channel) == trans_workers.end()){
+        TransWorker(cache_size_per_block, gpu_cache, rank, local_rank, nccl_local_rank);
         // trans_workers.emplace(std::make_pair(dst_channel, TransWorker(cache_size_per_block, gpu_cache, rank, local_rank, nccl_local_rank)));
         std::cout << "NCCL Unique ID get in C++";
     }else{
