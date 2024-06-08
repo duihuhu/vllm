b5fdb008e7674b961a0094d8a2be10f791b6f91e
duihuhu <duihuhu@163.com>
2024-06-08 17:06:28 +0800

    debug npnd build mul comm for layers

--- csrc/trans_engine.cu
+++ csrc/trans_engine.cu
@@ -233,6 +233,7 @@ std::vector<std::string> TransEngine::check_send_finished_comms_events() {
         }
         if (num_finished_events > 0) {
             request_ids_and_comms.erase(request_ids_and_comms.begin(), request_ids_and_comms.begin() + num_finished_events);
+            print("request_ids_and_comms len " , len(request_ids_and_comms))
         }
     }
     return send_blocks_finished;
