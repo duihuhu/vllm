bc719a0870e5667e3d1485685e9a0511bc02a067
duihuhu <duihuhu@163.com>
2024-06-11 19:15:09 +0800

    debug for radix tree

--- vllm/core/block_manager_v1.py
+++ vllm/core/block_manager_v1.py
@@ -348,7 +348,7 @@ class BlockSpaceManagerV1(BlockSpaceManager):
         block_table: BlockTable  = []
         if seq.data.prefix_blocks:
             block_table = seq.data.prefix_blocks
-        print("radix_manager_allocate block_table ", len(block_table))
+        print("radix_manager_allocate block_table ", len(block_table), " " ,is_kv_prepared)
         for idx in range(num_prompt_blocks - len(seq.data.prefix_blocks)):
             block = self.gpu_allocator.radix_manager_allocate()
             block_table.append(block)
