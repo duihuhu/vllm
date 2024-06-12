747c70043dcdcc086b0e3b23db2c6e7576b596ca
duihuhu <duihuhu@163.com>
2024-06-12 16:14:34 +0800

    debug add find scripts comms

--- vllm/experiment_tests/e2e/find_optimal_config_comm/by_req/disagg_pd_2comms.sh
+++ vllm/experiment_tests/e2e/find_optimal_config_comm/by_req/disagg_pd_2comms.sh
@@ -27,11 +27,12 @@ input_lens=(64 128 256 512 1024)
 output_lens=(16 32)
 request_rates=(10 20 40 80 160)
 num_requests=256
+gpu_ratio=0.88
 for input_len in "${input_lens[@]}"; do
   for output_len in "${output_lens[@]}"; do
     for request_rate in "${request_rates[@]}"; do
         output_file="${dirtype}/${type}_${input_len}_${output_len}_${request_rate}_${num_requests}.txt"
-        command="python3 ./vllm/global_scheduler/client/api_client_async_req_rate_len.py --input-len $input_len --output-len $output_len --request-rate $request_rate --num-requests $num_requests"
+        command="python3 ./vllm/global_scheduler/client/api_client_async_req_rate_len.py --input-len $input_len --output-len $output_len --request-rate $request_rate --num-requests $num_requests  --gpu-memory-utilization $gpu_ratio"
         eval "$command" > "$output_file"
         echo "Sleeping for 10 seconds..."
         sleep 10
