c34aabe47e73fcf5d026cbe2605ddf20b7edb1ca
duihuhu <duihuhu@163.com>
2024-06-13 09:13:06 +0800

    debug add find optimal layer config

--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -427,8 +427,8 @@ class _AsyncLLMEngine(LLMEngine):
             #             blocks_to_swap_out=cache_blocks_to_swap_out
             #         )
 
-        if self.deploy_config.enable_separate and self.deploy_config.role=="decoder":
-            print("req recv " , len(self.scheduler.meta_recv_finished), len(self.scheduler.decode_recv_finished), len(self.scheduler.kv_prepared_seq_group), len(self.scheduler.recv_transfering))
+        # if self.deploy_config.enable_separate and self.deploy_config.role=="decoder":
+        #     print("req recv " , len(self.scheduler.meta_recv_finished), len(self.scheduler.decode_recv_finished), len(self.scheduler.kv_prepared_seq_group), len(self.scheduler.recv_transfering))
         seq_group_metadata_list, scheduler_outputs, cached_seq_groups = self.scheduler.schedule()
 
         # if scheduler_outputs.is_empty():
