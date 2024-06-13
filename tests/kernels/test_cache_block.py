7cffc63d14dbe7650de076de00a59623b4c39be8
unknown <3328677832@qq.com>
2024-06-13 21:09:43 +0800

    debug2

--- tests/kernels/test_cache_block.py
+++ tests/kernels/test_cache_block.py
@@ -28,33 +28,79 @@ for _ in range(num_blocks):
 key_blocks_addresses = ops.tensor_for_caches_addresses(key_agg_blocks)
 value_blocks_addresses = ops.tensor_for_caches_addresses(value_agg_blocks)
 
+key_cache = []
+for _ in range(num_layers):
+    key_block_tensor2 = torch.empty(size = (num_blocks, num_kv_heads, head_size // x, block_size, x), 
+                                 dtype=torch.float16, 
+                                 device='cuda').uniform_(-1e-3, 1e-3)
+    key_cache.append(key_block_tensor2)
+
+value_cache = []
+for _ in range(num_layers):
+    value_block_tensor2 = torch.empty(size = (num_blocks, num_kv_heads, head_size // x, block_size, x), 
+                                 dtype=torch.float16, 
+                                 device='cuda').uniform_(-1e-3, 1e-3)
+    value_cache.append(value_block_tensor2)
+
 block_mapping = {0:2}
 
 block_size_in_bytes = key_agg_blocks[0].numel() * key_agg_blocks[0].element_size()
 
 #copy from keys to values -> simplize the test
+t1 = time.time()
 cache_ops.swap_blocks_agg(key_blocks_addresses, value_blocks_addresses, block_mapping, block_size_in_bytes)
+t2 = time.time()
+
+t3 = time.time()
+cache_ops.swap_blocks(key_cache, value_cache, block_mapping)
+t4 = time.time()
+
+print(f"swap_blocks_agg costs {t2-t1}, swap_blocks costs {t4-t3}")
 
 is_close = torch.allclose(key_agg_blocks[0], value_agg_blocks[2], atol=1e-3, rtol=1e-5)
 if is_close:
-    print("Pass")
+    print("Pass for Swap")
 else:
-    print("Error")
+    print("Error in Swap")
 
 block_mapping2 = {0:[3,4]}
+
+t5 = time.time()
 cache_ops.copy_blocks_agg(key_blocks_addresses, value_blocks_addresses, key_agg_blocks[0][0,0,:,0,:], 
-                          block_mapping2, num_layers, key_agg_blocks[0][0].numel())
+                          block_mapping2, num_layers, key_agg_blocks[0][0,:,:,:,:].numel())
+t6 = time.time()
+
+t7 = time.time()
+cache_ops.copy_blocks(key_cache, value_cache, block_mapping2)
+t8 = time.time()
+
+print(f"copy_blocks_agg costs {t6-t5}, copy_blocks costs {t8-t7}")
 
 is_close2 = torch.allclose(key_agg_blocks[0], value_agg_blocks[3], atol=1e-3, rtol=1e-5)
 is_close3 = torch.allclose(key_agg_blocks[0], value_agg_blocks[4], atol=1e-3, rtol=1e-5)
 if is_close2 and is_close3:
-    print("Pass2")
+    print("Pass for Copy")
 else:
-    print("Error2")
+    print("Error in Copy")
 
 
-key = torch.empty(size = (16, 10, 128), dtype=torch.float16, device='cuda').uniform_(-1e-3, 1e-3)
-value = torch.empty(size = (16, 10, 128), dtype=torch.float16, device='cuda').uniform_(-1e-3, 1e-3)
+key = torch.zeros(size = (16, 10, 128), dtype=torch.float16, device='cuda')
+value = torch.zeros(size = (16, 10, 128), dtype=torch.float16, device='cuda')
 slots = [i for i in range(16)]
+
+t9 = time.time()
 cache_ops.reshape_and_cache_agg(key, value, key_blocks_addresses, value_blocks_addresses, slots, "auto", 
                                 block_size, x, 0)
+t10 = time.time()
+
+t11 = time.time()
+cache_ops.reshape_and_cache(key, value, key_cache, value_cache, slots, "auto")
+t12 = time.time()
+
+print(f"reshape_and_cache_agg costs {t10-t9}, reshape_and_cache costs {t12-t11}")
+
+all_zero = torch.all(key_agg_blocks[0][0,:,:,:,:] == 0)
+if all_zero:
+    print("Pass for Store")
+else:
+    print("Error in Store")
