1dd371f65513d082e64c410454fd792557ab4445
duihuhu <duihuhu@163.com>
2024-06-17 21:14:01 +0800

    debug add allocate kv cache in d dram, and return swap use share cpu cache

--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -612,10 +612,9 @@ class _AsyncLLMEngine(LLMEngine):
             t2 = time.time()   
         for finished_tasks in finished_work_tasks:
             for worker_finished_tasks in finished_tasks:
-                print("worker_finished_tasks ", finished_work_tasks)
+                print("worker_finished_tasks ", worker_finished_tasks)
                 if worker_finished_tasks:
                     for worker_finished_task in worker_finished_tasks:
-                        print("worker_finished_tasks ", finished_tasks, worker_finished_tasks)
                         send_finished_tasks = []
                         swap_remote_finished_tasks = [] 
                         recv_finished_tasks = []
