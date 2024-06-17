abb33c7adc014d58abbc20366735d5b0d56b4d0b
duihuhu <duihuhu@163.com>
2024-06-17 08:26:26 +0800

    debug add pd req block test

--- vllm/entrypoints/server.py
+++ vllm/entrypoints/server.py
@@ -161,6 +161,7 @@ async def generate_decode(request: Request) -> Response:
     is_layer = payload.pop("is_layer")
     if not is_layer:
         request_id = payload.pop("request_id")
+        print("generate_decode ", request_id)
         opp_ranks = payload.pop("opp_ranks")
         prompt_token_ids = payload.pop("prompt_token_ids")
         prompt_logprobs = payload.pop("prompt_logprobs")
