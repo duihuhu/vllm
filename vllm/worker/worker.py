9629eb59db99200cb0612750a5025aca14adfb8a
duihuhu <duihuhu@163.com>
2024-06-17 11:44:19 +0800

    debug add share instance cpu cache  among all instances

--- vllm/worker/worker.py
+++ vllm/worker/worker.py
@@ -384,7 +384,7 @@ class Worker:
     
     def share_cpu_cache(self, global_ranks):
         self.deploy_config.set_global_ranks(global_ranks) 
-        channel = "_".join([str(rank) for rank in self.deploy_config.global_ranks()])
+        channel = "_".join([str(rank) for rank in self.deploy_config.global_ranks])
         # 将 Tensor 列表转换为 numpy 数组并计算每个 Tensor 的大小
         np_arrays = [tensor.cpu().numpy() for tensor in self.cpu_cache]
         self.tensor_sizes = [np_array.nbytes for np_array in np_arrays]
