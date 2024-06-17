2b3a3895c33faa699c09428828eacad8f24b8c22
duihuhu <duihuhu@163.com>
2024-06-17 21:14:43 +0800

    debug add allocate kv cache in d dram, and return swap use share cpu cache

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -691,7 +691,7 @@ class ModelRunner:
             execute_model_kwargs.update({"image_input": multi_modal_input})
         
         hidden_states = model_executable(**execute_model_kwargs)
-        torch.cuda.synchronize()
+        # torch.cuda.synchronize()
 
         logits = self.model.compute_logits(hidden_states, sampling_metadata)
         # Only perform sampling in the driver worker.
