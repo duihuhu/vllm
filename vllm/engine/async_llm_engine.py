6eecc36ec613e890685205d577e1969b93bb9104
duihuhu <duihuhu@163.com>
2024-06-20 20:58:33 +0800

    debug add allocate kv cache in d dram, and return swap use share cpu cache

--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -463,7 +463,8 @@ class _AsyncLLMEngine(LLMEngine):
                         self.radix_swap_scheduler.add_swap_task(swap_id)
             #TODO evict dram block
             evict_dram_nums = self.scheduler.evict_dram_num()
-
+            print("cpu blocks remain " , self.scheduler.block_manager.cpu_allocator.get_radix_num_free_blocks(),
+                  self.scheduler.block_manager.cpu_allocator.get_num_used_blocks(), evict_dram_nums)
             if evict_dram_nums:
                 evicted_nums = self.scheduler.evict_radix_tree(evict_nums=evict_dram_nums, device=Device.CPU)
                 print("evicted_nums ", evicted_nums)
