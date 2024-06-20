2442636cae26a067449f71b92bf51f244faf5be6
unknown <3328677832@qq.com>
2024-06-20 17:26:54 +0800

    cache_engine.py copy_agg debug

--- vllm/worker/cache_engine.py
+++ vllm/worker/cache_engine.py
@@ -303,7 +303,7 @@ class CacheEngine:
                  src_to_dsts: Dict[int, List[int]]) -> None:
         num_layers = self.gpu_cache[0][0].shape[0]
         numel_per_layer = self.gpu_cache[0][0].stride(0)
-        self.attn_backend.copy_blocks_agg(kv_cache_addresses, src_to_dsts, num_layers, numel_per_layer)
+        self.attn_backend.copy_blocks_agg(kv_cache_addresses, self.gpu_cache[0][0,0,0,0,0], src_to_dsts, num_layers, numel_per_layer)
 
     def wait_for_swap_out_events(self, wait_for_swap_out: List[str]) -> None:
         for key in wait_for_swap_out:
