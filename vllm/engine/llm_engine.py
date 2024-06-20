026f64cb1ee65ba05528e95505fe89c4a03a77b2
duihuhu <duihuhu@163.com>
2024-06-20 11:44:15 +0800

    debug add allocate kv cache in d dram, and return swap use share cpu cache

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -482,11 +482,11 @@ class LLMEngine:
         while self.scheduler.decode_waiting:
             seq_group = self.scheduler.decode_waiting[0][0]
             prefill_request_output = self.scheduler.decode_waiting[0][1]
-
             can_allocate = self.scheduler.block_manager.can_allocate(seq_group)
             #TODO there may has some issue
             if can_allocate == AllocStatus.OK or not self.deploy_config.enable_trans_to_dram:
-                if can_allocate == AllocStatus.OK:
+                is_hbm = self.scheduler.check_hbm_usage()
+                if can_allocate == AllocStatus.OK and not is_hbm:
                     seq_group.eprefill_host = prefill_request_output.eprefill_host
                     seq_group.eprefill_port = prefill_request_output.eprefill_port
                     seq_group.edecode_host = prefill_request_output.edecode_host
