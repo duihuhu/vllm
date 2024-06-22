57dbed1f961cf0fefc8c700154e2abe5fe23ad99
duihuhu <duihuhu@163.com>
2024-06-22 13:53:02 +0800

    debug add finished req into tree

--- vllm/engine/async_llm_engine.py
+++ vllm/engine/async_llm_engine.py
@@ -458,8 +458,9 @@ class _AsyncLLMEngine(LLMEngine):
             if evict_hbm_nums > 0:
                 t1 = time.time()
                 real_evicted_nums = self.scheduler.evict_radix_tree(evict_nums=evict_hbm_nums, device=Device.GPU)
-                t2 = time.time()
-                print("real evict_hbm_nums ", t2-t1, evict_hbm_nums, real_evicted_nums)
+                if real_evicted_nums > 0:
+                    t2 = time.time()
+                    print("real evict_hbm_nums ", t2-t1, evict_hbm_nums, real_evicted_nums)
                 
             # if is_hbm_evict:
                 # can_evicted_nodes, cpu_blocks = self.scheduler.get_evicted_blocks()
