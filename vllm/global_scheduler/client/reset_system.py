be5bdb3db4a6a8f83e6f181ea7f503b45c127086
duihuhu <duihuhu@163.com>
2024-06-23 00:15:37 +0800

    debug add reset system api script

---
+++ vllm/global_scheduler/client/reset_system.py
@@ -0,0 +1,18 @@
+import vllm.global_scheduler.entrypoints_config as cfg
+from typing import Dict, Set, List, Optional
+import requests
+import time
+def post_request(api_url, request_dict: Optional[Dict] = {}):
+    headers = {"User-Agent": "Test Client"}
+    resp = requests.post(api_url, headers=headers, json=request_dict)
+    return resp
+
+
+def reset_system(host, port):
+
+    creat_comm_api_url = cfg.create_comm_url % (host, port)
+    payload = {"reset":"reset"}
+    resp = post_request(creat_comm_api_url, payload)
+    return resp
+
+resp = reset_system(cfg.eprefill_host, 8082)
