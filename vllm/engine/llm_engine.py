e0d29964d435c5e356a3510764fa600689cd7a85
duihuhu <duihuhu@163.com>
2024-06-22 12:21:23 +0800

    debug add finished req

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -632,13 +632,6 @@ class LLMEngine:
         samples = outputs.samples
         parent_seqs = seq_group.get_seqs(status=SequenceStatus.RUNNING)
         existing_finished_seqs = seq_group.get_finished_seqs()
-        
-        if existing_finished_seqs:
-            print("existing_finished_seqs ", existing_finished_seqs)
-            if self.scheduler.block_manager.enable_radix_caching \
-                or (self.scheduler.block_manager.enable_radix_caching and self.deploy_config.enable_separate \
-                    and self.deploy_config.role == "decoder"):
-                self.radix_manager_update(seq_group)
                 
         parent_child_dict = {
             parent_seq.seq_id: []
@@ -680,6 +673,13 @@ class LLMEngine:
             self.detokenizer.decode_sequence_inplace(seq,
                                                      seq_group.sampling_params)
             self._check_stop(seq, seq_group.sampling_params)
+            
+            if seq.is_finished():
+                #enable_radix_caching in pd or enable_radix_caching in p/d's decoder, we should update radix tree
+                if self.scheduler.block_manager.enable_radix_caching \
+                    or (self.scheduler.block_manager.enable_radix_caching and self.deploy_config.enable_separate \
+                        and self.deploy_config.role == "decoder"):
+                    self.radix_manager_update(seq_group)
 
         # Non-beam search case
         if not seq_group.sampling_params.use_beam_search:
