55ae31fda0bb43434e7b4c635a3e8cc2a835caf4
unknown <3328677832@qq.com>
2024-06-22 20:45:03 +0800

    new test starts 1

--- benchmarks/test.py
+++ benchmarks/test.py
@@ -1,18 +1,18 @@
 import os
 
 # 定义参数范围
-input_lengths = [64, 128, 256, 512, 1024]
-output_lengths = [64, 128, 256, 512, 1024]
-use_agg_block_options = [True, False]
+input_lengths = [32, 64, 128, 256, 512, 1024, 2048, 4096]
+ratio = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+#use_agg_block_options = [True, False]
 
 # 基础命令模板
-base_command = "python3 benchmark_latency.py --input-len {x} --output-len {y} --use-agg-block {z} --file-name /home/jovyan/hhy/vllm-hhy/benchmarks/log_{n}_{x}_{y}.txt"
+base_command = "python3 benchmark_latency.py --input-len {x} --ratio {y} --use-agg-block --enable-prefix-caching --file-name /home/jovyan/hhy/vllm-hhy/benchmarks/log2_{x}_{y}.txt"
 
 # 遍历所有参数组合并生成命令
 for x in input_lengths:
-    for y in output_lengths:
-        for z in use_agg_block_options:
-            n = 1 if z else 0
-            command = base_command.format(x=x, y=y, z=z, n=n)
-            print(f"Executing: {command}")
-            os.system(command)
+    for y in ratio:
+        #for z in use_agg_block_options:
+        #    n = 1 if z else 0
+        command = base_command.format(x=x, y=y)
+        print(f"Executing: {command}")
+        os.system(command)
