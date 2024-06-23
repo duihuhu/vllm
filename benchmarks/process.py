65f34e64e8d0d1f3185d1c071f72ebc863be7759
unknown <3328677832@qq.com>
2024-06-23 21:30:46 +0800

    start2

--- benchmarks/process.py
+++ benchmarks/process.py
@@ -1,14 +1,21 @@
 root_path = "/home/jovyan/hhy/vllm-hhy/benchmarks/"
-dir_path = "logs2/"
-prefix_path = "log2_"
+dir_path = "logs5/"
+prefix_path = "log2b_"
 suffix_path = ".txt"
 total_len = [32,64,128,256,512,1024,2048,4080]
-ratio = [10,20,30,40,50,60,70,80,90,100]
+#ratio = [10,20,30,40,50,60,70,80,90,100]
 
 total_costs = []
 for tl in total_len:
     tl_costs = []
-    for r in ratio:
+    file_name = root_path + dir_path  + prefix_path + str(tl) + suffix_path
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        for i in range(len(lines)):
+            if i >= 3:
+                data = float(lines[i].strip().split(' ')[-1])
+                tl_costs.append(data)
+    '''for r in ratio:
         file_name = root_path + dir_path  + prefix_path + str(tl) + "_" + str(r) + suffix_path
         costs = []
         with open(file_name, 'r') as file:
@@ -19,5 +26,6 @@ for tl in total_len:
                     ed = float(lines[i+1].strip().split(' ')[-1])
                     costs.append(ed - st)
         tl_costs.append(sum(costs) / len(costs))
-    total_costs.append(tl_costs)
+    total_costs.append(tl_costs)'''
+    total_costs.append(sum(tl_costs) / len(tl_costs))
 print(total_costs)    
