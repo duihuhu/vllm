9e75cb8bf20fe424b4a338f5758a8aac5bd12303
duihuhu <duihuhu@163.com>
2024-06-23 09:24:51 +0800

    debug add reset system api script

--- benchmarks_flowserve/open_run_experiments_hucc.py
+++ benchmarks_flowserve/open_run_experiments_hucc.py
@@ -44,7 +44,7 @@ def execute_exp(deploy_type):
         os.makedirs(dirname)
 
     for i, request_rate in enumerate(request_rates):
-        command = f'python3 main.py --test-type {"open"} --dataset {dataset} --num-clients {request_rate} --num-requests {configs["num_requests"]}  --duration 10 '
+        command = f'python3 main.py --test-type {"open"} --dataset {dataset} --request-rate {request_rate} --num-requests {configs["num_requests"]}  --duration 10 '
         print(f'Running command: {configs["deploy_type"]}, {command}')
         os.system(f'{command}')
         time.sleep(5)
