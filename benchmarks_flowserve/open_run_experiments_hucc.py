86784d529b5b07e52fc7492ffbb766f2f229ba1d
duihuhu <duihuhu@163.com>
2024-06-23 09:20:40 +0800

    debug add reset system api script

--- benchmarks_flowserve/open_run_experiments_hucc.py
+++ benchmarks_flowserve/open_run_experiments_hucc.py
@@ -20,30 +20,35 @@ Each folder represents a figure that compares  the performance of the baselines
 and our method. Each .csv file represents one line in the figure.
 '''
 import os
-import pandas as pd
 import time
+import sys
 
-# Configurable parameters
-basename = 'end2end_exp_results'
-dataset = 'LooGLE' # ['ShareGPT', 'LooGLE', 'ReAct']
-configs = {
-    'type': 'disagg_ncache',
-    'num_requests': 1000,
-}
+def execute_exp(deploy_type):
+    # Configurable parameters
+    basename = 'end2end_exp_results'
+    dataset = 'LooGLE' # ['ShareGPT', 'LooGLE', 'ReAct']
+    configs = {
+        'num_requests': 1000,
+    }
+    configs['deploy_type'] = deploy_type
 
-if "disagg" in configs['type']:
-    request_rates = [0.2, 0.4, 0.6, 0.8] # x-axis 
-else:
-    request_rates= [0.1, 0.2, 0.3, 0.4] # x-axis 
+    if "disagg" in configs['deploy_type']:
+        request_rates = [0.2, 0.4, 0.6, 0.8] # x-axis 
+    else:
+        request_rates= [0.1, 0.2, 0.3, 0.4] # x-axis 
 
-# Derived parameters
-dirname = f'{basename}/{dataset}/{configs["type"]}'
+    # Derived parameters
+    dirname = f'{basename}/{dataset}/{configs["deploy_type"]}'
 
-if not os.path.exists(dirname):
-    os.makedirs(dirname)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
 
-for i, request_rate in enumerate(request_rates):
-    command = f'python3 main.py --test-type {"open"} --dataset {dataset} --num-clients {request_rate} --num-requests {configs["num_requests"]}  --duration 10 '
-    print(f'Running command: {configs['type']}, {command}')
-    os.system(f'{command}')
-    time.sleep(5)
+    for i, request_rate in enumerate(request_rates):
+        command = f'python3 main.py --test-type {"open"} --dataset {dataset} --num-clients {request_rate} --num-requests {configs["num_requests"]}  --duration 10 '
+        print(f'Running command: {configs['deploy_type']}, {command}')
+        os.system(f'{command}')
+        time.sleep(5)
+
+if __name__ == "__main__":
+    deploy_type = sys.argv[1]
+    execute_exp(deploy_type)
