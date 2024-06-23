af86996dbacbdcbfc33099f72884c1ea717d052d
duihuhu <duihuhu@163.com>
2024-06-23 16:00:15 +0800

    debug edit close run exp hucc

--- benchmarks_flowserve/close_run_experiments_hucc.py
+++ benchmarks_flowserve/close_run_experiments_hucc.py
@@ -22,28 +22,35 @@ and our method. Each .csv file represents one line in the figure.
 import os
 import pandas as pd
 import time
+import sys
+def execute_exp(deploy_type):
+    # Configurable parameters
+    basename = 'end2end_exp_results'
+    dataset = 'ReAct' # ['ShareGPT', 'LooGLE', 'ReAct']
+    configs = {
+        'num_requests': 256,
+    }
 
-# Configurable parameters
-basename = 'end2end_exp_results'
-dataset = 'ReAct' # ['ShareGPT', 'LooGLE', 'ReAct']
-configs = {
-    'type': 'disagg_ncache',
-    'num_requests': 256,
-}
-if "disagg" in configs['type']:
-    num_clients= [4, 8, 16, 32] # x-axis 
-else:
-    num_clients= [2, 4, 8, 16] # x-axis 
+    configs['deploy_type'] = deploy_type
 
-# Derived parameters
-dirname = f'{basename}/{dataset}/{configs["type"]}'
+    num_clients= [2, 4, 8, 16]
+    
+    if "disagg" in configs['deploy_type']:
+        num_clients = [x * 2 for x in num_clients]
+    
+    # Derived parameters
+    dirname = f'{basename}/{dataset}/{configs["type"]}'
 
-if not os.path.exists(dirname):
-    os.makedirs(dirname)
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
 
 
-for i, num_client in enumerate(num_clients):
-    command = f'python3 main.py --test-type {"closed"} --dataset {dataset} --num-clients {num_client} --num-requests {configs["num_requests"]}'
-    print(f'Running command: {command}')
-    os.system(f'{command}')
-    time.sleep(5)
+    for i, num_client in enumerate(num_clients):
+        command = f'python3 main.py --test-type {"closed"} --dataset {dataset} --num-clients {num_client} --num-requests {configs["num_requests"]}'
+        print(f'Running command: {deploy_type}, {command}')
+        os.system(f'{command}')
+        time.sleep(5)
+
+if __name__ == "__main__":
+    deploy_type = sys.argv[1]
+    execute_exp(deploy_type)
