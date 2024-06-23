07e22eac828643af9f9645078393f5867ee91f16
duihuhu <duihuhu@163.com>
2024-06-23 22:30:38 +0800

    debug add open loop

--- benchmarks_flowserve/open_run_experiments_hucc.py
+++ benchmarks_flowserve/open_run_experiments_hucc.py
@@ -23,20 +23,37 @@ import os
 import time
 import sys
 
-def execute_exp(deploy_type):
+import vllm.global_scheduler.entrypoints_config as cfg
+from typing import Dict, Set, List, Optional
+import requests
+import time
+def post_request(api_url, request_dict: Optional[Dict] = {}):
+    headers = {"User-Agent": "Test Client"}
+    resp = requests.post(api_url, headers=headers, json=request_dict)
+    return resp
+
+def reset_system(host, port):
+
+    creat_comm_api_url = cfg.reset_system_url % (host, port)
+    payload = {"reset":"reset"}
+    resp = post_request(creat_comm_api_url, payload)
+    return resp
+
+def execute_exp(deploy_type, is_cache):
     # Configurable parameters
     basename = 'end2end_exp_results'
-    dataset = 'LooGLE' # ['ShareGPT', 'LooGLE', 'ReAct']
+    dataset = 'ReAct' # ['ShareGPT', 'LooGLE', 'ReAct']
     configs = {
         'num_requests': 256,
     }
     configs['deploy_type'] = deploy_type
     
     request_rates = []
-    req = 1
-    while req <= 10:
+
+    req = 0.5
+    while req <= 5:
         request_rates.append(req)
-        req = req + 1
+        req = req + 0.5
     
     if "disagg" in configs['deploy_type']:
         request_rates = [x * 2 for x in request_rates]
@@ -48,12 +65,14 @@ def execute_exp(deploy_type):
         os.makedirs(dirname)
 
     for request_rate in request_rates:
-        duration = 100 / request_rate
         command = f'python3 main.py --test-type {"open"} --dataset {dataset} --request-rate {request_rate} --num-requests {configs["num_requests"]} '
         print(f'Running command: {configs["deploy_type"]}, {command}')
         os.system(f'{command}')
+        if is_cache > 0:
+            resp = reset_system(cfg.eprefill_host, 8082)
         time.sleep(5)
 
 if __name__ == "__main__":
     deploy_type = sys.argv[1]
-    execute_exp(deploy_type)
+    is_cache = sys.argv[2]
+    execute_exp(deploy_type, is_cache)
