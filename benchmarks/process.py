b9e914699d1e7353d6a2f64bfffe3fd7d4fbb1cd
unknown <3328677832@qq.com>
2024-06-23 21:56:19 +0800

    start2

--- benchmarks/process.py
+++ benchmarks/process.py
@@ -1,31 +1,32 @@
 root_path = "/home/jovyan/hhy/vllm-hhy/benchmarks/"
-dir_path = "logs5/"
-prefix_path = "log2b_"
+dir_path = "logs3/"
+prefix_path = "log2_2_"
 suffix_path = ".txt"
-total_len = [32,64,128,256,512,1024,2048,4080]
-#ratio = [10,20,30,40,50,60,70,80,90,100]
+total_len = [32,64,128,256,512,1024,2040]
+ratio = [10,20,30,40,50,60,70,80,90,100]
 
 total_costs = []
 for tl in total_len:
     tl_costs = []
-    file_name = root_path + dir_path  + prefix_path + str(tl) + suffix_path
+    '''file_name = root_path + dir_path  + prefix_path + str(tl) + suffix_path
     with open(file_name, 'r') as file:
         lines = file.readlines()
         for i in range(len(lines)):
             if i >= 3:
                 data = float(lines[i].strip().split(' ')[-1])
-                tl_costs.append(data)
-    '''for r in ratio:
+                tl_costs.append(data)'''
+    for r in ratio:
         file_name = root_path + dir_path  + prefix_path + str(tl) + "_" + str(r) + suffix_path
         costs = []
         with open(file_name, 'r') as file:
             lines = file.readlines()
-            for i in range(len(lines)):
-                if i == 8 or i == 10 or i == 12:
-                    st = float(lines[i].strip().split(' ')[-1])
-                    ed = float(lines[i+1].strip().split(' ')[-1])
+            length = len(lines)
+            for i in range(length):
+                if i == (length - 1) or i == (length - 3) or i == (length - 5):
+                    st = float(lines[i - 1].strip().split(' ')[-1])
+                    ed = float(lines[i].strip().split(' ')[-1])
                     costs.append(ed - st)
         tl_costs.append(sum(costs) / len(costs))
-    total_costs.append(tl_costs)'''
-    total_costs.append(sum(tl_costs) / len(tl_costs))
+    total_costs.append(tl_costs)
+    #total_costs.append(sum(tl_costs) / len(tl_costs))
 print(total_costs)    
