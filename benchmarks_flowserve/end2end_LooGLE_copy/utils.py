774a355f704493102ebd41a49c14f1ee8a9aaa9d
DerekHJH <derekhjh9@gmail.com>
2024-06-24 17:06:10 +0800

    Add LooGLE copy benchmark

---
+++ benchmarks_flowserve/end2end_LooGLE_copy/utils.py
@@ -0,0 +1,12 @@
+from typing import Iterable, List, Optional, Tuple
+import numpy as np 
+import torch
+import random
+
+def set_seed(seed):
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
