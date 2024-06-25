8aed6e5dd9666061a9754c0411519b4d5e7d0cdd
duihuhu <duihuhu@163.com>
2024-06-25 10:19:09 +0800

    debug add new testfor loogle scheduler

---
+++ benchmarks_flowserve/end2end_LooGLE_copy/dataset_processing/LooGLE.py
@@ -0,0 +1,66 @@
+import json
+from typing import Iterable, List, Optional, Tuple
+import random
+from transformers import PreTrainedTokenizerBase
+import pickle 
+import os
+from .common import find_range_of_multi_turn_conversations
+from .ReAct import PROMPT_FORWORD
+import math 
+
+def sample_requests(
+    args,
+    dataset_path: str,
+    tokenizer: PreTrainedTokenizerBase,
+    num_requests: int 
+) -> Tuple[List[Tuple[str, List[int], int, int]], List[int]]:
+
+    cached_file_name = dataset_path.split('.')[0] + '_cached.pkl'
+    if os.path.exists(cached_file_name):
+        with open(cached_file_name, 'rb') as f:
+            reqs = pickle.load(f)
+    else:
+        doc_QAs = []
+        with open(dataset_path, 'r') as f:
+            for line in f:
+                doc_QAs.append(json.loads(line)) 
+            
+        prompts = []
+        completions = []
+        for i in range(len(doc_QAs)):
+            # Cut the long document
+            s = 'Title: ' + doc_QAs[i]['title'] + '\n' + doc_QAs[i]['input'][:len(PROMPT_FORWORD)] + '\n'
+            QAs = eval(doc_QAs[i]['qa_pairs'])
+            for j in range(len(QAs)):
+                # Since there are too many QAs for one single document, we only sample the first 6 QAs
+                if j > 5:
+                    break
+                s += 'Question: ' + QAs[j]['Q'] + '\n'
+                prompts.append(s)
+                completions.append('Answer: ' + QAs[j]['A'] + '\n')
+                s += completions[-1]
+
+        prompt_token_ids = tokenizer(prompts).input_ids
+        completion_token_ids = tokenizer(completions).input_ids
+
+        reqs = []
+        for i in range(len(prompts)):
+            prompt_len = len(prompt_token_ids[i])
+            completion_len = len(completion_token_ids[i])
+            if prompt_len + completion_len > 4090:
+                continue
+            reqs.append((prompts[i], prompt_token_ids[i], prompt_len, completion_len))
+        # Caution: we only cache the first 1024 requests
+        pickle.dump(reqs[:1024], open(cached_file_name, 'wb')) 
+
+    sampled_requests = reqs[:num_requests]
+    while len(sampled_requests) < num_requests:
+        sampled_requests.extend(reqs[:num_requests - len(sampled_requests)])
+
+    multi_conversations_range = find_range_of_multi_turn_conversations(sampled_requests)
+
+    # Copy the first args.num_sessions sessions args.share_ratio times
+    sampled_requests = sampled_requests[:multi_conversations_range[args.num_sessions]] * args.share_ratio
+    multi_conversations_range = find_range_of_multi_turn_conversations(sampled_requests)
+
+    return sampled_requests, multi_conversations_range
---
+++ benchmarks_flowserve/end2end_LooGLE_copy/plot_result.py
@@ -0,0 +1,66 @@
+'''
+We assume the following result folder structure:
+
+
+basename  
+    dataset1
+        config1.csv
+        config2.csv
+        ...
+    dataset2
+        ...
+    dataset3
+        ...
+    ...
+
+In each of the .csv file, each column represents a different metric, 
+and each row represents a different experiment configuration.
+
+Each folder represents a figure that compares  the performance of the baselines 
+and our method. Each .csv file represents one line in the figure.
+'''
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+from typing import List, Union
+
+# Configurable parameters
+basename = 'end2end_exp_results'
+dataset = 'LooGLE' # ['ShareGPT', 'LooGLE', 'ReAct']
+
+# Derived parameters
+dirname = f'{basename}/{dataset}'
+
+def plot_figure(x_axis: str, y_axis: Union[str, List[str]]):
+    plt.figure(figsize=(12, 10))
+    for file in os.listdir(dirname):
+        if os.path.isfile(os.path.join(dirname, file)) and '.csv' in file:
+            df = pd.read_csv(f'{dirname}/{file}')
+            type = file.split('.')[0] + '_'
+            plt.plot(df[x_axis], df[y_axis], label = type + df[y_axis].columns)
+    
+    plt.xlim(0)
+    plt.ylim(0)
+    plt.xticks(fontsize=24)
+    plt.yticks(fontsize=24)
+    plt.xlabel('req/s', fontsize=24)
+    plt.ylabel('time (s)', fontsize=24)
+    plt.legend()
+    plt.grid(True)
+    plt.title(' '.join(y_axis[0].split('_')[1:]), fontsize=24)   
+    figure_title = '_'.join(y_axis[0].split('_')[1:])
+    plt.savefig(f'{dirname}/{figure_title}.png')
+    plt.clf()
+    
+if __name__ == '__main__':
+    for y_axis in [
+        ['average_jct', 'p99_jct'], 
+        ['average_ttft', 'p99_ttft'], 
+        ['average_tbt_no_second_token', 'p99_tbt_no_second_token'], 
+        ['average_tbt_with_second_token', 'p99_tbt_with_second_token']
+    ]:
+        plot_figure(
+            x_axis = 'request_rate',
+            y_axis = y_axis
+        )
+    
