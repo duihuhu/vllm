781a13f887603dd0951dcceb6ea4181411e211f9
unknown <3328677832@qq.com>
2024-07-31 21:25:36 +0800

    debug3

--- benchmarks/benchmark_latency.py
+++ benchmarks/benchmark_latency.py
@@ -135,7 +135,7 @@ if __name__ == '__main__':
                         '-q',
                         choices=['awq', 'gptq', 'squeezellm', None],
                         default=None)
-    parser.add_argument('--tensor-parallel-size', '-tp', type=int, default=1)
+    parser.add_argument('--tensor-parallel-size', '-tp', type=int, default=2)
     parser.add_argument('--input-len', type=int, default=8)
     parser.add_argument('--output-len', type=int, default=1)
     parser.add_argument('--num-seqs', type=int, default=1)
