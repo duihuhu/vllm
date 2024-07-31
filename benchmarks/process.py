ee468f1f66cc064f79f089bee1b123ab6ef6da94
unknown <3328677832@qq.com>
2024-07-31 21:52:10 +0800

    debug6

--- benchmarks/process.py
+++ benchmarks/process.py
@@ -13,33 +13,33 @@ for lens in total_len:
     attn = []
     o_proj = []
     layer = []
-    for b in bs:
-        file_name = root_path + dir_path + prefix_path + str(lens) + "_" + str(b) + suffix_path
-        with open(file_name, 'r') as file:
-            lines = file.readlines()
-            for line in lines:
-                data = line.strip().split(' ')
-                if str(data[0]) == "ffn1":
-                    ffn1.append(float(data[-2]))
-                elif str(data[0]) == "ffn2":
-                    ffn2.append(float(data[-2]))
-                elif str(data[0]) == "qkv_proj":
-                    qkv_proj.append(float(data[-2]))
-                elif str(data[0]) == "rope":
-                    rope.append(float(data[-2]))
-                elif str(data[0]) == "attn":
-                    attn.append(float(data[-2]))
-                elif str(data[0]) == "o_proj":
-                    o_proj.append(float(data[-2]))
-                elif str(data[0]) == "layer":
-                    layer.append(float(data[-2]))
+    #for b in bs:
+    file_name = root_path + dir_path + prefix_path + str(lens) + suffix_path
+    with open(file_name, 'r') as file:
+        lines = file.readlines()
+        for line in lines:
+            data = line.strip().split(' ')
+            if str(data[0]) == "ffn1":
+                ffn1.append(float(data[-2]))
+            elif str(data[0]) == "ffn2":
+                ffn2.append(float(data[-2]))
+            elif str(data[0]) == "qkv_proj":
+                qkv_proj.append(float(data[-2]))
+            elif str(data[0]) == "rope":
+                rope.append(float(data[-2]))
+            elif str(data[0]) == "attn":
+                attn.append(float(data[-2]))
+            elif str(data[0]) == "o_proj":
+                o_proj.append(float(data[-2]))
+            elif str(data[0]) == "layer":
+                layer.append(float(data[-2]))
     ffn1_m = sum(ffn1) / len(ffn1)
     ffn2_m = sum(ffn2) / len(ffn2)
     qkv_proj_m = sum(qkv_proj) / len(qkv_proj)
     rope_m = sum(rope) / len(rope)
     attn_m = sum(attn) / len(attn)
     o_proj_m = sum(o_proj) / len(o_proj)
-    layer_m = ffn1_m + ffn2_m + qkv_proj_m + rope_m + attn_m + o_proj_m 
+    layer_m = sum(layer) / len(layer)
     print(f"----------{lens}-----------")
     print(f"ffn1 ffn2 qkv_proj rope attn o_proj others")
     print(f"{ffn1_m / layer_m:.4f}")
@@ -48,4 +48,4 @@ for lens in total_len:
     print(f"{rope_m / layer_m:.4f}")
     print(f"{attn_m / layer_m:.4f}")
     print(f"{o_proj_m / layer_m:.4f}")
-    #print(f"{(layer_m - ffn1_m - ffn1_m - qkv_proj_m - rope_m - attn_m - o_proj_m) / layer_m:.4f}")
+    print(f"{(layer_m - ffn1_m - ffn1_m - qkv_proj_m - rope_m - attn_m - o_proj_m) / layer_m:.4f}")
