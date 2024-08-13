b6f9ab8aef0429387833aff963719bc9e56ec224
unknown <3328677832@qq.com>
2024-08-13 22:13:39 +0800

    re-test10

--- benchmarks/benchmark_latency.py
+++ benchmarks/benchmark_latency.py
@@ -114,7 +114,7 @@ def main(args: argparse.Namespace):
             return latency
 
     print("Warming up...")
-    run_to_completion(inputs=warmups, profile_dir=None, file_name=None)
+    #run_to_completion(inputs=warmups, profile_dir=None, file_name=None)
 
     '''if args.profile:
         profile_dir = args.profile_result_dir
