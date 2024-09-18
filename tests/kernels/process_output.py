ca0b6d552cfcc6886d317113839d600f31beb041
unknown <3328677832@qq.com>
2024-09-18 12:27:55 +0800

    process ncu profile outputs

--- tests/kernels/process_output.py
+++ tests/kernels/process_output.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-file_path_1 = "thread_duration_sm_1_"
+file_path_1 = "/home/jovyan/vllm/tests/kernels/profile_logs_transaction/total_transaction_1_"
 file_path_2 = ".ncu-rep"
 file_path_3 = ".csv"
 lengths = [8,16,32]
@@ -16,7 +16,7 @@ while True:
 for length in lengths:
     file_path_in = file_path_1 + str(length) + file_path_2
     file_path_out = file_path_1 + str(length) + file_path_3
-    base_command = "ncu --import {x} --csv --output {y}"
+    base_command = "ncu -i {x} --page details --csv --log-file {y}"
     command = base_command.format(x = file_path_in, y = file_path_out)
     os.system(command)
 
