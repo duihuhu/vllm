524a2d58c62b66ab210867061f0466c94e7c1261
unknown <3328677832@qq.com>
2024-09-20 13:30:23 +0800

    process outputs 1

--- tests/kernels/process_output.py
+++ tests/kernels/process_output.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import os
 
-file_path_1 = "/home/jovyan/vllm/tests/kernels/profile_logs_transaction/total_transaction_1_"
+file_path_1 = "/home/jovyan/vllm/tests/kernels/profile_logs_bytes/total_bytes_1_"
 file_path_2 = ".ncu-rep"
 file_path_3 = ".csv"
 lengths = [8,16,32]
@@ -13,14 +13,14 @@ while True:
         lengths.append(i)
         i += 64
 
-'''for length in lengths:
+for length in lengths:
     file_path_in = file_path_1 + str(length) + file_path_2
     file_path_out = file_path_1 + str(length) + file_path_3
     base_command = "ncu -i {x} --page details --csv --log-file {y}"
     command = base_command.format(x = file_path_in, y = file_path_out)
-    os.system(command)'''
+    os.system(command)
 
-iter_length = 10
+'''iter_length = 10
 st = 975
 ed = 1374
 layers = 40
@@ -192,4 +192,4 @@ for i, name in enumerate(names):
     print(f"----------{name}----------")
     for data in outputs2[i]:
         print(data)
-    print(f"----------END----------")
+    print(f"----------END----------")'''
