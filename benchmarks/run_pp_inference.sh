a7eeeb073f0953bd5eeebf5a3308c9eb81b0bb80
duihuhu <duihuhu@163.com>
2024-09-29 14:35:05 +0800

    debug add for p/p inference

--- benchmarks/run_pp_inference.sh
+++ benchmarks/run_pp_inference.sh
@@ -1,26 +1,26 @@
  #!/bin/bash
-python3 benchmark_latency.py --input-len 18 --output-len 1 --num-iter 5 --num-light 1 --batch-size 2
+python3 benchmark_latency_inference.py --input-len 18 --output-len 1 --num-iter 5 --num-light 1 --batch-size 2
 
 sleep 5
 
-python3 benchmark_latency.py --input-len 18 --output-len 1 --num-iter 5 --num-light 2 --batch-size 2
+python3 benchmark_latency_inference.py --input-len 18 --output-len 1 --num-iter 5 --num-light 2 --batch-size 2
 
 sleep 5
 
-python3 benchmark_latency.py --input-len 18 --output-len 1 --num-iter 5 --num-light 4 --batch-size 4
+python3 benchmark_latency_inference.py --input-len 18 --output-len 1 --num-iter 5 --num-light 4 --batch-size 4
 
 sleep 5
 
-python3 benchmark_latency.py --input-len 18 --output-len 1 --num-iter 5 --num-light 8 --batch-size 8
+python3 benchmark_latency_inference.py --input-len 18 --output-len 1 --num-iter 5 --num-light 8 --batch-size 8
 
 sleep 5
 
-python3 benchmark_latency.py --input-len 18 --output-len 1 --num-iter 5 --num-light 16 --batch-size 16
+python3 benchmark_latency_inference.py --input-len 18 --output-len 1 --num-iter 5 --num-light 16 --batch-size 16
 
 sleep 5
 
-python3 benchmark_latency.py --input-len 18 --output-len 1 --num-iter 5 --num-light 32 --batch-size 32
+python3 benchmark_latency_inference.py --input-len 18 --output-len 1 --num-iter 5 --num-light 32 --batch-size 32
 
 sleep 5
 
-python3 benchmark_latency.py --input-len 18 --output-len 1 --num-iter 5 --num-light 64 --batch-size 64
+python3 benchmark_latency_inference.py --input-len 18 --output-len 1 --num-iter 5 --num-light 64 --batch-size 64
