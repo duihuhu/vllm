53944afd4e8b8f4f2e471289219b5ae4b25363f4
unknown <3328677832@qq.com>
2024-10-02 02:41:32 +0800

    profile long range tp2 act

--- vllm/model_executor/models/llama.py
+++ vllm/model_executor/models/llama.py
@@ -74,7 +74,21 @@ class LlamaMLP(nn.Module):
 
     def forward(self, x, log_file_path: Optional[str] = None):     
         gate_up, _ = self.gate_up_proj(x)
-        x = self.act_fn(gate_up)
+
+        if log_file_path:
+            start = torch.cuda.Event(enable_timing = True)
+            end = torch.cuda.Event(enable_timing = True)
+
+            start.record()
+            x = self.act_fn(gate_up)
+            end.record()
+            torch.cuda.synchronize()
+
+            with open(log_file_path, 'a') as file:
+                file.write(f"act costs {start.elapsed_time(end)}\n")
+        else:
+            x = self.act_fn(gate_up)
+        
         x, _ = self.down_proj(x)
         return x
 
@@ -254,21 +268,8 @@ class LlamaDecoderLayer(nn.Module):
                 log_file_path=log_file_path)
         
         # Fully Connected
-        if log_file_path:
-            start = torch.cuda.Event(enable_timing = True)
-            end = torch.cuda.Event(enable_timing = True)
-
-            start.record()
-            hidden_states, residual = self.post_attention_layernorm(
-                hidden_states, residual)
-            end.record()
-            torch.cuda.synchronize()
-
-            with open(log_file_path, 'a') as file:
-                file.write(f"postattnnorm costs {start.elapsed_time(end)}\n")
-        else:
-            hidden_states, residual = self.post_attention_layernorm(
-                hidden_states, residual)
+        hidden_states, residual = self.post_attention_layernorm(
+            hidden_states, residual)
         
         hidden_states = self.mlp(hidden_states, log_file_path)
         
