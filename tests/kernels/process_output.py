e4fbf96d82d5dd68362a46aedeb935724fda48a2
unknown <3328677832@qq.com>
2024-10-01 13:44:34 +0800

    long range 3

--- tests/kernels/process_output.py
+++ tests/kernels/process_output.py
@@ -33,12 +33,169 @@ while True:
         lengths.append(i)
         i += 64
 
-for length in lengths:
+'''for length in lengths:
     file_path_in = file_path_1 + str(length) + file_path_2
     file_path_out = file_path_1 + str(length) + file_path_3
     base_command = "ncu -i {x} --page details --csv --log-file {y}"
     command = base_command.format(x = file_path_in, y = file_path_out)
-    os.system(command)
+    os.system(command)'''
+
+st = 975
+ed = 1374
+layers = 40
+iter_length = 10
+
+total_preattnnorm = []
+total_qkvproj = []
+total_rope = []
+total_store = []
+total_attn = []
+total_oproj = []
+total_postattnnorm = []
+total_ffn1 = []
+total_act = []
+total_ffn2 = []
+
+total_preattnnorm2 = []
+total_qkvproj2 = []
+total_rope2 = []
+total_store2 = []
+total_attn2 = []
+total_oproj2 = []
+total_postattnnorm2 = []
+total_ffn12 = []
+total_act2 = []
+total_ffn22 = []
+
+for length in lengths:
+    preattnnorm = []
+    qkvproj =  []
+    rope = []
+    store = []
+    attn = []
+    oproj = []
+    postattnnorm = []
+    ffn1 = []
+    act = []
+    ffn2 = []
+
+    preattnnorm2 = []
+    qkvproj2 =  []
+    rope2 = []
+    store2 = []
+    attn2 = []
+    oproj2 = []
+    postattnnorm2 = []
+    ffn12 = []
+    act2= []
+    ffn22 = []
+
+    file_path = file_path_1 + str(length) + file_path_3
+    df = pd.read_csv(file_path)
+    for _, row in df.iterrows():
+        id = int(str(row['ID']))
+        mname = str(row['Metric Name'])
+        if id >= st and id <= ed and mname == "gpu__time_duration.avg":
+            data1 = str(row['Block Size']).split("(")[1].split(",")[0]
+            data2 = float(str(row['Metric Value']))
+
+            offset = (id - st) % iter_length
+            if offset == 0:
+                preattnnorm.append(data1)
+                preattnnorm2.append(data2)
+            elif offset == 1:
+                qkvproj.append(data1)
+                qkvproj2.append(data2)
+            elif offset == 2:
+                rope.append(data1)
+                rope2.append(data2)
+            elif offset == 3:
+                store.append(data1)
+                store2.append(data2)
+            elif offset == 4:
+                attn.append(data1)
+                attn2.append(data2)
+            elif offset == 5:
+                oproj.append(data1)
+                oproj2.append(data2)
+            elif offset == 6:
+                postattnnorm.append(data1)
+                postattnnorm2.append(data2)
+            elif offset == 7:
+                ffn1.append(data1)
+                ffn12.append(data2)
+            elif offset == 8:
+                act.append(data1)
+                act2.append(data2)
+            else:
+                ffn2.append(data1)
+                ffn22.append(data2)
+
+    total_preattnnorm.append(sum(preattnnorm) / layers)
+    total_qkvproj.append(sum(qkvproj) / layers)
+    total_rope.append(sum(rope) / layers)
+    total_store.append(sum(store) / layers)
+    total_attn.append(sum(attn) / layers)
+    total_oproj.append(sum(oproj) / layers)
+    total_postattnnorm.append(sum(postattnnorm) / layers)
+    total_ffn1.append(sum(ffn1) / layers)
+    total_act.append(sum(act) / layers)
+    total_ffn2.append(sum(ffn2) / layers)
+
+    total_preattnnorm2.append(sum(preattnnorm2) / layers)
+    total_qkvproj2.append(sum(qkvproj2) / layers)
+    total_rope2.append(sum(rope2) / layers)
+    total_store2.append(sum(store2) / layers)
+    total_attn2.append(sum(attn2) / layers)
+    total_oproj2.append(sum(oproj2) / layers)
+    total_postattnnorm2.append(sum(postattnnorm2) / layers)
+    total_ffn12.append(sum(ffn12) / layers)
+    total_act2.append(sum(act2) / layers)
+    total_ffn22.append(sum(ffn22) / layers)
+
+outputs1 = []
+outputs2 = []
+
+outputs1.append(total_preattnnorm)
+outputs1.append(total_qkvproj)
+outputs1.append(total_rope)
+outputs1.append(total_store)
+outputs1.append(total_attn)
+outputs1.append(total_oproj)
+outputs1.append(total_postattnnorm)
+outputs1.append(total_ffn1)
+outputs1.append(total_act)
+outputs1.append(total_ffn2)
+    
+outputs2.append(total_preattnnorm2)
+outputs2.append(total_qkvproj2)
+outputs2.append(total_rope2)
+outputs2.append(total_store2)
+outputs2.append(total_attn2)
+outputs2.append(total_oproj2)
+outputs2.append(total_postattnnorm2)
+outputs2.append(total_ffn12)
+outputs2.append(total_act2)
+outputs2.append(total_ffn22)
+
+names= ["preattnnorm", "qkvproj", "rope", "store", "attn", "oproj", "postattnnorm", "ffn1", "act", "ffn2"]
+
+print("----------BLOCK SIZE----------")
+print("\n")
+for i, name in enumerate(names):
+    print(f"----------{name}----------")
+    for item in outputs1[i]:
+        print(item)
+
+print("\n")
+
+print("----------Time----------")
+print("\n")
+for i, name in enumerate(names):
+    print(f"----------{name}----------")
+    for item in outputs2[i]:
+        print(item)
+
 
 '''iter_length = 10
 st = 975
