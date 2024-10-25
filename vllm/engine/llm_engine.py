3da70f8a6a695f80cb65b431424e89821256b498
duihuhu <duihuhu@163.com>
2024-10-25 15:50:30 +0800

    debug add test for aplaca dataset

--- vllm/engine/llm_engine.py
+++ vllm/engine/llm_engine.py
@@ -392,6 +392,7 @@ class LLMEngine:
         request_outputs: List[RequestOutput] = []
         for seq_group in seq_groups + ignored_seq_groups:
             request_output = RequestOutput.from_seq_group(seq_group)
+            print("first token time ", self.first_token_output[request_output.request_id])
             request_output.first_token_time = self.first_token_output[request_output.request_id]
             request_outputs.append(request_output)
         # self.scheduler.store_prompt_kv_cache()
@@ -456,12 +457,12 @@ class LLMEngine:
         )
         
         # print who get the output
-        first_token_time = time.time()
-        for seq_group_metadata in seq_group_metadata_list:
-           seq_group_id = seq_group_metadata.request_id
-           if seq_group_id not in self.first_token_output:
-               print(f"seq {seq_group_id} gets its' first token at {first_token_time}")
-               self.first_token_output[seq_group_id] = 1
+        # first_token_time = time.time()
+        # for seq_group_metadata in seq_group_metadata_list:
+        #    seq_group_id = seq_group_metadata.request_id
+        #    if seq_group_id not in self.first_token_output:
+        #        print(f"seq {seq_group_id} gets its' first token at {first_token_time}")
+        #        self.first_token_output[seq_group_id] = 1
 
         # Update the scheduler with the model outputs.
         seq_groups = self.scheduler.update(output)
