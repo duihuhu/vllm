55810348ee5600279d62aa3450c650ddf97f56f4
duihuhu <duihuhu@163.com>
2024-11-03 20:54:40 +0800

    debug add request id to time

--- vllm/chunked/chunkrunner.py
+++ vllm/chunked/chunkrunner.py
@@ -611,7 +611,7 @@ class ChunkRunner:
                     self._reduce_outputs()
                     output_num = 0
             #print(f"now {total_num} reqs have been finished")
-            print("mprefill!!:  prefill iteration now is no unfinished")
+            print("chunk mprefill!!:  prefill iteration now is no unfinished")
         return prefill_nums       
     
         
