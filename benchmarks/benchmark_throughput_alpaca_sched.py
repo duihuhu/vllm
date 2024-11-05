eeacade0bb864c6f2bc10193bc2310a45bd551ce
duihuhu <duihuhu@163.com>
2024-11-05 15:15:52 +0800

    debug sched for execute alpaca

---
+++ benchmarks/benchmark_throughput_alpaca_sched.py
@@ -0,0 +1,319 @@
+"""Benchmark offline inference throughput."""
+import argparse
+import json
+import random
+import time
+from typing import List, Tuple
+
+import torch
+from transformers import AutoModelForCausalLM, PreTrainedTokenizerBase
+from tqdm import tqdm
+
+from vllm import LLM, SamplingParams
+from vllm.transformers_utils.tokenizer import get_tokenizer
+
+def get_data(filename):
+    input_len = []
+    output_len = []
+    with open(filename, "r") as fd:
+        for line in fd.readlines():
+            content = line.split("\n")[0].split(" ")
+            input_len.append(int(content[1]))
+            output_len.append(int(content[2]))
+    return input_len, output_len
+        
+def sample_requests_aplaca(
+    dataset_path: str,
+    num_requests: int,
+    tokenizer: PreTrainedTokenizerBase,
+    filename: str,
+) -> List[Tuple[str, int, int]]:
+    random.seed(0)
+    # Load the dataset.
+    with open(dataset_path) as f:
+        dataset = json.load(f)
+    # Filter out the conversations with less than 2 turns.
+    aplaca_data = []
+    for data in dataset:
+        if data["input"] == "":
+            aplaca_data.append((data["instruction"], data["output"]))
+
+    # Tokenize the prompts and completions.
+    prompts = [prompt for prompt, _ in aplaca_data]
+    prompt_token_ids = tokenizer(prompts).input_ids
+    completions = [completion for _, completion in aplaca_data]
+    completion_token_ids = tokenizer(completions).input_ids
+    tokenized_dataset = []
+    for p, pt, c, ct in zip(prompts, prompt_token_ids, completions, completion_token_ids):
+        tokenized_dataset.append((p, pt, len(ct)))
+
+    # Filter out too long sequences.
+    # filtered_dataset: List[Tuple[str, int, int]] = []
+    filtered_dataset: List[Tuple[str, List[int], str, int]] = []
+    for prompt, prompt_token_ids, output_len in tokenized_dataset:
+        prompt_len = len(prompt_token_ids)
+        # if prompt_len > 256 or output_len > 128:
+        if prompt_len < 4 or output_len < 4:
+        # if prompt_len < 512 or output_len < 256:
+        # if prompt_len > 512 or output_len < 128:
+            # Prune too short sequences.
+            continue
+        # if prompt_len > 1024 or prompt_len + output_len > 2048:
+        if prompt_len + output_len > 2048:
+            # Prune too long sequences.
+            continue
+        filtered_dataset.append((prompt, prompt_len, output_len))
+        # filtered_prompts.append(prompt)
+        # filtered_tokenids.append(prompt_token_ids)
+    # Sample the requests.
+    # sampled_requests = random.sample(filtered_dataset, num_requests)
+
+    sampled_prompts = random.sample(filtered_dataset, num_requests)
+    for req in sampled_prompts:
+        print("choose req info ", req[1], req[2])
+    input_lens, output_lens = get_data(filename)
+    
+    sample_request = []
+    for input_len, output_len in zip(input_lens, output_lens):
+        for req in sampled_prompts:
+            if req[1] == input_len and req[2] == output_len:
+                sample_request.append(req)
+    # return sampled_requests
+    print(len(sample_request))
+    return sample_request
+    # return sampled_prompts
+
+def sample_requests(
+    dataset_path: str,
+    num_requests: int,
+    tokenizer: PreTrainedTokenizerBase,
+) -> List[Tuple[str, int, int]]:
+    # Load the dataset.
+    with open(dataset_path) as f:
+        dataset = json.load(f)
+    # Filter out the conversations with less than 2 turns.
+    dataset = [
+        data for data in dataset
+        if len(data["conversations"]) >= 2
+    ]
+    # Only keep the first two turns of each conversation.
+    dataset = [
+        (data["conversations"][0]["value"], data["conversations"][1]["value"])
+        for data in dataset
+    ]
+
+    # Tokenize the prompts and completions.
+    prompts = [prompt for prompt, _ in dataset]
+    prompt_token_ids = tokenizer(prompts).input_ids
+    completions = [completion for _, completion in dataset]
+    completion_token_ids = tokenizer(completions).input_ids
+    tokenized_dataset = []
+    for i in range(len(dataset)):
+        # if i < 1 :
+        output_len = len(completion_token_ids[i])
+        tokenized_dataset.append((prompts[i], prompt_token_ids[i], output_len))
+        # else:
+        #     output_len = len(completion_token_ids[8])
+        #     tokenized_dataset.append((prompts[8], prompt_token_ids[8], output_len))
+    # print(prompts[71])
+    # Filter out too long sequences.
+    filtered_dataset: List[Tuple[str, int, int]] = []
+    for prompt, prompt_token_ids, output_len in tokenized_dataset:
+        prompt_len = len(prompt_token_ids)
+        if prompt_len < 4 or output_len < 4:
+        # if prompt_len > 256 or output_len > 128:
+            # Prune too short sequences.
+            continue
+        # if prompt_len > 1024 or prompt_len + output_len > 2048:
+        # if prompt_len > 1024 or prompt_len + output_len > 2048:
+        if prompt_len + output_len > 2048:
+            # Prune too long sequences.
+            continue
+        filtered_dataset.append((prompt, prompt_len, output_len))
+
+    # Sample the requests.
+    # sampled_requests = filtered_dataset[:num_requests]
+    sampled_requests = random.sample(filtered_dataset, num_requests)
+    for req in sampled_requests:
+        print("choose req info ", req[1], req[2])
+    return sampled_requests
+
+
+def run_vllm(
+    requests: List[Tuple[str, int, int]],
+    model: str,
+    tokenizer: str,
+    tensor_parallel_size: int,
+    seed: int,
+    n: int,
+    use_beam_search: bool,
+    batch_size: int,
+    split_two_phase: int
+) -> float:
+    llm = LLM(
+        model=model,
+        tokenizer=tokenizer,
+        tensor_parallel_size=tensor_parallel_size,
+        seed=seed,
+        max_num_seqs=batch_size,
+    )
+
+    # Add the requests to the engine.
+    for prompt, _, output_len in requests:
+        sampling_params = SamplingParams(
+            n=n,
+            # temperature=0.0 if use_beam_search else 1.0,
+            temperature=0.0,
+            top_p=1.0,
+            use_beam_search=False,
+            ignore_eos=True,
+            max_tokens=output_len,
+        )
+        # FIXME(woosuk): Do not use internal method.
+        llm._add_request(
+            prompt=prompt,
+            prompt_token_ids=None,
+            sampling_params=sampling_params,
+        )
+
+    start = time.time()
+    # FIXME(woosuk): Do use internal method.
+    outputs = llm._run_engine(use_tqdm=False, split_two_phase=split_two_phase)
+    end = time.time()
+    
+    
+    elapsed_time = end-start 
+    total_num_tokens = sum(
+        len(output.prompt_token_ids) + len(output.outputs[0].token_ids)
+        for output in outputs
+    )
+    total_decode_num_tokens = sum(
+         len(output.outputs[0].token_ids)
+        for output in outputs
+    )
+    print(f"End start is {start}, End end is {end}", end-start)
+    print("total_num_reqs: ", len(outputs))
+    print("total_num_tokens: ", total_num_tokens)
+    print("total_decode_num_tokens: ", total_decode_num_tokens)
+    print(f"Throughput: {len(requests) / elapsed_time:.2f} requests/s, "
+         f"{total_num_tokens / elapsed_time:.2f} tokens/s")
+
+    return end - start
+
+
+def run_hf(
+    requests: List[Tuple[str, int, int]],
+    model: str,
+    tokenizer: PreTrainedTokenizerBase,
+    n: int,
+    use_beam_search: bool,
+    max_batch_size: int,
+) -> float:
+    assert not use_beam_search
+    llm = AutoModelForCausalLM.from_pretrained(model, torch_dtype=torch.float16)
+    if llm.config.model_type == "llama":
+        # To enable padding in the HF backend.
+        tokenizer.pad_token = tokenizer.eos_token
+    llm = llm.cuda()
+
+    pbar = tqdm(total=len(requests))
+    start = time.time()
+    batch: List[str] = []
+    max_prompt_len = 0
+    max_output_len = 0
+    for i in range(len(requests)):
+        prompt, prompt_len, output_len = requests[i]
+        # Add the prompt to the batch.
+        batch.append(prompt)
+        max_prompt_len = max(max_prompt_len, prompt_len)
+        max_output_len = max(max_output_len, output_len)
+        if len(batch) < max_batch_size and i != len(requests) - 1:
+            # Check if we can add more requests to the batch.
+            _, next_prompt_len, next_output_len = requests[i + 1]
+            if (max(max_prompt_len, next_prompt_len) + max(
+                max_output_len, next_output_len)) <= 2048:
+                # We can add more requests to the batch.
+                continue
+
+        # Generate the sequences.
+        input_ids = tokenizer(batch, return_tensors="pt", padding=True).input_ids
+        llm_outputs = llm.generate(
+            input_ids=input_ids.cuda(),
+            do_sample=not use_beam_search,
+            num_return_sequences=n,
+            temperature=1.0,
+            top_p=1.0,
+            use_cache=True,
+            max_new_tokens=max_output_len,
+        )
+        # Include the decoding time.
+        tokenizer.batch_decode(llm_outputs, skip_special_tokens=True)
+        pbar.update(len(batch))
+
+        # Clear the batch.
+        batch = []
+        max_prompt_len = 0
+        max_output_len = 0
+    end = time.time()
+    return end - start
+
+
+def main(args: argparse.Namespace):
+    random.seed(args.seed)
+
+    # Sample the requests.
+    tokenizer = get_tokenizer(args.tokenizer)
+    # requests = sample_requests(args.dataset, args.num_prompts, tokenizer)
+    requests = sample_requests_aplaca(args.dataset, args.num_prompts, tokenizer, args.filename)
+
+    if args.backend == "vllm":
+        elapsed_time = run_vllm(
+            requests, args.model, args.tokenizer, args.tensor_parallel_size,
+            args.seed, args.n, args.use_beam_search, args.batch_size, args.split_two_phase)
+    elif args.backend == "hf":
+        assert args.tensor_parallel_size == 1
+        elapsed_time = run_hf(requests, args.model, tokenizer, args.n,
+                              args.use_beam_search, args.hf_max_batch_size)
+    else:
+        raise ValueError(f"Unknown backend: {args.backend}")
+    total_num_tokens = sum(
+        prompt_len + output_len
+        for _, prompt_len, output_len in requests
+    )
+    print(f"Throughput: {len(requests) / elapsed_time:.2f} requests/s, "
+          f"{total_num_tokens / elapsed_time:.2f} tokens/s")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Benchmark the throughput.")
+    parser.add_argument("--backend", type=str, choices=["vllm", "hf"],
+                        default="vllm")
+    parser.add_argument("--dataset", type=str, required=True,
+                        help="Path to the dataset.")
+    parser.add_argument("--model", type=str, default="facebook/opt-125m")
+    parser.add_argument("--tokenizer", type=str, default=None)
+    parser.add_argument("--tensor-parallel-size", "-tp", type=int, default=1)
+    parser.add_argument("--n", type=int, default=1,
+                        help="Number of generated sequences per prompt.")
+    parser.add_argument("--use-beam-search", action="store_true")
+    parser.add_argument("--num-prompts", type=int, default=1000,
+                        help="Number of prompts to process.")
+    parser.add_argument("--seed", type=int, default=0)
+    parser.add_argument("--hf-max-batch-size", type=int, default=None,
+                        help="Maximum batch size for HF backend.")
+    parser.add_argument("--batch-size", type=int, default=256)
+    parser.add_argument("--split-two-phase", type=int, default=0)
+    parser.add_argument("--filename", type=str, default="mixed_datasets/spitwise_0.txt")
+
+    args = parser.parse_args()
+
+    if args.backend == "vllm":
+        if args.hf_max_batch_size is not None:
+            raise ValueError("HF max batch size is only for HF backend.")
+    elif args.backend == "hf":
+        if args.hf_max_batch_size is None:
+            raise ValueError("HF max batch size is required for HF backend.")
+    if args.tokenizer is None:
+        args.tokenizer = args.model
+
+    main(args)
