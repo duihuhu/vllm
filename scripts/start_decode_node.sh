a3d00a042a91ae87b32155f9511528becd94b347
ZhuJiaqi <zhujiaqi9905@gmail.com>
2024-12-28 06:31:30 +0000

    debug

--- scripts/start_decode_node.sh
+++ scripts/start_decode_node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SERVER_PORT={1:-8083}
-MODEL_PATH={2:-/workspace/file/models/llama2-13b/models--meta-llama--Llama-2-13b-hf/snapshots/5c31dfb671ce7cfe2d7bb7c04375e44c55e815b1/}
+SERVER_PORT=${1:-8083}
+MODEL_PATH=${2:-/workspace/file/models/llama2-13b/models--meta-llama--Llama-2-13b-hf/snapshots/5c31dfb671ce7cfe2d7bb7c04375e44c55e815b1/}
 
 
 python3 ./vllm/entrypoints/server.py --model ${MODEL_PATH} \
--- scripts/start_prefill_node.sh
+++ scripts/start_prefill_node.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-SERVER_PORT={1:-8082}
-MODEL_PATH={2:-/workspace/file/models/llama2-13b/models--meta-llama--Llama-2-13b-hf/snapshots/5c31dfb671ce7cfe2d7bb7c04375e44c55e815b1/}
+SERVER_PORT=${1:-8082}
+MODEL_PATH=${2:-/workspace/file/models/llama2-13b/models--meta-llama--Llama-2-13b-hf/snapshots/5c31dfb671ce7cfe2d7bb7c04375e44c55e815b1/}
 
 python ./vllm/entrypoints/server.py  --model ${MODEL_PATH} \
     --local_host 0.0.0.0 --local_port ${SERVER_PORT} \
