ffaa87f36f94bb99b3778cb4a4f4f7e877b4c4cc
ZhuJiaqi <zhujiaqi9905@gmail.com>
2024-12-28 06:53:06 +0000

    change to a small model

--- scripts/start_decode_node.sh
+++ scripts/start_decode_node.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 SERVER_PORT=${1:-8083}
-MODEL_PATH=${2:-/workspace/file/models/llama2-13b/models--meta-llama--Llama-2-13b-hf/snapshots/5c31dfb671ce7cfe2d7bb7c04375e44c55e815b1/}
-
+# MODEL_PATH=${2:-/workspace/file/models/llama2-13b/models--meta-llama--Llama-2-13b-hf/snapshots/5c31dfb671ce7cfe2d7bb7c04375e44c55e815b1/}
+MODEL_PATH=${2:-/workspace/file/models/opt-125m/models--facebook--opt-125m/snapshots/27dcfa74d334bc871f3234de431e71c6eeba5dd6/}
 
 python3 ./vllm/entrypoints/server.py --model ${MODEL_PATH} \
     --local_host 0.0.0.0 --local_port ${SERVER_PORT} --worker-use-ray  \
